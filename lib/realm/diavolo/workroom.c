570b26f005e4d05587dc625d8ddd4f23797c0d33
Michael Angeli <michael.programs@gmail.com>
2021-10-11 01:11:34 -0400

    expand workroom

--- lib/realm/diavolo/workroom.c
+++ lib/realm/diavolo/workroom.c
@@ -4,4 +4,9 @@ void create() {
     ::create();
     set_short("a workroom");
     set_long("A blank canvas of a workroom.");
+    set_exits(([
+        "void": "/domain/Nowhere/void.c",
+        "freezer": "/domain/Nowhere/freezer.",
+        "planet": "/domain/Planet/virtual/room/planet/3.3",
+    ]));
 }
