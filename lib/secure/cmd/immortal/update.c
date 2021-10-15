7cf9cc58568d228768adf67b9205ec5ff2a7fb84
Michael Angeli <michael.programs@gmail.com>
2021-10-15 00:31:21 -0400

    add error check to test & update commands

--- lib/secure/cmd/immortal/update.c
+++ lib/secure/cmd/immortal/update.c
@@ -1,5 +1,9 @@
 varargs void done (int numTests, int numPassed, int numFailed, int fnsTested, int fnsUntested) {
     int numExpects = numPassed + numFailed;
+    if (!numExpects) {
+        message("system", "\nNo expects found.\n", this_user());
+        return;
+    }
     message("system", "\nPassed:    " + sprintf("%3d", numPassed) + " / " + sprintf("%3d", numExpects) + " (" + sprintf("%3d", to_int(numPassed * 100 / numExpects)) + "%)"+"\n", this_user());
     message("system", "Failed:    " + sprintf("%3d", numFailed) + " / " + sprintf("%3d", numExpects) + " (" + sprintf("%3d", to_int(numFailed * 100 / numExpects)) + "%)"+"\n", this_user());
     message("system", "Functions: " + sprintf("%3d", fnsTested) + " / " + sprintf("%3d", (fnsTested + fnsUntested)) + " (" + sprintf("%3d", to_int(fnsTested * 100 / (fnsTested + fnsUntested))) + "%)"+"\n\n", this_user());
