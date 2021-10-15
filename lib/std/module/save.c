fdb6da0facb299fb947354bb7ceee82f59f1b671
Michael Angeli <michael.programs@gmail.com>
2021-10-15 00:52:18 -0400

    remove a TODO comment

--- lib/std/module/save.c
+++ lib/std/module/save.c
@@ -3,7 +3,7 @@ nosave private string __SavePath;
 string query_save_path () {
     return __SavePath;
 }
-void set_save_path (string path) { // @TODO private for security?
+void set_save_path (string path) {
     if (undefinedp(path) || (!stringp(path) && path != 0)) {
         error("Bad argument 1 to save->set_save_path");
     }
