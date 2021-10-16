4b131001efa8f405310a74c99a0699d8c246e198
Michael Angeli <michael.programs@gmail.com>
2021-10-16 00:04:52 -0400

    add a callouts command

---
+++ lib/secure/cmd/immortal/callouts.c
@@ -0,0 +1,14 @@
+void command (string input) {
+    mixed *callouts;
+
+    write(format_header_bar("CALLOUTS", mud_name()) + "\n\n");
+    if (!sizeof(callouts = call_out_info())) {
+        write("No pending callouts.\n");
+    } else {
+        write(sprintf("%:-40s %:-25s Delay", "Object", "Function") + "\n");
+        foreach (mixed *callout in callouts) {
+            write(sprintf("%:-40O %:-25s %d\n", callout...) + "\n");
+        }
+    }
+    write("\n" + format_footer_bar() + "\n");
+}
