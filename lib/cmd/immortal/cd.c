eac92e0d21d066c196bc16032fc99983a92238cb
Michael Angeli <michael.programs@gmail.com>
2021-10-17 22:31:34 -0400

    update cd command to take relative paths

--- lib/cmd/immortal/cd.c
+++ lib/cmd/immortal/cd.c
@@ -2,6 +2,10 @@ void command (string input) {
     if (!input) {
         input = user_path(this_character()->query_key_name());
     }
+    if (input[0] != '/') {
+        input = this_user()->query_shell()->query_variable("cwd") + "/" + input;
+    }
+    input = sanitize_path(input);
     if (file_size(input) == -2) {
         this_user()->query_shell()->set_cwd(input);
         write(input + ":\n");
