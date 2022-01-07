1e0922a9cc4a97f26d6fd0b7283b2d191668355e
Michael Angeli <michael.programs@gmail.com>
2022-01-07 00:06:33 -0500

    escape newlines from callout Function column

--- lib/secure/cmd/immortal/callouts.c
+++ lib/secure/cmd/immortal/callouts.c
@@ -7,6 +7,7 @@ void command (string input) {
     } else {
         write(sprintf("%:-40s %:-25s Delay", "Object", "Function") + "\n");
         foreach (mixed *callout in callouts) {
+            callout[1] = replace_string(callout[1], "\n", "\\n");
             write(sprintf("%:-40O %:-25s %d\n", callout...) + "\n");
         }
     }
