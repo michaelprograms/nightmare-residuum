e5a60b2ee4f560a691905fde463f86bb35ae3058
Michael Angeli <michael.programs@gmail.com>
2022-01-08 00:29:24 -0500

    sort objects command by instance number

--- lib/secure/cmd/immortal/objects.c
+++ lib/secure/cmd/immortal/objects.c
@@ -1,4 +1,13 @@
-string format_object (string path, string *children, int indent);
+string *sort_instance(string *list) {
+    return sort_array(list, (: explode($1, "#")[<1] > explode($2, "#")[<1] :));
+}
+string format_object (string path, string *children, int indent) {
+    string line = pad_right("", indent * 2, " ") + path + "\n";
+    foreach (string c in sort_instance(children)) {
+        line += pad_right("", (indent + 1) * 2, " ") + c + "\n";
+    }
+    return line;
+}
 
 void command (string input) {
     object *obs = filter_array(objects(), (:$1 && regexp(file_name($1), "#"):));
@@ -19,19 +28,9 @@ void command (string input) {
             }
         }
     }
-    // @TODO recursive here...?
-    foreach (string path in keys(structure)) {
-        // write(path + " " + identify(structure[path]) + "\n");
-        write(format_object(path, structure[path], 0)+"\n");
+    foreach (string path in sort_instance(keys(structure))) {
+        write(format_object(path, structure[path], 0));
     }
 
     write("\n" + format_footer_bar() + "\n");
-}
-
-string format_object (string path, string *children, int indent) {
-    string line = pad_right("", indent * 2, " ") + path + "\n";
-    foreach(string c in children) {
-        line += pad_right("", (indent + 1) * 2, " ") + c + "\n";
-    }
-    return line;
 }
