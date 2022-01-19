98d6d559f1c55de38957e52056dbca9fec4459f3
Michael Angeli <michael.programs@gmail.com>
2022-01-19 16:56:00 -0500

    add rm and cat commands

---
+++ lib/cmd/immortal/cat.c
@@ -0,0 +1,24 @@
+void command (string input) {
+    string cwd, file, *lines;
+
+    if (!input) {
+        write("Syntax: cat [file]\n");
+        return;
+    }
+    cwd = this_user()->query_shell()->query_variable("cwd");
+    file = absolute_path(input, cwd);
+    switch (file_size(file)) {
+        case -2:
+            write("cat: " + file + ": not a file.\n");
+            return;
+        case -1:
+            write("cat: " + file + ": no such file.\n");
+            return;
+    }
+
+    lines = explode(read_file(file), "\n");
+    foreach (string line in lines) {
+        write(line + "\n");
+    }
+    return;
+}
---
+++ lib/cmd/immortal/rm.c
@@ -0,0 +1,20 @@
+void command (string input) {
+    string cwd, file;
+    if (!input) {
+        write("Syntax: rm [file]\n");
+        return;
+    }
+
+    cwd = this_user()->query_shell()->query_variable("cwd");
+    file = absolute_path(input, cwd);
+    switch (file_size(file)) {
+        case -2:
+            write("rm: " + file + ": not a file.\n");
+            return;
+        case -1:
+            write("rm: " + file + ": no such file.\n");
+            return;
+    }
+
+    write("rm: " + file + " " + (rm(file) ? "removed" : " failed to remove") + ".\n");
+}
