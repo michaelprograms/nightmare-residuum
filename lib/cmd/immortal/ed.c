c197bcc36456e02327f2b8e606c0a9ae1d7679d9
Michael Angeli <michael.programs@gmail.com>
2022-01-19 22:29:30 -0500

    add ed command

---
+++ lib/cmd/immortal/ed.c
@@ -0,0 +1,37 @@
+nosave private mapping __Locks = ([]);
+nomask private void unlock (string file) {
+    map_delete(__Locks, file);
+}
+
+void command (string input) {
+    string cwd, file;
+    int size;
+
+    if (!input) {
+        write("Syntax: ed [file]\n");
+        return;
+    }
+    cwd = this_user()->query_shell()->query_variable("cwd");
+    file = absolute_path(input, cwd);
+
+    if (objectp(__Locks[file])) {
+        write("ed: " + file + " locked by " + __Locks[file]->query_name() + ".\n");
+        return;
+    }
+
+    switch (size = file_size(file)) {
+        case -2:
+            write("ed: " + file + ": not a file.\n");
+            return;
+        case -1:
+            write("ed: " + file + ": new file.\n");
+            break;
+        default:
+            write("ed: " + file + ": " + sizeof(explode(read_file(file), "\n")) + " lines, " + size + " bytes.\n");
+            break;
+    }
+
+    __Locks[file] = this_character();
+
+    new("/secure/std/editor.c")->editor_start(file, (: unlock($(file)) :));
+}
---
+++ lib/secure/std/editor.c
@@ -0,0 +1,39 @@
+nosave private function __EndFn;
+
+nomask private string query_prompt () {
+    switch (query_ed_mode()) {
+        case -2: case 0:
+            return "";
+        default:
+            return ":";
+    }
+}
+
+private nomask void editor_input (mixed s) {
+    if (s == -1) {
+        return destruct(this_object());
+    }
+    message("no_ansi", ed_cmd(s), this_user());
+    if (query_ed_mode() == -1) {
+        this_user()->input_pop();
+        // this_user()->set_flag("editing", 0);
+        if (__EndFn) {
+            evaluate(__EndFn);
+        }
+        return destruct(this_object());
+    }
+}
+void editor_start (string file, function endFn) {
+    __EndFn = endFn;
+    this_user()->input_push((: editor_input :), (: query_prompt :));
+    ed_start(file, 0);
+    // this_user()->set_flag("editing", 1); // @TODO
+}
+
+private int clean_up() {
+    if (query_ed_mode() == -1) {
+        destruct(this_object());
+    } else {
+        return 1;
+    }
+}
