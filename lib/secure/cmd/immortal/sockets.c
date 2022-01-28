41017c3bcf8dd8449349469793b84f84edf8d3da
Michael Angeli <michael.programs@gmail.com>
2022-01-28 01:21:00 -0500

    add socket network stats to sockets

--- lib/secure/cmd/immortal/sockets.c
+++ lib/secure/cmd/immortal/sockets.c
@@ -9,5 +9,12 @@ END;
     foreach (mixed *item in socket_status()) {
         write(sprintf("    %2d  %|9s  %|8s  %-21s  %-21s\n", item[0], item[1], item[2], item[3], item[4]));
     }
+
+    write("\n");
+
+    foreach(string key, int value in filter_array(network_stats(), (:strsrch($1, "socket") > -1:))) {
+        write("    " + sprintf("%20s", key) + " : " + value + "\n");
+    }
+
     write("\n" + format_footer_bar() + "\n");
 }
