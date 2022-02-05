e4ad4da08f1f49c4bdba8b4741c8c4598952e180
Michael Angeli <michael.programs@gmail.com>
2022-02-05 17:54:19 -0500

    update STD_ACCOUNT, add properties and shorten variable names

--- lib/std/account.c
+++ lib/std/account.c
@@ -1,11 +1,11 @@
 inherit M_PARENT;
 inherit M_SAVE;
 
-private string __AccountName;
-private string __AccountPassword;
-private int __AccountCreated = time();
-private int __AccountLastOn = time();
-private mapping __AccountCharacters = ([
+private string __Name;
+private string __Password;
+private int __Created = time();
+private int __LastOn = time();
+private mapping __Characters = ([
     /* Data Format:
         STRING: ([                      // Name sanitized version
             "type":             STRING, // Body type for BODY
@@ -17,17 +17,17 @@ private mapping __AccountCharacters = ([
         ]),
     */
 ]);
-private mapping __AccountSettings = ([
+private mapping __Settings = ([
     // default settings
     "width": 80,
     "ansi": "on",
     "screenreader": "off",
 ]);
-// private mapping __AcountProperties = ([]); // @TODO
 private int __EdSetup;  // @TODO
 
+
 // cache variable, @TODO should probably be renamed
-nosave private string *__AccountCharacterNames = ({});
+nosave private string *__CharacterNames = ({});
 
 // -----------------------------------------------------------------------------
 
@@ -37,48 +37,48 @@ void create () {
 
 int is_account () { return 1; }
 
-void set_name (string name) {
-    __AccountName = name;
-    if (!stringp(__AccountName)) {  // name can be cleared during account creation
+void set_name (string name) { // @TODO setup_account?
+    __Name = name;
+    if (!stringp(__Name)) {  // name can be cleared during account creation
         return;
     }
-    set_save_path(D_ACCOUNT->query_save_path(lower_case(__AccountName)));
-    if (D_ACCOUNT->query_exists(__AccountName)) {
+    set_save_path(D_ACCOUNT->query_save_path(lower_case(__Name)));
+    if (D_ACCOUNT->query_exists(__Name)) {
         restore_data();
     }
 }
 string query_name () {
-    return __AccountName;
+    return __Name;
 }
 string query_key_name () {
-    return __AccountName ? lower_case(__AccountName) : 0;
+    return __Name ? lower_case(__Name) : 0;
 }
 
 nomask void set_password (string str) {
     if (base_name(previous_object()) != "/secure/user/user") {  // TODO better way to match?
         error("illegal attempt to set_password");
     }
-    __AccountPassword = str;
+    __Password = str;
     save_data();
 }
 nomask string query_password () {
-    return __AccountPassword;
+    return __Password;
 }
 
 int query_created () {
-    return __AccountCreated;
+    return __Created;
 }
 int query_last_on () {
-    return __AccountLastOn;
+    return __LastOn;
 }
 void set_last_on () {
-    __AccountLastOn = time();
+    __LastOn = time();
     save_data();
 }
 
 void add_character (string name, string nameClean, string type) {
     // @TODO security?
-    __AccountCharacters[nameClean] = ([
+    __Characters[nameClean] = ([
         "name": name,
         "deleted": 0,
         "type": type,
@@ -89,43 +89,43 @@ void add_character (string name, string nameClean, string type) {
     save_data();
 }
 mapping query_character (string name) {
-    return __AccountCharacters[name];
+    return __Characters[name];
 }
 int query_has_playable_characters () {
-    mapping tmpCharacters = filter_mapping(__AccountCharacters, (: !$2["deleted"] :));
-    __AccountCharacterNames = sort_array(keys(tmpCharacters), 1);
-    return sizeof(__AccountCharacterNames) > 0;
+    mapping tmpCharacters = filter_mapping(__Characters, (: !$2["deleted"] :));
+    __CharacterNames = sort_array(keys(tmpCharacters), 1);
+    return sizeof(__CharacterNames) > 0;
 }
 string *query_character_names () {
-    return __AccountCharacterNames;
+    return __CharacterNames;
 }
 void update_character_data (object character) {
     string name;
     if (!character || !(name = character->query_key_name())) {
         return;
     }
-    __AccountCharacters[name]["type"] = character->query_type();
-    __AccountCharacters[name]["level"] = character->query_level();
-    __AccountCharacters[name]["last_action"] = character->query_last_action();
-    __AccountCharacters[name]["last_location"] = character->query_last_location();
+    __Characters[name]["type"] = character->query_type();
+    __Characters[name]["level"] = character->query_level();
+    __Characters[name]["last_action"] = character->query_last_action();
+    __Characters[name]["last_location"] = character->query_last_location();
     save_data();
 }
 
 void set_deleted (string name) {
-    __AccountCharacters[name]["deleted"] = 1;
+    __Characters[name]["deleted"] = 1;
     save_data();
 }
 
 void set_setting (string key, mixed value) {
-    if (!__AccountSettings || !key) return;
-    __AccountSettings[key] = value;
+    if (!__Settings || !key) return;
+    __Settings[key] = value;
     save_data();
 }
 mixed query_setting (string key) {
-    if (!__AccountSettings || !key) return 0;
-    if (member_array(key, keys(__AccountSettings)) == -1) return 0;
-    return __AccountSettings[key];
+    if (!__Settings || !key) return 0;
+    if (member_array(key, keys(__Settings)) == -1) return 0;
+    return __Settings[key];
 }
 mapping query_settings () {
-    return __AccountSettings;
+    return __Settings;
 }
