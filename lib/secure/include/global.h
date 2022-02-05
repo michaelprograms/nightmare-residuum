cc7387e2b6560ee6d9c7b4ce4278d6986416c6fc
Michael Angeli <michael.programs@gmail.com>
2022-02-05 18:39:51 -0500

    separate daemons in global.h

--- lib/secure/include/global.h
+++ lib/secure/include/global.h
@@ -19,17 +19,20 @@
 #define M_SAVE          "/secure/module/save.c"
 #define M_TEST          "/secure/module/test.c"
 
-// daemon
+// daemons
+#define D_ANSI          "/daemon/ansi.c"
+#define D_CHANNEL       "/daemon/channel.c"
+#define D_LOG           "/daemon/log.c"
+#define D_SOUL          "/daemon/soul.c"
+#define D_WELCOME       "/daemon/welcome.c"
+
+// secure daemons
 #define D_ACCESS        "/secure/daemon/access.c"
 #define D_ACCOUNT       "/secure/daemon/account.c"
 #define D_CHARACTER     "/secure/daemon/character.c"
 #define D_COMMAND       "/secure/daemon/command.c"
 #define D_IPC           "/secure/daemon/ipc.c"
 #define D_TEST          "/secure/daemon/test.c" // needs .c
-#define D_ANSI          "/daemon/ansi.c"
-#define D_LOG           "/daemon/log.c"
-#define D_SOUL          "/daemon/soul.c"
-#define D_WELCOME       "/daemon/welcome.c"
 
 #define OBJ_USER        "/secure/user/user"
 
