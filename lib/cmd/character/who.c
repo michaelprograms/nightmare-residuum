d25d99f71989ebe96e77709fad8bb04dbabe9d8b
Michael Angeli <michael.programs@gmail.com>
2022-02-05 23:24:23 -0500

    update who command to only show accounts to immortals

--- lib/cmd/character/who.c
+++ lib/cmd/character/who.c
@@ -2,7 +2,11 @@ void command (string input) {
     write(format_header_bar("WHO", mud_name()) + "\n\n");
     foreach (object user in users()) {
         if (user->query_character()) {
-            write("    "+pad_right(user->query_character()->query_name()+" ("+user->query_account()->query_name()+")", 50) + capitalize(user->query_character()->query_species()+"") + "\n");
+            string characterName = user->query_character()->query_name();
+            if (this_character()->query_immortal()) {
+                characterName += " (" + user->query_account()->query_name() + ")";
+            }
+            write("    "+pad_right(characterName, 50) + capitalize(user->query_character()->query_species()+"") + "\n");
         } else {
             write("    ("+user->query_account()->query_name()+" - "+identify(user)+"\n");
         }
