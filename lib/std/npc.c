29daa64b8801dfe0363dae6bff3ca901a02af079
Michael Angeli <michael.programs@gmail.com>
2022-02-06 18:25:50 -0500

    add npc.test.c

--- lib/std/npc.c
+++ lib/std/npc.c
@@ -1,3 +1,5 @@
 inherit STD_LIVING;
 
+int is_npc () { return 1; }
+
 // @TODO what does NPC need?
---
+++ lib/std/npc.test.c
@@ -0,0 +1,25 @@
+inherit M_TEST;
+
+private nosave object testOb;
+void before_each_test () {
+    if (objectp(testOb)) destruct(testOb);
+    testOb = clone_object("/std/npc.c");
+}
+void after_all_tests () {
+    if (objectp(testOb)) destruct(testOb);
+}
+
+void test_npc () {
+    int *values = ({}), *results = ({});
+
+    expect_function("is_npc", testOb);
+
+    values += ({ testOb->is_living() });
+    results += ({ 1 });
+    values += ({ testOb->is_npc() });
+    results += ({ 1 });
+    values += ({ testOb->is_character() });
+    results += ({ UNDEFINED });
+
+    expect_arrays_equal(values, results, "is_npc handled");
+}
