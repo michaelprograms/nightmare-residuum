c22f222471ff2790ca792c83f122c4d14abfb171
Michael Angeli <michael.programs@gmail.com>
2022-02-13 00:20:39 -0500

    add test files for remaining untested sefun files

---
+++ lib/secure/sefun/user.test.c
@@ -0,0 +1,11 @@
+#include <time.h>
+
+inherit M_TEST;
+
+private nosave object testOb;
+void before_all_tests () {
+    testOb = clone_object("/secure/sefun/user.c");
+}
+void after_all_tests () {
+    if (objectp(testOb)) destruct(testOb);
+}
