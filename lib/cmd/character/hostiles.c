462d3c9f04e73f250462a3175f5f34786b68b264
Michael Angeli <michael.programs@gmail.com>
2022-02-27 21:00:25 -0500

    add hostiles command

---
+++ lib/cmd/character/hostiles.c
@@ -0,0 +1,16 @@
+void command (string input) {
+    object *hostiles = this_character()->query_hostiles();
+
+    write(format_header_bar("HOSTILES") + "\n");
+
+    if (hostiles) {
+        foreach (object ob in this_character()->query_hostiles()) {
+            write("\n    " + ob->query_name());
+        }
+        write("\n");
+    } else {
+        write("\n    No current hostiles\n");
+    }
+
+    write("\n" + format_footer_bar() + "\n");
+}
