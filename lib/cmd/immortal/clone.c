ba8ae0ea0461c5f2d62a3428632054c4720f972a
Michael Angeli <michael.programs@gmail.com>
2022-03-03 18:02:17 -0500

    update clone command to check file existence and display environment messages

--- lib/cmd/immortal/clone.c
+++ lib/cmd/immortal/clone.c
@@ -8,12 +8,19 @@ void command (string input) {
     }
     cwd = this_user()->query_shell()->query_variable("cwd");
     file = absolute_path(input, cwd);
+    if (file_size(file) < 1) {
+        write("clone: no such file.\n");
+        return;
+    }
+
     ob = clone_object(file);
     if (ob->is_living()) {
-        write("You clone " + ob->query_name() + " into your environment.\n");
+        message("action", "You clone " + ob->query_name() + " into your environment.\n", this_character());
+        message("action", this_character()->query_name() + " clones " + ob->query_name() + ".\n", environment(this_character()), this_character());
         ob->handle_move(environment(this_character()));
     } else {
-        write("You clone " + ob->query_name() + " into your inventory.\n");
+        message("action", "You clone " + ob->query_name() + " into your inventory.\n", this_character());
+        message("action", this_character()->query_name() + " clones " + ob->query_name() + ".\n", environment(this_character()), this_character());
         ob->handle_move(this_character());
     }
 }
