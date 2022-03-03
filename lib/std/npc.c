6aec26c240091f96a184f411ce5bd52d2c0956c8
Michael Angeli <michael.programs@gmail.com>
2022-03-02 22:08:31 -0500

    update npc to adjust stats on set_level

--- lib/std/npc.c
+++ lib/std/npc.c
@@ -6,6 +6,18 @@ void create () {
     ::create();
     if (!clonep()) return;
     set_heart_beat(1);
+}
+
+void set_level (int l) {
+    ::set_level(l);
+
+    set_stat("strength", l * 4);
+    set_stat("perception", l * 5);
+    set_stat("endurance", l * 5);
+    set_stat("charisma", l * 3);
+    set_stat("intelligence", l * 4);
+    set_stat("agility", l * 5);
+    set_stat("luck", to_int(l * 2.5) + secure_random(l + 1));
+
     update_vitals(1);
 }
-// @TODO what does NPC need?
