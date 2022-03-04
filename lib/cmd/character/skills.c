6ede86356bc549b33b796a3878c735ab92290aa0
Michael Angeli <michael.programs@gmail.com>
2022-03-03 21:26:32 -0500

    update skills command to allow target

--- lib/cmd/character/skills.c
+++ lib/cmd/character/skills.c
@@ -1,17 +1,21 @@
 void command (string input) {
-    object tc = this_character();
-    string *skills = tc->query_all_skills(), *list = ({});
+    object tc = this_character(), target = tc;
+    string *skills = sort_array(tc->query_all_skills(), 1), *list = ({});
     int width, columns;
 
-    write(format_header_bar("SKILLS") + "\n\n");
+    if (input && tc->query_immortal()) {
+        if (find_character(input)) target = find_character(input);
+        else if(present(input, environment(tc))) target = present(input, environment(tc));
+    }
+
+    write(format_header_bar("SKILLS", (target != tc ? target->query_name() : 0)) + "\n\n");
 
     if (sizeof(skills)) {
         width = this_user()->query_account()->query_setting("width") || 80;
         columns = width / 40;
 
-        skills = sort_array(skills, 1);
         foreach (string skill in skills) {
-            list += ({ sprintf("  %-18s %4s %4s", skill, "" + tc->query_skill(skill), tc->query_skill_progress(skill)) });
+            list += ({ sprintf("  %-18s %4s %4s", skill, "" + target->query_skill(skill), target->query_skill_progress(skill)) });
         }
         write(format_page(list, columns));
     }
