a33819158f522d346d24cc729e9307d0a1b5edc0
Michael Angeli <michael.programs@gmail.com>
2022-03-05 15:18:40 -0500

    update stats command for immortals to query others

--- lib/cmd/character/stats.c
+++ lib/cmd/character/stats.c
@@ -1,7 +1,18 @@
+#define STATS ({"strength","perception","endurance","charisma","intelligence","agility","luck"})
+
 void command (string input) {
-    write(format_header_bar("STATS") + "\n\n");
-    foreach(string stat in ({"strength","perception","endurance","charisma","intelligence","agility","luck"})) {
-        write(pad_left(stat, 15)+"   "+this_character()->query_stat(stat)+"\n");
+    object tc = this_character(), target = tc;
+
+    if (input && tc->query_immortal()) {
+        if (find_character(input)) target = find_character(input);
+        else if(present(input, environment(tc))) target = present(input, environment(tc));
     }
+
+    write(format_header_bar("STATS", (target != tc ? target->query_name() : 0)) + "\n\n");
+
+    foreach (string stat in STATS) {
+        write(sprintf("  %-15s %s", stat, ""+target->query_stat(stat)) + "\n");
+    }
+
     write("\n" + format_footer_bar() + "\n");
 }
