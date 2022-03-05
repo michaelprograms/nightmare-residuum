1ea50d8d32527f7f32839caf2872856b33a89fdf
Michael Angeli <michael.programs@gmail.com>
2022-03-05 15:18:27 -0500

    update hp command to show level

--- lib/cmd/character/hp.c
+++ lib/cmd/character/hp.c
@@ -3,5 +3,5 @@ void command (string input) {
     string hp = tc->query_hp() + "/" + tc->query_max_hp() + " hp";
     string sp = tc->query_sp() + "/" + tc->query_max_sp() + " sp";
     string mp = tc->query_mp() + "/" + tc->query_max_mp() + " mp";
-    write(hp + " " + sp + " " + mp + "\n");
+    write("You have " + hp + " " + sp + " " + mp + " and your level is " + tc->query_level() + ".\n");
 }
