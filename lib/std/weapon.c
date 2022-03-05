b4fc3edf163b8963c8098d0eef1e7ed71a217ffa
Michael Angeli <michael.programs@gmail.com>
2022-03-04 22:16:39 -0500

    update weapon

--- lib/std/weapon.c
+++ lib/std/weapon.c
@@ -3,6 +3,16 @@ inherit STD_ITEM;
 nosave string __Type;
 nosave object __Wielder;
 
+object query_wielded ();
+
+string query_short () {
+    string short = ::query_short();
+
+    if (query_wielded()) short += " (wielded)";
+
+    return short;
+}
+
 void set_type (string type) {
     __Type = type;
 }
@@ -24,4 +34,8 @@ mixed direct_wield_obj (object ob, string str) {
 
     if (environment() != previous_object()) return 0;
     return 1;
+}
+mixed direct_unwield_obj (object ob, string str) {
+    object po = previous_object();
+    return environment() == po && __Wielder == po && member_array(this_object(), po->query_wielded_weapons()) > -1;
 }
