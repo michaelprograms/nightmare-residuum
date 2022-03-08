750c8dbbe763266cfa663a6aea867aa0e2626c50
Michael Angeli <michael.programs@gmail.com>
2022-03-07 21:23:32 -0500

    update room in human start area

--- lib/domain/Start/human/room/broad_st_n1.c
+++ lib/domain/Start/human/room/broad_st_n1.c
@@ -5,7 +5,7 @@ inherit STD_ROOM;
 void create () {
     ::create();
     set_short("a village street");
-    set_long("A small street traveling north and south through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. A building with an open doorway stands on the east side of the street. A public square can be seen to the south. A framework of hexagons glimmer overhead.");
+    set_long("A small street traveling north and south through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. A building with an Adventurer Hall banner has open doors on the east side of the street. A public square can be seen to the south. A framework of hexagons glimmer overhead.");
     set_exits(([
         "north": HUMAN_ROOM + "broad_st_n2.c",
         "enter east": HUMAN_ROOM "hall_adventure.c",
@@ -13,7 +13,8 @@ void create () {
     ]));
     set_looks(([
         "street": "A village street labeled N Broad St.",
-        "building": "The open doorway leads into a large hall filled with training equipment.",
+        "building": "The open doors leads into a hall for all adventuerers.",
+        "banner": "The banner simply says Adventurer Hall.",
         "square": "A square plaza with trees, benches, and more.",
     ]));
 }
