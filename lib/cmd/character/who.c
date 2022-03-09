d435ea38a4584aaad331d901078163e9c1e9cece
Michael Angeli <michael.programs@gmail.com>
2022-03-08 23:18:01 -0500

    update who command to show levels

--- lib/cmd/character/who.c
+++ lib/cmd/character/who.c
@@ -3,15 +3,16 @@ void command (string input) {
     write(format_header_bar("WHO", mud_name()) + "\n\n");
 
     foreach (object user in users()) {
-        if (user->query_character()) {
-            string characterName = user->query_character()->query_cap_name();
+        object char;
+        if (char = user->query_character()) {
+            string characterName = char->query_cap_name();
             if (this_character()->query_immortal()) {
                 characterName += " (" + user->query_account()->query_name() + ")";
             }
             if (this_user() != user) {
                 characterName += " " + time_from_seconds(query_idle(user));
             }
-            write("    "+sprintf("%-50s", characterName) + capitalize(user->query_character()->query_species()+"") + "\n");
+            write("    "+sprintf("%-50s", characterName) + sprintf("%-16s", capitalize(char->query_species())) + " " + sprintf("%3d", char->query_level()) + "\n");
         } else {
             write("    ("+user->query_account()->query_name()+" - "+identify(user)+"\n");
         }
