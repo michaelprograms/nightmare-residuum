2ec2f02ff452a78e2fb7b5edb4c43b7939293467
Michael Angeli <michael.programs@gmail.com>
2022-03-18 21:41:59 -0400

    update sefun/color tests

--- lib/secure/sefun/color.test.c
+++ lib/secure/sefun/color.test.c
@@ -1,41 +1,39 @@
 inherit M_TEST;
 
 private nosave object testOb;
-void before_all_tests () {
-    testOb = clone_object("/secure/sefun/color");
+void before_each_test () {
+    testOb = clone_object("/secure/sefun/color.c");
 }
-void after_all_tests () {
+void after_each_test () {
     if (objectp(testOb)) destruct(testOb);
 }
 
 void test_hex_to_int () {
-    int *values = ({}), *results = ({});
-
     expect_function("hex_to_int", testOb);
 
-    values += ({ testOb->hex_to_int("00") });
-    results += ({ 0 });
-    values += ({ testOb->hex_to_int("0A") });
-    results += ({ 10 });
-    values += ({ testOb->hex_to_int("0F") });
-    results += ({ 15 });
-    values += ({ testOb->hex_to_int("A0") });
-    results += ({ 160 });
-    values += ({ testOb->hex_to_int("F0") });
-    results += ({ 240 });
-    values += ({ testOb->hex_to_int("AA") });
-    results += ({ 170 });
-    values += ({ testOb->hex_to_int("BB") });
-    results += ({ 187 });
-    values += ({ testOb->hex_to_int("CC") });
-    results += ({ 204 });
-    values += ({ testOb->hex_to_int("DD") });
-    results += ({ 221 });
-    values += ({ testOb->hex_to_int("EE") });
-    results += ({ 238 });
-    values += ({ testOb->hex_to_int("FF") });
-    results += ({ 255 });
-    expect_arrays_equal(values, results, "hex_to_int handled base16");
+    expect("hex_to_int handled base 16 to base 10", (: ({
+        assert((: testOb->hex_to_int("00") :), "==", 0),
+        assert((: testOb->hex_to_int("0A") :), "==", 10),
+        assert((: testOb->hex_to_int("0F") :), "==", 15),
+        assert((: testOb->hex_to_int("A0") :), "==", 160),
+        assert((: testOb->hex_to_int("F0") :), "==", 240),
+        assert((: testOb->hex_to_int("AA") :), "==", 170),
+        assert((: testOb->hex_to_int("BB") :), "==", 187),
+        assert((: testOb->hex_to_int("CC") :), "==", 204),
+        assert((: testOb->hex_to_int("DD") :), "==", 221),
+        assert((: testOb->hex_to_int("EE") :), "==", 238),
+        assert((: testOb->hex_to_int("FF") :), "==", 255),
+        assert((: testOb->hex_to_int("0a") :), "==", 10),
+        assert((: testOb->hex_to_int("0f") :), "==", 15),
+        assert((: testOb->hex_to_int("a0") :), "==", 160),
+        assert((: testOb->hex_to_int("f0") :), "==", 240),
+        assert((: testOb->hex_to_int("aa") :), "==", 170),
+        assert((: testOb->hex_to_int("bb") :), "==", 187),
+        assert((: testOb->hex_to_int("cc") :), "==", 204),
+        assert((: testOb->hex_to_int("dd") :), "==", 221),
+        assert((: testOb->hex_to_int("ee") :), "==", 238),
+        assert((: testOb->hex_to_int("ff") :), "==", 255),
+    }) :));
 
     expect_catches (({
         (: testOb->hex_to_int() :),
@@ -45,6 +43,8 @@ void test_hex_to_int () {
         (: testOb->hex_to_int(([])) :),
         (: testOb->hex_to_int("A") :),
         (: testOb->hex_to_int("GG") :),
+        (: testOb->hex_to_int("#A") :),
+        (: testOb->hex_to_int("-a") :),
+        (: testOb->hex_to_int("a ") :),
     }), "*Bad argument 1 to color->hex_to_int\n", "hex_to_int bandled bad inputs");
-
 }
