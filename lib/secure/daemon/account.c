d33f9cc36f2a9dffb67ce07f45b499a39598a350
Michael Angeli <michael.programs@gmail.com>
2022-03-20 21:43:16 -0400

    update D_ACCOUNT and test

--- lib/secure/daemon/account.c
+++ lib/secure/daemon/account.c
@@ -7,6 +7,7 @@ int query_valid_name (string name) {
 }
 
 string query_save_path (string name) {
+    if (strlen(name) < 4) error("Bad argument 1 to account->query_save_path");
     return sprintf(DIR_SAVE_ACCOUNT+"/%c/%s.o", name[0], name);
 }
 
--- lib/secure/daemon/account.test.c
+++ lib/secure/daemon/account.test.c
@@ -5,63 +5,54 @@ void before_each_test () {
     if (objectp(testOb)) destruct(testOb);
     testOb = clone_object("/secure/daemon/account");
 }
-void after_all_tests () {
+void after_each_test () {
     if (objectp(testOb)) destruct(testOb);
 }
 
 void test_valid_name () {
-    int *values = ({}), *results = ({});
-
     expect_function("query_valid_name", testOb);
 
-    values += ({ testOb->query_valid_name("valid") });
-    results += ({ 1 });
-    values += ({ testOb->query_valid_name("VALID") });
-    results += ({ 1 });
-    values += ({ testOb->query_valid_name("V4L1D") });
-    results += ({ 1 });
-    values += ({ testOb->query_valid_name("Valid123") });
-    results += ({ 1 });
-    values += ({ testOb->query_valid_name("valid_") });
-    results += ({ 1 });
-    values += ({ testOb->query_valid_name("valid@") });
-    results += ({ 1 });
-    values += ({ testOb->query_valid_name("valid.") });
-    results += ({ 1 });
-    values += ({ testOb->query_valid_name("valid-") });
-    results += ({ 1 });
+    expect("query_valid_name handles names", (: ({
+        assert((: testOb->query_valid_name("valid") :), "==", 1),
+        assert((: testOb->query_valid_name("VALID") :), "==", 1),
+        assert((: testOb->query_valid_name("V4L1D") :), "==", 1),
+        assert((: testOb->query_valid_name("Valid123") :), "==", 1),
+        assert((: testOb->query_valid_name("valid_") :), "==", 1),
+        assert((: testOb->query_valid_name("valid@") :), "==", 1),
+        assert((: testOb->query_valid_name("valid.") :), "==", 1),
+        assert((: testOb->query_valid_name("valid-") :), "==", 1),
 
-    values += ({ testOb->query_valid_name("") });
-    results += ({ 0 });
-    values += ({ testOb->query_valid_name("no") });
-    results += ({ 0 });
-    values += ({ testOb->query_valid_name("nonononononononononononononononononononononononononononononononom") });
-    results += ({ 0 });
-    values += ({ testOb->query_valid_name("invalid!@#") });
-    results += ({ 0 });
-    values += ({ testOb->query_valid_name(",<.>;:'\"[{]}\\|=+!#$%^&*()~`") });
-    results += ({ 0 });
-    values += ({ testOb->query_valid_name("_valid") });
-    results += ({ 0 });
-    values += ({ testOb->query_valid_name("@valid") });
-    results += ({ 0 });
-    values += ({ testOb->query_valid_name(".valid") });
-    results += ({ 0 });
-    values += ({ testOb->query_valid_name("-valid") });
-    results += ({ 0 });
-
-    expect_arrays_equal (values, results, "query_valid_name handled names");
+        assert((: testOb->query_valid_name("") :), "==", 0),
+        assert((: testOb->query_valid_name("no") :), "==", 0),
+        assert((: testOb->query_valid_name("nonononononononononononononononononononononononononononononononom") :), "==", 0),
+        assert((: testOb->query_valid_name("invalid!@#") :), "==", 0),
+        assert((: testOb->query_valid_name(",<.>;:'\"[{]}\\|=+!#$%^&*()~`") :), "==", 0),
+        assert((: testOb->query_valid_name("_valid") :), "==", 0),
+        assert((: testOb->query_valid_name("@valid") :), "==", 0),
+        assert((: testOb->query_valid_name(".valid") :), "==", 0),
+        assert((: testOb->query_valid_name("-valid") :), "==", 0),
+    }) :));
 }
 
-void test_valid_account () {
-    int *values = ({}), *results = ({});
+void test_save_path () {
+    expect_function("query_save_path", testOb);
 
+    expect("query_save_path handles valid names", (: ({
+        assert((: testOb->query_save_path("name") :), "regex", "^/save/account/n/name"),
+    }) :));
+    expect("query_save_path handles bad arguments", (: ({
+        assert((: testOb->query_save_path("") :), "catch", "*Bad argument 1 to account->query_save_path\n"),
+        assert((: testOb->query_save_path("abc") :), "catch", "*Bad argument 1 to account->query_save_path\n"),
+    }) :));
+}
+
+void test_exists () {
     expect_function("query_exists", testOb);
 
-    values += ({ testOb->query_exists("") });
-    results += ({ 0 });
-    values += ({ testOb->query_exists("testaccountinvalid") });
-    results += ({ 0 });
+    // @TODO valid test account
 
-    expect_arrays_equal (values, results, "query_exists handled accounts");
+    expect("query_exists handles invalid accounts", (: ({
+        assert((: testOb->query_exists("") :), "==", 0),
+        assert((: testOb->query_exists("testaccountinvalid") :), "==", 0),
+    }) :));
 }
