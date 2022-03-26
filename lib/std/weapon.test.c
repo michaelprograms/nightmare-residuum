14987286e8ecf37c5f2d2cd202ce59b0b61f3fb2
Michael Angeli <michael.programs@gmail.com>
2022-03-26 01:00:44 -0400

    cleanup weapon.test.c

--- lib/std/weapon.test.c
+++ lib/std/weapon.test.c
@@ -87,37 +87,3 @@ void test_item_verb_wield_applies () {
     __WieldableLimbs = 0;
     __WieldedWeapons = 0;
 }
-
-// void test_handle_eat () {
-//     object r = new(STD_ROOM);
-
-//     expect_function("handle_eat", testOb);
-
-//     expect("food handle_eat behaves", (: ({
-//         // setup food
-//         assert(testOb->set_strength(5), "==", 0),
-//         assert(testOb->query_strength(), "==", 5),
-
-//         // setup test object
-//         assert(this_object()->handle_move($(r)), "==", 1),
-//         assert(this_object()->update_vitals(), "==", 0), // initialize vitals
-//         assert(this_object()->add_hp(5), "==", 0),
-//         assert(this_object()->query_hp(), "==", 5),
-//         assert(this_object()->add_sp(5), "==", 0),
-//         assert(this_object()->query_sp(), "==", 5),
-//         assert(this_object()->add_mp(5), "==", 0),
-//         assert(this_object()->query_mp(), "==", 5),
-
-//         // test eating
-//         assert(testOb->handle_eat(this_object()), "==", 0),
-//         assert(this_object()->query_hp(), "==", 10),
-//         assert(this_object()->query_sp(), "==", 10),
-//         assert(this_object()->query_mp(), "==", 10),
-
-//         assert(objectp(testOb), "==", 0), // food was removed
-//     }) :));
-
-//     // cleanup
-//     this_object()->handle_move("/domain/Nowhere/room/void.c");
-//     if (r) destruct(r);
-// }
