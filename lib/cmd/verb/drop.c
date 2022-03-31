df20f52bf479afb5498fe66b055cef3829a44c65
Michael Angeli <michael.programs@gmail.com>
2022-03-30 23:29:02 -0400

    update verb/drop to allow dropping of currencies

--- lib/cmd/verb/drop.c
+++ lib/cmd/verb/drop.c
@@ -2,7 +2,7 @@ inherit STD_VERB;
 
 void create () {
     verb::create();
-    add_rules( ({ "", "OBS", }) ); // @TODO if currency "WRD STR" }));
+    add_rules( ({ "", "OBS", "WRD WRD", }) );
 }
 
 mixed can_drop () {
@@ -26,10 +26,33 @@ void do_drop_obs (mixed *info, string str) {
     }
 }
 
-// @TODO if currency
-// varargs mixed can_drop_wrd_str (string word, string str) {
-//     write("can_drop_wrd_str " + word + " " + str + "\n");
-// }
-// varargs mixed do_drop_wrd_str (string word, string str) {
-//     write("do_drop_wrd_str " + word + " " + str + "\n");
-// }
+mixed can_drop_wrd_wrd (mixed args...) {
+    int amount = to_int(args[0]), n;
+    string currency = args[1];
+
+    if (amount < 1) return 0;
+    if ((n = this_character()->query_currency(currency)) < 1) {
+        return "You do not have any " + currency + " to drop.";
+    }
+    if (n < amount) {
+        return "You do not have enough " + currency + " to drop that much.";
+    }
+    return 1;
+}
+mixed do_drop_wrd_wrd (mixed args...) {
+    int amount = to_int(args[0]);
+    string currency = args[1];
+    object env = environment(this_character());
+    object coins;
+
+    coins = new("/std/item/coins.c");
+    if (!coins->handle_move(env) || !this_character()->add_currency(currency, -amount)) {
+        message("verb", "You are unable to drop that.\n", this_character());
+        coins->handle_remove();
+        return 1;
+    }
+    coins->add_currency(currency, amount);
+    message("verb", "You drop " + amount + " " + currency + ".\n", this_character());
+    message("verb", this_character()->query_cap_name() + " drops some " + currency + ".\n", env, this_character());
+    return 1;
+}
