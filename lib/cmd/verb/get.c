08f4e9a76b0e1b7558b9b8f1556241434f9a4acd
Michael Angeli <michael.programs@gmail.com>
2022-04-10 12:36:38 -0400

    update verb/get messaging

--- lib/cmd/verb/get.c
+++ lib/cmd/verb/get.c
@@ -13,13 +13,16 @@ mixed can_get_obj (object ob, string str) {
     return 1;
 }
 void do_get_obj (object ob, string str) {
-    write("You get " + ob->query_name() + ".\n");
-    ob->handle_move(this_character());
+    object po = previous_object();
+
+    message("verb", "You take " + ob->query_name() + ".\n", po);
+    message("verb", po->query_cap_name() + " takes " + ob->query_name() + ".\n", environment(ob), ({ po, ob }));
+    ob->handle_move(po);
 }
 void do_get_obs (mixed *info, string str) {
     foreach (mixed item in info) {
         if (stringp(item)) {
-            write(item + "\n");
+            message("verb", item + "\n", previous_object());
         } else {
             do_get_obj(item, str);
         }
