bab382115eaf232a21826a24e4aec4852221b69b
Michael Angeli <michael.programs@gmail.com>
2022-04-10 12:50:42 -0400

    add test to coins.test.c

--- lib/std/item/coins.test.c
+++ lib/std/item/coins.test.c
@@ -7,3 +7,15 @@ void before_each_test () {
 void after_each_test () {
     if (objectp(testOb)) destruct(testOb);
 }
+
+void test_currency () {
+    expect_function("is_currency", testOb);
+
+    expect("is_currency behaves", (: ({
+        assert(testOb->is_item(), "==", 1),
+        assert(testOb->is_currency(), "==", 1),
+        assert(testOb->is_character(), "==", UNDEFINED),
+        assert(testOb->is_monster(), "==", UNDEFINED),
+        assert(testOb->is_npc(), "==", UNDEFINED),
+    }) :));
+}
