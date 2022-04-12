5c4bf024c9e5a3f9b1262982a301698f2ea95f01
Michael Angeli <michael.programs@gmail.com>
2022-04-11 23:47:46 -0400

    update stats command to use format_border

--- lib/cmd/character/stats.c
+++ lib/cmd/character/stats.c
@@ -1,18 +1,32 @@
-#define STATS ({"strength","perception","endurance","charisma","intelligence","agility","luck"})
-
 void command (string input) {
     object tc = this_character(), target = tc;
+    string *border, subtitle, *items = ({ });
 
     if (input && tc->query_immortal()) {
         if (find_character(input)) target = find_character(input);
         else if(present(input, environment(tc))) target = present(input, environment(tc));
     }
 
-    write(format_header_bar("STATS", (target != tc ? target->query_cap_name() : 0)) + "\n\n");
-
-    foreach (string stat in STATS) {
-        write(sprintf("  %-15s %3d %12s xp", stat, target->query_stat(stat), format_integer(D_EXPERIENCE->query_stat_cost(stat, target->query_stat(stat)))) + "\n");
+    foreach (string stat in ({ "strength", "perception", "endurance", "charisma", "intelligence", "agility", "luck", })) {
+        items += ({ sprintf("%-15s %3d %12s xp", stat, target->query_stat(stat), format_integer(D_EXPERIENCE->query_stat_cost(stat, target->query_stat(stat)))) });
     }
 
-    write("\n" + format_footer_bar() + "\n");
+    border = format_border(([
+        "title": "STATS",
+        "subtitle": target->query_cap_name(),
+        "body": ([
+            "items": items,
+            "columns": 1,
+        ]),
+        "footer": ([
+            "items": ({
+                "You have " + format_integer(target->query_experience()) + " experience points to train",
+            }),
+            "columns": 1,
+            "align": "center",
+        ])
+    ]));
+    foreach (string line in border) {
+        message("system", line + "\n", tc);
+    }
 }
