bcd85d1c493c6129258efb3b1af7d8cbf4bf0a6e
Michael Angeli <michael.programs@gmail.com>
2022-04-19 18:32:59 -0400

    update body command

--- lib/cmd/character/body.c
+++ lib/cmd/character/body.c
@@ -1,17 +1,27 @@
 void command (string input, mapping flags) {
     object tc = this_character(), target = tc;
-    string *border, *items = ({ });
+    string *border, *items = ({ }), *limbs;
 
     if (input && tc->query_immortal()) {
         if (find_character(input)) target = find_character(input);
         else if(present(input, environment(tc))) target = present(input, environment(tc));
     }
 
-    foreach (string type in ({ "head", "torso", "arm", "hand", "leg", "foot", "paw", "tail", })) {
-        foreach (string l in sort_array(filter_array(target->query_limbs(), (: regexp($1, $(type)) :)), 1)) {
+    limbs = target->query_limbs();
+    foreach (string type in ({
+        "head", "torso", "thorax", "abdomen",
+        "arm", "hand", "leg", "foot",
+        "paw", "wing", "tail",
+    })) {
+        foreach (string l in sort_array(filter_array(limbs, (: regexp($1, $(type)) :)), 1)) {
             mapping limb = target->query_limb(l);
             items += ({ sprintf("  %-16s %3s", l, (limb["damage"]*100/limb["maxdamage"])+"%") });
+            limbs -= ({ l });
         }
+    }
+    foreach (string l in limbs) {
+        mapping limb = target->query_limb(l);
+        items += ({ sprintf("  %-16s %3s", l, (limb["damage"]*100/limb["maxdamage"])+"%") });
     }
 
     border = format_border(([
