3c7c9e647da128d29471c47c735f81f3a7758565
Michael Angeli <michael.programs@gmail.com>
2022-05-03 00:38:13 -0400

    update alias command to use format_border

--- lib/secure/cmd/character/alias.c
+++ lib/secure/cmd/character/alias.c
@@ -1,32 +1,50 @@
 inherit "/std/class/shell_alias.c";
 
 private void print_all_aliases (object shell) {
-    string output;
     class ShellAlias alias;
     string *aliasKeys;
+    string *border, *list = ({ });
 
-    write(format_header_bar("ALIAS", "List") + "\n\n");
-    // @TODO cleanup columns
-    write(sprintf("Alias:%9sExpansion:%29sDefaults:\n", "", "") + "\n");
     aliasKeys = shell->query_alias_names();
     foreach (string name in sort_array(aliasKeys, 1)) {
         alias = shell->query_alias(name);
-        output = sprintf("%-14s %-38s ", name, alias->template);
-        if (sizeof(alias->defaults) == 1 && alias->defaults[0] == "") {
-            output += "\n";
-        } else {
-            output += "$*: " + alias->defaults[0] + "\n";
+        list += ({ name, alias->template, });
+        if (sizeof(alias->defaults) == 1 && alias->defaults[0] != "") {
+            list += ({ "$*: " + alias->defaults[0], });
+        } else if (sizeof(alias->defaults) > 1) {
+            list += ({ "$0: " + alias->defaults[0], });
             for (int i = 1; i < sizeof(alias->defaults); i ++) {
-                output += sprintf("%54s$%d: %s\n", "", i, alias->defaults[i]);
+                // insert blank 1st & 2nd columns
+                list += ({ "", "", "$" + i + ": " + alias->defaults[i], });
             }
+        } else {
+            // insert blank 3rd column
+            list += ({ "", });
         }
-        write(output);
     }
-    write("\n" + sizeof(aliasKeys) + " aliases\n");
-    write("\n" + format_footer_bar() + "\n");
+
+    border = format_border(([
+        "title": "ALIAS",
+        "subtitle": "List",
+        "body": ([
+            "header": ({ "Alias", "Expansion", "Default", }),
+            "items": list,
+            "columns": 3,
+        ]),
+        "footer": ([
+            "items": ({
+                sizeof(aliasKeys) + " aliases",
+            }),
+            "columns": 1,
+            "align": "center",
+        ]),
+    ]));
+    foreach (string line in border) {
+        message("system", line + "\n", this_character());
+    }
 }
 
-void command (string input) {
+void command (string input, mapping flags) {
     if (!input) {
         print_all_aliases(this_user()->query_shell());
     } else {
