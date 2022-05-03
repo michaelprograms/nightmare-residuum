e87e72b6628540fbf48058c3dc35988a37c940a4
Michael Angeli <michael.programs@gmail.com>
2022-05-03 00:49:32 -0400

    update objects command to use format_border

--- lib/secure/cmd/immortal/objects.c
+++ lib/secure/cmd/immortal/objects.c
@@ -2,19 +2,18 @@ string *sort_instance (string *list) {
     return sort_array(list, (: explode($1, "#")[<1] > explode($2, "#")[<1] :));
 }
 string format_object (string path, string *children, int indent) {
-    string line = sprintf("%"+sprintf("%d", indent * 2)+"s", " ") + path + "\n";
+    string line = sprintf("%"+sprintf("%d", indent * 2)+"s", " ") + path;
     foreach (string c in sort_instance(children)) {
-        line += sprintf("%"+sprintf("%d", (indent + 1) * 2)+"s", " ") + c + "\n";
+        line += "\n" + sprintf("%"+sprintf("%d", (indent + 1) * 4)+"s", " ") + c;
     }
     return line;
 }
 
 void command (string input, mapping flags) {
     object *obs = filter_array(objects(), (:$1 && regexp(file_name($1), "#"):));
-    mapping structure = ([]);
+    mapping structure = ([ ]);
+    string *border, *list = ({ });
 
-    write(format_header_bar("OBJECTS", mud_name()) + "\n\n");
-    write("Spawned objects:\n");
     foreach (object ob in obs) {
         object parent;
         if (parent = ob->query_parent()) {
@@ -37,8 +36,23 @@ void command (string input, mapping flags) {
         }
     }
     foreach (string path in sort_instance(keys(structure))) {
-        write(format_object(path, structure[path], 0));
+        list += explode(format_object(path, structure[path], 0), "\n");
     }
 
-    write("\n" + format_footer_bar() + "\n");
+    border = format_border(([
+        "title": "SOCKETS",
+        "subtitle": mud_name(),
+        "body": ([
+            "items": list,
+            "columns": 1,
+        ]),
+        "footer": ([
+            "items": ({ sizeof(list) + " objects" }),
+            "columns": 1,
+            "align": "center",
+        ]),
+    ]));
+    foreach (string line in border) {
+        message("system", line + "\n", this_character());
+    }
 }
