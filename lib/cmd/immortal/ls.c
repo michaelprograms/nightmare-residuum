f4d7c69ec6e85d7761d0d4029c57883aacd1b300
Michael Angeli <michael.programs@gmail.com>
2022-05-12 20:23:51 -0400

    update ls command to display brief mode and add all files mode

--- lib/cmd/immortal/ls.c
+++ lib/cmd/immortal/ls.c
@@ -1,23 +1,57 @@
-private void output_long (string dir, string path) { // mixed *files) {
+private void output_long (string dir, mixed *files) {
     string acc, loaded, filename;
 
     acc = D_ACCESS->query_allowed(previous_object(), "read_file", dir, "read") ? "r" : "-";
     acc += D_ACCESS->query_allowed(previous_object(), "write_file", dir, "write") ? "w" : "-";
     acc += member_array(dir[0..<2], D_COMMAND->query_paths()) > -1 ? "x" : "-";
 
-    foreach (string file in get_dir(path, -1)) {
+    foreach (string file in files) {
         loaded = file[1] != -2 && find_object(dir + file[0]) ? "*" : "";
         filename = file[0] + (file[1] == -2 ? "/" : "");
-        write(sprintf("%:-3s%:-5s%:-30s %d\t%s", loaded, acc, ctime(file[2]), file[1], filename)+"\n");
+        message("system", sprintf("%:-3s%:-5s%:-25s %8d  %s", loaded, acc, ctime(file[2]), file[1], filename)+"\n", this_character());
+    }
+}
+private void output_brief (string dir, mixed *files) {
+    int widest = 0, w, width, cols, rows, max;
+    string *list = map(files, function (mixed *file, string dir) {
+        if (file[1] == -2) return "      " + file[0] + "/";
+        else {
+            string tmp = find_object(dir + file[0]) ? "*" : " ";
+            if (!file[1]) tmp += sprintf("   0 %s", file[0]);
+            else if (file[1] >= 1024) tmp += sprintf("%4d %s", file[1]/1024, file[0]);
+            else tmp += sprintf("  .%1d %s", to_int(file[1]/1024.0*10), file[0]);
+            return tmp;
+        }
+    }, dir);
+
+    width = this_user()->query_account()->query_setting("width") - 2;
+    foreach (string file in list) {
+        if ((w = strlen(file)) > widest) widest = w;
+    }
+    if (widest > width / 3 - 3) widest = width / 3 - 3;
+    cols = width / (widest + 3);
+    max = sizeof(list);
+    rows = max / cols;
+    if (max % cols) rows ++;
+
+    for (int i = 0; i < rows; i ++) {
+        string line = "";
+        for (int j = 0; j < cols; j ++) {
+            int index = (rows * j) + i;
+            if (index < max) line += sprintf("%:-"+(widest+3)+"s", list[index]);
+            else line += sprintf("%:-"+(widest+3)+"s", "");
+        }
+        message("action", line + "\n", this_character());
     }
 }
 
 void command (string input, mapping flags) {
     string path, dir;
+    mixed *files = ({ });
 
     if (!input || input == "") {
         path = this_user()->query_shell()->query_variable("cwd");
-    } else if (input[0] != '/') {
+    } else if (input[0] != '/' && input[0] != '~' && input[0] != '^') {
         path = this_user()->query_shell()->query_variable("cwd");
         path = sanitize_path(path + "/" + input);
     } else {
@@ -33,6 +67,15 @@ void command (string input, mapping flags) {
         dir = path[0..strsrch(path, "/", -1)];
     }
 
-    write(path + ":\n");
-    output_long(dir, path);
+    files = get_dir(path, -1);
+    if (!flags["a"]) {
+        files = filter(files, (: $1[0][0] != '.' :));
+    }
+
+    message("action", path + ":\n", this_character());
+    if (flags["l"]) {
+        output_long(dir, files);
+    } else {
+        output_brief(dir, files);
+    }
 }
