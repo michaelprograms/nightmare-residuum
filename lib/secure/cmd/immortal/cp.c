b1852d6ea387df663c722c0ffe15295e7e4a0bef
Michael Angeli <michael.programs@gmail.com>
2022-05-12 22:16:30 -0400

    add cp command finally

---
+++ lib/secure/cmd/immortal/cp.c
@@ -0,0 +1,17 @@
+void command (string input, mapping flags) {
+    string file1, file2, cwd;
+
+    if (!input || sscanf(input, "%s %s", file1, file2) != 2) {
+        write("Syntax: mv [file_location] [new_location]\n");
+        return;
+    }
+
+    cwd = this_user()->query_shell()->query_variable("cwd");
+    file1 = absolute_path(file1, cwd);
+    file2 = absolute_path(file2, cwd);
+    if (file_size(file2) == -2) {
+        file2 += "/" + explode(file1, "/")[<1];
+    }
+    cp(file1, file2);
+    write("cp " + (file_size(file2) > -1 ? "succeeded" : "failed") + ": " + file1 + " to "+ file2 + "\n");
+}
