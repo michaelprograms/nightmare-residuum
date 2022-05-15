720679c80ccd29e538caf24ecf7943984d64cfc6
Michael Angeli <michael.programs@gmail.com>
2022-05-15 01:49:46 -0400

    add new armor item with a bonus

---
+++ lib/domain/Start/human/armor/leather_coat.c
@@ -0,0 +1,13 @@
+inherit STD_ARMOR;
+
+void create () {
+    ::create();
+    set_name("leather coat");
+    set_id(({ "leather coat", "coat" }));
+    set_short("leather coat");
+    set_long("A flimsy leather coat.");
+    set_type("coat");
+    set_limbs(({ "torso", "left arm", "right arm" }));
+    set_ac(2);
+    set_bonus("endurance", 1);
+}
