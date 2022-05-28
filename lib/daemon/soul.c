12339c09a364297c42f0532a94f8ec121228a402
Michael Angeli <michael.programs@gmail.com>
2022-05-27 22:50:57 -0400

    finish D_SOUL up a bit

--- lib/daemon/soul.c
+++ lib/daemon/soul.c
@@ -1,23 +1,41 @@
 inherit M_SAVE;
 
 private mapping __Emotes = ([ ]);
+nosave private mapping __EmotesGenerated = ([ ]);
 private string *__Adverbs = ({ });
 
+void generate_emotes () {
+    string *list = ({ "smile", "frown", "cheer", "scream", "fingerguns", });
+
+    foreach (string emote in list) {
+        __EmotesGenerated[emote] = ([
+            "":"$N $v" + emote + ".",
+            "LIV":"$N $v" + emote + " at $t.",
+            "LVS":"$N $v" + emote + " at $O.",
+        ]);
+    }
+}
+
 void create () {
+    parse_init();
     set_save_path("/save/daemon/soul.o");
     restore_data();
 
-    parse_init();
+    generate_emotes();
 
-    foreach (string verb, mapping value in __Emotes) {
+    foreach (string verb, mapping value in __EmotesGenerated + __Emotes) {
         foreach (string rule, mixed ignore in value) {
             catch (parse_add_rule(verb, rule));
         }
     }
 }
 
+mapping query_emote (string verb) {
+    return __Emotes[verb] || __EmotesGenerated[verb];
+}
+
 string *query_emotes () {
-    return keys(__Emotes);
+    return keys(__Emotes) + keys(__EmotesGenerated);
 }
 string *query_adverbs () {
     return __Adverbs;
@@ -63,27 +81,26 @@ int remove_emote (string verb, string rule) {
     return 1;
 }
 
-// O/o:
-// T/t:
+// O/o: Object list
+// T/t: Objective Name?
 // N/n: You/Name
-// R/r:
+// R/r: Reflexive   your/him/her/them/it + self
 // V/v: pluralize word
-// P/p
-varargs string compose_message (object forwhom, string msg, object *who, mixed *obs...) {
+// P/p: Possessive
+varargs string compose_message (object forwhom, string msg, object *who, mixed *args...) {
     mixed *fmt;
     string res;
     int num, subj;
     string str;
     string bit;
     mapping has = ([ ]);
+    object *obs;
 
     fmt = reg_assoc(msg, ({ "\\$[OoTtNnRrVvPp][a-z0-9]*" }), ({ 1 }));
     fmt = fmt[0];
     res = fmt[0];
-    // write("fmt: "+identify(fmt)+"\n");
     for (int i = 1; i < sizeof(fmt); i += 2) {
         int c = fmt[i][1];
-        // write(identify(fmt[i][1..1])+"\n");
         if (fmt[i][2] && fmt[i][2]<'a') {
             if (fmt[i][3] && fmt[i][3] < 'a') {
                 subj = fmt[i][2] - '0';
@@ -96,23 +113,24 @@ varargs string compose_message (object forwhom, string msg, object *who, mixed *
             }
         } else {
             subj = 0;
-            num = ((c == 't' || c == 'T') ? 1 : 0); // target defaults to 1, not zero
+            num = (c == 't' || c == 'T') ? 1 : 0; // target defaults to 1, not zero
             str = fmt[i][2..<0];
         }
         switch (c) {
         case 'O':
         case 'o':
-            // @TODO
+            obs = args[num];
+            bit = conjunction(map(obs, (: $1 ? ($1->is_living() ? ($1 == $(forwhom) ? "you" : $1->query_cap_name()) : $1->query_name()) : 0 :)));
             break;
         case 'T':
         case 't':
             if (str == "") {
-                str = "o";
+                str = "o"; // default to objective
             }
         case 'N':
         case 'n':
             if (str == "") {
-                str = "s";
+                str = "s"; // default is subjective
             }
             if (str != "p") {
                 if (str != "d") {
@@ -136,12 +154,12 @@ varargs string compose_message (object forwhom, string msg, object *who, mixed *
                         break;
                     }
                     has[who[num]]++;
-                    bit = who[num]->query_cap_name(); // @TODO name vs short
+                    bit = who[num]->query_cap_name();
                     break;
                 }
             }
             has[who[num]]++;
-            bit = who[num]->query_cap_name(); // @TODO name vs short
+            bit = who[num]->query_cap_name();
             break;
         case 'R':
         case 'r':
@@ -149,12 +167,6 @@ varargs string compose_message (object forwhom, string msg, object *who, mixed *
             break;
         case 'V':
         case 'v':
-            // @TODO is this necessary for emotes? what uses contractions?
-            // Contractions
-            // if (i + 1 < sizeof(fmt) && fmt[i+1][0..2] == "'t ") {
-            //     str += "'t";
-            //     fmt[i+1] = fmt[i+1][2..];
-            // }
             if (num >= sizeof(who) || who[num] != forwhom) {
                 bit = pluralize(str);
             } else {
@@ -163,20 +175,7 @@ varargs string compose_message (object forwhom, string msg, object *who, mixed *
             break;
         case 'P':
         case 'p':
-            // //Bit of defensive coding here
-            // if (num>=sizeof(who)) {
-            //     bit = "WHOSE(?)";
-            //     break;
-            // } else if (forwhom == who[num]) {
-            //     bit = "your";
-            //     break;
-            // }
-            // if (has[who[num]]) {
-            //     bit = who[num]->query_possessive();
-            //     break;
-            // }
-            // bit = who[num]->query_named_possessive();
-            // has[who[num]]++;
+            // @ TODO
             break;
         }
         if (!bit) {
@@ -193,6 +192,10 @@ varargs string compose_message (object forwhom, string msg, object *who, mixed *
     return res;
 }
 
+int query_soul_exists (string verb) {
+    return !!query_emote(verb);
+}
+
 varargs mixed *get_soul (string verb, string rule, mixed *args) {
     mapping rules;
     mixed soul;
@@ -200,7 +203,7 @@ varargs mixed *get_soul (string verb, string rule, mixed *args) {
     object *who;
     string tmp;
 
-    if (!(rules = __Emotes[verb]) || !(soul = rules[rule])) {
+    if (!(rules = query_emote(verb)) || !(soul = rules[rule])) {
         return 0;
     }
 
@@ -208,14 +211,8 @@ varargs mixed *get_soul (string verb, string rule, mixed *args) {
         soul = rules[soul[1..]];
     }
     if (!stringp(soul)) {
-    //     if (soul[<1] != '\n') {
-    //         soul += "\n";
-    //     }
-    // } else {
         soul = map_array(soul, (: $1[<1] == '\n' ? $1 : $1 + "\n" :));
     }
-
-    // write("soul == "+identify(soul)+"\n");
 
     who = ({ this_character() });
     if (strsrch(rule, "LIV") != -1) {
@@ -223,26 +220,24 @@ varargs mixed *get_soul (string verb, string rule, mixed *args) {
         foreach (string token in explode(rule, " ")) {
             if (token == "LIV") {
                 who += ({ args[i] });
-                args[i..i] = ({});
+                args[i..i] = ({ }); // @TODO why? necessary?
+            } else if (token[0] >= 'A' && token[0] <= 'Z') {
+                i ++;
+            }
+        }
+    } else if (strsrch(rule, "LVS") != -1) {
+        int i = 0;
+        foreach (string token in explode(rule, " ")) {
+            if (token == "LVS") {
+                // @TODO
+                who += ({ args[0]... }) - ({ this_character() }); // + args[1];
+                i ++;
             } else if (token[0] >= 'A' && token[0] <= 'Z') {
                 i ++;
             }
         }
     }
-    // if (strsrch(rule, "LVS") != -1) {
-    //     int i = 0;
-    //     foreach (string token in explode(rule, " ")) {
-    //         if (token == "LVS") {
-    //             who += ({ args[i] });
-    //             args[i..i] = ({});
-    //             write("who is now: "+identify(who)+"\n");
-    //         } else if (token[0] >= 'A' && token[0] <= 'Z') {
-    //             i ++;
-    //         }
-    //     }
-    // }
     result = ({ who, allocate(sizeof(who) + 1) });
-    // write("result1: "+identify(result)+"\n");
 
     for (int i = 0; i < sizeof(who); i ++) {
         if (stringp(soul)) {
@@ -254,9 +249,11 @@ varargs mixed *get_soul (string verb, string rule, mixed *args) {
         }
         result[1][i] = compose_message(who[i], tmp, who, args...);
     }
-    // @TODO is this necessary
-    result[1][<1] = compose_message(0, stringp(soul) ? soul : soul[1], who, args...);
-    // write("result2: "+identify(result)+"\n");
+
+    if (!result[1][<1]) {
+        result[1][<1] = compose_message(0, stringp(soul) ? soul : soul[1], who, args...);
+    }
+
     return result;
 }
 
@@ -266,7 +263,7 @@ void display_soul (object *who, string *msgs, mixed others) {
     for (int i = 0; i < sizeof(who); i ++) {
         if (done[who[i]]) continue;
         done[who[i]] ++;
-        message("soul", who[i], msgs[i]);
+        message("soul", msgs[i], who[i]);
     }
     if (arrayp(others)) {
         message("soul", msgs[<1], others, who);
@@ -278,38 +275,55 @@ void display_soul (object *who, string *msgs, mixed others) {
 int livings_are_remote () {
     return 1;
 }
-
 mixed can_verb_wrd (string verb, string wrd) {
     return member_array(wrd, __Adverbs) != -1 || member_array('*', wrd) != -1;
 }
-mixed can_verb_rule (string verb, string rule) {
-    if (!__Emotes[verb]) return;
-    return !undefinedp(__Emotes[verb][rule]);
+mixed can_verb_rule (mixed args...) {
+    string verb, rule;
+    mapping emote;
+    if (sizeof(args) < 2) return;
+    verb = args[0];
+    rule = args[1];
+    if (!(emote = query_emote(verb))) return;
+    return !undefinedp(emote[rule]);
 }
 
-mixed direct_verb_rule (string verb, string rule) {
-    return !undefinedp(__Emotes[verb][rule]);
+mixed direct_verb_rule (mixed args...) {
+    string verb, rule;
+
+    if (sizeof(args) < 2) return;
+    verb = args[0];
+    rule = args[1];
+
+    if (rule == "LVS" && sizeof(args) > 2) {
+        object po = previous_object();
+        object who = args[2];
+        if (po == who || environment(po) != environment(who)) return 0;
+    }
+
+    return !undefinedp(query_emote(verb)[rule]);
 }
-mixed indirect_verb_rule (string verb, string rule) {
-    return !undefinedp(__Emotes[verb][rule]);
+mixed indirect_verb_rule (mixed args...) {
+    string verb, rule;
+    if (sizeof(args) < 2) return;
+    verb = args[0];
+    rule = args[1];
+    return !undefinedp(query_emote(verb)[rule]);
 }
 
-void do_verb_rule (string verb, string rule, mixed args...) {
+void do_verb_rule (mixed args...) {
+    string verb, rule;
     mixed soul;
 
-    // write("do_verb_rule: "+verb+" "+rule+" args: "+identify(args)+"\n");
-    soul = get_soul(verb, rule, args);
+    if (sizeof(args) < 2) return;
+    verb = args[0];
+    rule = args[1];
+
+    soul = get_soul(verb, rule, args[2..]);
 
     if (!soul) {
         return;
     }
-    // if (sizeof(soul[0] == 2 && environment(soul[0][1]) == environment(this_character())) {
-    //     soul[1][0] = "*" + soul[1][0];
-    //     soul[1][1] = "*" + soul[1][1];
-    //     inform(soul[0], soul[1], 0);
-    // } else {
-    // }
 
-    // write("Soul is: "+identify(soul)+"\n");
-    display_soul(soul[0], soul[1], environment(this_character())->query_living_contents());
+    display_soul(soul[0], soul[1], environment(this_character())->query_living_contents() - ({ this_character() }));
 }
