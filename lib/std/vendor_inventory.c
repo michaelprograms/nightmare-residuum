46a2323136ee68d837515e344ac392e0f095c229
Michael Angeli <michael.programs@gmail.com>
2022-06-05 23:26:59 -0400

    add M_RESET to vendor_inventory

--- lib/std/vendor_inventory.c
+++ lib/std/vendor_inventory.c
@@ -1,6 +1,7 @@
 inherit M_CLEAN;
 inherit M_CONTAINER;
 inherit M_PARENT;
+inherit M_RESET;
 
 nosave private int __MaxItems = 0;
 
@@ -16,4 +17,9 @@ int handle_receive (object ob) {
     if (!ob->is_item()) return 0;
     if (__MaxItems > 0 && sizeof(all_inventory()) >= __MaxItems) return 0;
     return ::handle_receive(ob);
+}
+
+void create () {
+    parent::create();
+    reset::create();
 }
