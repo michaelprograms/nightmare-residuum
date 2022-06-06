f211e01d5ddf821475ddaaedec79e605d8b40cd0
Michael Angeli <michael.programs@gmail.com>
2022-06-05 23:05:59 -0400

    update vendor_inventory to have max items and add tests

--- lib/std/vendor_inventory.c
+++ lib/std/vendor_inventory.c
@@ -1,3 +1,19 @@
 inherit M_CLEAN;
 inherit M_CONTAINER;
 inherit M_PARENT;
+
+nosave private int __MaxItems = 0;
+
+int query_max_items () {
+    return __MaxItems;
+}
+void set_max_items (int n) {
+    if (!intp(n)) error("Bad argument 1 to vendor_inventory->set_max_items");
+    __MaxItems = n;
+}
+
+int handle_receive (object ob) {
+    if (!ob->is_item()) return 0;
+    if (__MaxItems > 0 && sizeof(all_inventory()) >= __MaxItems) return 0;
+    return ::handle_receive(ob);
+}
--- lib/std/vendor_inventory.test.c
+++ lib/std/vendor_inventory.test.c
@@ -9,3 +9,61 @@ void before_each_test () {
 void after_each_test () {
     if (objectp(testOb)) destruct(testOb);
 }
+
+void test_receive () {
+    object living, item;
+
+    expect_function("handle_receive", testOb);
+
+    // create test items
+    living = new(STD_LIVING);
+    item = new(STD_ITEM);
+
+    expect("inventory receives only items", (: ({
+        assert(testOb->handle_receive($(living)), "==", 0),
+        assert(testOb->handle_receive($(item)), "==", 1),
+    }) :));
+
+    if (living) destruct(living);
+    if (item) destruct(item);
+}
+
+void test_max_items () {
+    object item1, item2;
+
+    expect_function("query_max_items", testOb);
+    expect_function("set_max_items", testOb);
+
+    // create test items
+    item1 = new(STD_ITEM);
+    item2 = new(STD_ITEM);
+
+    expect("max items prevents receiving items", (: ({
+        // set max items to 1
+        testOb->set_max_items(1),
+        assert(testOb->query_max_items(), "==", 1),
+        // 0 of 1 items, receive passes
+        assert(testOb->handle_receive($(item1)), "==", 1),
+        // move item to inventory to count
+        assert($(item1)->handle_move(testOb), "==", 1),
+        // 1 of 1 items, receive fails
+        assert(testOb->handle_receive($(item2)), "==", 0),
+        // set max items to 2
+        testOb->set_max_items(2),
+        assert(testOb->query_max_items(), "==", 2),
+        // 1 of 2 items, receive passes
+        assert(testOb->handle_receive($(item2)), "==", 1),
+    }) :));
+
+    expect("set_max_items handles invalid argument 1", (: ({
+        assert((: testOb->set_max_items(this_object()) :), "catch", "*Bad argument 1 to vendor_inventory->set_max_items\n"),
+        assert((: testOb->set_max_items("") :), "catch", "*Bad argument 1 to vendor_inventory->set_max_items\n"),
+        assert((: testOb->set_max_items(1.0) :), "catch", "*Bad argument 1 to vendor_inventory->set_max_items\n"),
+        assert((: testOb->set_max_items(({})) :), "catch", "*Bad argument 1 to vendor_inventory->set_max_items\n"),
+        assert((: testOb->set_max_items(([])) :), "catch", "*Bad argument 1 to vendor_inventory->set_max_items\n"),
+        assert((: testOb->set_max_items((: 1 :)) :), "catch", "*Bad argument 1 to vendor_inventory->set_max_items\n"),
+    }) :));
+
+    if (item1) destruct(item1);
+    if (item2) destruct(item2);
+}
