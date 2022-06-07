c13fef9618711bcd67e2d586d9caabfe456352e1
Michael Angeli <michael.programs@gmail.com>
2022-06-06 20:09:48 -0400

    update human start area clothing shop

--- lib/domain/Start/human/room/market_st_e1.c
+++ lib/domain/Start/human/room/market_st_e1.c
@@ -5,7 +5,7 @@ inherit STD_ROOM;
 void create () {
     ::create();
     set_short("a village street");
-    set_long("A small street traveling north and south through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. A public square can be seen to the west. A framework of hexagons glimmer overhead.");
+    set_long("A small street traveling north and south through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. A building with a glass window is on the south side of the street. A public square can be seen to the west. A framework of hexagons glimmer overhead.");
     set_exits(([
         "east": HUMAN_ROOM + "market_st_e2.c",
         "west": HUMAN_ROOM + "square.c",
--- lib/domain/Start/human/room/store_clothing.c
+++ lib/domain/Start/human/room/store_clothing.c
@@ -8,14 +8,15 @@ void create () {
         "no attack": 1,
     ]));
     set_short("a village bank");
-    set_long("The inside of a brick building that consists of an open room. There is a large glass window to the north facing the street with a door to the side and several mannequins wearing examples of the merchandise. Behind the counter are several racks of clothes.");
+    set_long("The inside of a brick building that consists of an open room. There is a glass window to the north facing the street with a door to the side and several mannequins wearing examples of the merchandise. Behind the counter are several racks of clothes.");
     set_exits(([
-        "out north": HUMAN_ROOM "market_st_e1.c",
+        "out north": HUMAN_ROOM + "market_st_e1.c",
     ]));
     set_looks(([
         "mannequin": "Wooden dolls as large as a human, showing clothing examples that the store sells.",
         "racks": "Racks containing the clothing the merchant is selling.",
         "counter": "A wooden counter spanning the room and bisecting it nearly in the middle.",
+        "window": "Mannequins are placed in the window to display some clothes.",
     ]));
     set_reset(([
         HUMAN_NPC + "vendor_clothing.c": 1,
