c55e517317f45aab6a296342fb61d6fe00b4e3bc
Michael Angeli <michael.programs@gmail.com>
2022-06-13 20:06:27 -0400

    add start of village pub

--- lib/domain/Start/human/room/broad_st_s1.c
+++ lib/domain/Start/human/room/broad_st_s1.c
@@ -5,10 +5,11 @@ inherit STD_ROOM;
 void create () {
     ::create();
     set_short("a village street");
-    set_long("A small street traveling north and south through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. A public square can be seen to the north. A framework of hexagons glimmer overhead.");
+    set_long("A small street traveling north and south through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. A building on the west side of the street has benches set up and large open doors. A public square can be seen to the north. A framework of hexagons glimmer overhead.");
     set_exits(([
         "north": HUMAN_ROOM + "square.c",
         "south": HUMAN_ROOM + "broad_st_s2.c",
+        "enter west": HUMAN_ROOM + "store_pub.c",
     ]));
     set_looks(([
         "street": "A village street labeled S Broad St.",
