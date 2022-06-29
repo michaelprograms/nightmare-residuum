aad99a3d5abaafd074be7d13602b8ff7dac3a2b2
Michael Angeli <michael.programs@gmail.com>
2022-06-28 23:19:05 -0400

    update verb/say for synthetic species

--- lib/cmd/verb/say.c
+++ lib/cmd/verb/say.c
@@ -12,13 +12,16 @@ void do_say () {
 
 int can_say_str (mixed args...) { return 1; }
 void do_say_str (mixed args...) {
+    object po = previous_object();
     string str = args[0];
     string verb;
 
     switch (str[<1]) {
         case '?': verb = "ask"; break;
         case '!': verb = "exclaim"; break;
-        default: verb = "say";
+        default:
+            if (po->query_species() == "synthetic") verb = "synthesize";
+            else verb = "say";
     }
 
     message("say", "You "+verb+": "+str+"\n", previous_object());
