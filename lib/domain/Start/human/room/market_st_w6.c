331eb94c91799117e5c51eee1eb7709ef36d4742
Michael Angeli <michael.programs@gmail.com>
2022-07-05 20:08:24 -0400

    add more human start rooms

---
+++ lib/domain/Start/human/room/market_st_w6.c
@@ -0,0 +1,16 @@
+#include "human.h"
+
+inherit STD_ROOM;
+
+void create () {
+    ::create();
+    set_short("a village street");
+    set_long("A small street traveling north and south through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. A framework of hexagons glimmer overhead.");
+    set_exits(([
+        "west": HUMAN_ROOM + "market_st_w7.c",
+        "east": HUMAN_ROOM + "market_st_w5.c",
+    ]));
+    set_looks(([
+        "street": "A village street labeled W Market St.",
+    ]));
+}
