1e3a4c012aa55cca2eb9f69eaa676650fbb0aef3
Michael Angeli <michael.programs@gmail.com>
2022-07-09 21:05:03 -0400

    adjust how D_CLASS formats class data

--- lib/daemon/class.c
+++ lib/daemon/class.c
@@ -1,88 +1,146 @@
 nosave private mapping __Class = ([
     "warrior": ([       // str
-        "hp": 3,
-        "sp": 1,
-        "mp": 0,
-        "strength": 3,
-        "perception": 1,
-        "endurance": 2,
-        "charisma": 0,
-        "intelligence": 0,
-        "agility": 2,
-        "luck": 0,
+        "vitals": ([
+            "hp": 3,
+            "sp": 1,
+            "mp": 0,
+        ]),
+        "stats": ([
+            "strength": 3,
+            "perception": 1,
+            "endurance": 2,
+            "charisma": 0,
+            "intelligence": 0,
+            "agility": 2,
+            "luck": 0,
+        ]),
+        "skills": ([
+
+        ]),
     ]),
     "templar": ([       // str and agi
-        "hp": 2,
-        "sp": 2,
-        "mp": 0,
-        "strength": 2,
-        "perception": 1,
-        "endurance": 2,
-        "charisma": 1,
-        "intelligence": 0,
-        "agility": 2,
-        "luck": 0,
+        "vitals": ([
+            "hp": 2,
+            "sp": 2,
+            "mp": 0,
+        ]),
+        "stats": ([
+            "strength": 2,
+            "perception": 1,
+            "endurance": 2,
+            "charisma": 1,
+            "intelligence": 0,
+            "agility": 2,
+            "luck": 0,
+        ]),
+        "skills": ([
+
+        ]),
     ]),
     "scoundrel": ([     // agi
-        "hp": 1,
-        "sp": 3,
-        "mp": 0,
-        "strength": 2,
-        "perception": 1,
-        "endurance": 1,
-        "charisma": 1,
-        "intelligence": 0,
-        "agility": 3,
-        "luck": 0,
+        "vitals": ([
+            "hp": 1,
+            "sp": 3,
+            "mp": 0,
+        ]),
+        "stats": ([
+            "strength": 2,
+            "perception": 1,
+            "endurance": 1,
+            "charisma": 1,
+            "intelligence": 0,
+            "agility": 3,
+            "luck": 0,
+        ]),
+        "skills": ([
+
+        ]),
     ]),
     "ranger": ([        // agi and int
-        "hp": 0,
-        "sp": 2,
-        "mp": 2,
-        "strength": 1,
-        "perception": 1,
-        "endurance": 1,
-        "charisma": 1,
-        "intelligence": 2,
-        "agility": 2,
-        "luck": 0,
+        "vitals": ([
+            "hp": 0,
+            "sp": 2,
+            "mp": 2,
+        ]),
+        "stats": ([
+            "strength": 1,
+            "perception": 1,
+            "endurance": 1,
+            "charisma": 1,
+            "intelligence": 2,
+            "agility": 2,
+            "luck": 0,
+        ]),
+        "skills": ([
+
+        ]),
     ]),
     "mentalist": ([     // int
-        "hp": 0,
-        "sp": 1,
-        "mp": 3,
-        "strength": 0,
-        "perception": 2,
-        "endurance": 1,
-        "charisma": 1,
-        "intelligence": 3,
-        "agility": 0,
-        "luck": 1,
+        "vitals": ([
+            "hp": 0,
+            "sp": 1,
+            "mp": 3,
+        ]),
+        "stats": ([
+            "strength": 0,
+            "perception": 2,
+            "endurance": 1,
+            "charisma": 1,
+            "intelligence": 3,
+            "agility": 0,
+            "luck": 1,
+        ]),
+        "skills": ([
+
+        ]),
     ]),
     "paladin": ([       // int and str
-        "hp": 2,
-        "sp": 0,
-        "mp": 2,
-        "strength": 2,
-        "perception": 1,
-        "endurance": 2,
-        "charisma": 1,
-        "intelligence": 2,
-        "agility": 0,
-        "luck": 0,
+        "vitals": ([
+            "hp": 2,
+            "sp": 0,
+            "mp": 2,
+        ]),
+        "stats": ([
+            "strength": 2,
+            "perception": 1,
+            "endurance": 2,
+            "charisma": 1,
+            "intelligence": 2,
+            "agility": 0,
+            "luck": 0,
+        ]),
+        "skills": ([
+
+        ]),
     ]),
 ]);
 
 int query_adjust_hp (string c) {
-    return __Class[c] ? __Class[c]["hp"] : 0;
+    if (__Class[c] && __Class[c]["vitals"]) {
+        return __Class[c]["vitals"]["hp"];
+    } else {
+        return 0;
+    }
 }
 int query_adjust_sp (string c) {
-    return __Class[c] ? __Class[c]["sp"] : 0;
+    if (__Class[c] && __Class[c]["vitals"]) {
+        return __Class[c]["vitals"]["sp"];
+    } else {
+        return 0;
+    }
 }
 int query_adjust_mp (string c) {
-    return __Class[c] ? __Class[c]["mp"] : 0;
+    if (__Class[c] && __Class[c]["vitals"]) {
+        return __Class[c]["vitals"]["mp"];
+    } else {
+        return 0;
+    }
 }
 
 int query_adjust_stat (string c, string stat) {
-    return __Class[c] ? __Class[c][stat] : 0;
+    if (__Class[c] && __Class[c]["stats"]) {
+        return __Class[c]["stats"][stat];
+    } else {
+        return 0;
+    }
 }
