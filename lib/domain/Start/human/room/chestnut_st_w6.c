8147ec0581755ed3923a97f9bfa7ba93596c7632
Michael Angeli <michael.programs@gmail.com>
2022-07-20 19:21:31 -0400

    fix a typo in some rooms

--- lib/domain/Start/human/room/chestnut_st_w6.c
+++ lib/domain/Start/human/room/chestnut_st_w6.c
@@ -5,7 +5,7 @@ inherit STD_ROOM;
 void create () {
     ::create();
     set_short("a village street");
-    set_long("A small street traveling east through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. A framework of hexagons glimmer overhead. A concrete wall at the base of the hexagon dome stands blocks further passage west.");
+    set_long("A small street traveling east through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. A framework of hexagons glimmer overhead. A concrete wall at the base of the hexagon dome blocks further passage west.");
     set_exits(([
         "east": HUMAN_ROOM + "chestnut_st_w5.c",
     ]));
--- lib/domain/Start/human/room/spruce_st_s6.c
+++ lib/domain/Start/human/room/spruce_st_s6.c
@@ -5,7 +5,7 @@ inherit STD_ROOM;
 void create () {
     ::create();
     set_short("a village street");
-    set_long("A small street traveling north through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. A framework of hexagons glimmer overhead. A concrete wall at the base of the hexagon dome stands blocks further passage south.");
+    set_long("A small street traveling north through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. A framework of hexagons glimmer overhead. A concrete wall at the base of the hexagon dome blocks further passage south.");
     set_exits(([
         "north": HUMAN_ROOM + "spruce_st_s5.c",
     ]));
