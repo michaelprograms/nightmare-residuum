ff954172b5227630c40a779a8d19ebb1ee0fce4c
Michael Angeli <michael.programs@gmail.com>
2022-07-21 19:16:17 -0400

    add plasma snail monster

---
+++ lib/domain/Start/human/monster/plasma_snail.c
@@ -0,0 +1,18 @@
+inherit STD_MONSTER;
+
+#define PLASMA_COLORS ({"%^BLUE%^BOLD%^", "%^BLUE%^", "%^BLUE%^BOLD%^", "%^CYAN%^BOLD%^", "%^CYAN%^", "%^GREEN%^BOLD%^", })
+
+void create() {
+    string plasma = "";
+    ::create();
+    foreach (string s in ({ "p", "l", "a", "s", "m", "a" })) {
+        plasma += element_of(PLASMA_COLORS) + s;
+    }
+    plasma = "%^RESET" + plasma + "%^DEFAULT%^";
+    set_id(({ "snail", "plasma snail" }));
+    set_name("plasma snail");
+    set_short(plasma + " snail");
+    set_long("A snail swirling with a colorful array of " + plasma + ".");
+    set_level(3);
+    set_species("snail");
+}
