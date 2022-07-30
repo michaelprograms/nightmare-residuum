da89e0149042783b6688730af5071d7d8a59c68c
Michael Angeli <michael.programs@gmail.com>
2022-07-30 17:42:58 -0400

    create a fountain and add bench to parks

---
+++ lib/domain/Start/human/item/fountain.c
@@ -0,0 +1,11 @@
+inherit STD_ITEM;
+
+void create () {
+    ::create();
+    set_name("fountain");
+    set_id(({ "fountain", "basin", "fountain basin", }));
+    set_adjective(({ "wide" }));
+    set_short("a wide %^CYAN%^fountain basin%^DEFAULT%^");
+    set_long("A fountain with a wide basin made of a hardened clay. A metallic winged figure stands upon a small pedestal in the center of the basin with water gently spraying out of its mouth.");
+    set_no_get(1);
+}
--- lib/domain/Start/human/room/park_southwest.c
+++ lib/domain/Start/human/room/park_southwest.c
@@ -16,6 +16,8 @@ void create () {
 
     ]));
     set_reset(([
+        HUMAN_ITEM + "bench.c": 1,
+        HUMAN_ITEM + "fountain.c": 1,
         HUMAN_MONSTER + "ant.c": !random(3),
         HUMAN_MONSTER + "bee.c": !random(3),
         HUMAN_MONSTER + "beetle.c": !random(3),
