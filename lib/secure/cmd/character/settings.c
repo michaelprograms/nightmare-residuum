70cb75a2b4fb38a448fce4189a63b692d4c12320
Michael Angeli <michael.programs@gmail.com>
2022-08-04 22:09:09 -0400

    update settings command to allow changing settings

--- lib/secure/cmd/character/settings.c
+++ lib/secure/cmd/character/settings.c
@@ -1,15 +1,41 @@
 void command (string input, mapping flags) {
-    object tc = this_character(), target = tc;
-    string *border;
-    object account;
-    string *list = ({ });
+    object tc = this_character(), target = tc, account;
+    string *border, *list = ({ });
+    mapping header;
 
     if (input && tc->query_immortal()) {
-        if (find_character(input)) target = find_character(input);
-        else if(present(input, environment(tc))) target = present(input, environment(tc));
+        if (find_character(input)) {
+            target = find_character(input);
+            input = 0;
+        }
     }
 
     account = target->query_user()->query_account();
+
+    if (input) {
+        string key, *split;
+        mixed value;
+
+        split = explode(input, " ");
+        key = split[0];
+        value = implode(split[1..], " ");
+
+        switch (key) {
+            case "width":
+                value = to_int(value);
+                break;
+        }
+        header = ([
+            "columns": 1,
+            "align": "center",
+        ]);
+        account->set_setting(key, value);
+        if (account->query_setting(key) == value) {
+            header["items"] = ({ "Setting %^BOLD%^" + key + "%^BOLD_OFF%^ to %^BOLD%^" + value + "%^BOLD_OFF%^" });
+        } else {
+            header["items"] = ({ "Invalid setting: " + key });
+        }
+    }
 
     foreach (string key,mixed value in account->query_settings()) {
         list += ({ sprintf("%16s : %s", key, ""+value) });
@@ -18,6 +44,7 @@ void command (string input, mapping flags) {
     border = format_border(([
         "title": "SETTINGS",
         "subtitle": target->query_cap_name(),
+        "header": header,
         "body": ([
             "items": list,
             "columns": 1,
