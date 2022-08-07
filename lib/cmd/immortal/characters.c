a1a809f3d62e5c7bad3c1490267f9ef102981f3b
Michael Angeli <michael.programs@gmail.com>
2022-08-06 23:20:48 -0400

    add characters command

---
+++ lib/cmd/immortal/characters.c
@@ -0,0 +1,31 @@
+void command (string input, mapping flags) {
+    mapping data = ([
+        "title": "CHARACTERS",
+        "subtitle": mud_name(),
+    ]);
+    object tc = this_character();
+    string *border, *bodyItems = ({ });
+    object *characters = characters();
+
+    foreach (object char in characters) {
+        bodyItems += ({
+            char->query_cap_name(),
+            file_name(environment(char)),
+        });
+    }
+
+    data["body"] = sizeof(bodyItems) ? ([
+        "items": bodyItems,
+        "columns": 3,
+        "align": "left",
+    ]) : ([
+        "items": ({ "No player characters connected" }),
+        "columns": 1,
+        "align": "center",
+    ]);
+
+    border = format_border(data);
+    foreach (string line in border) {
+        message("system", line + "\n", tc);
+    }
+}
