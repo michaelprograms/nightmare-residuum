49013e1245bc307060267bb111e8435ff061bba5
Michael Angeli <michael.programs@gmail.com>
2022-08-08 22:26:01 -0400

    cleanup descriptions in paladin hall

--- lib/domain/Start/human/room/hall_paladin.c
+++ lib/domain/Start/human/room/hall_paladin.c
@@ -8,7 +8,7 @@ void create () {
         "no attack": 1,
     ]));
     set_short("a paladin's hall");
-    set_long("The basement floor of a brick building that consists of the personal training area of the paladins. It is a sparsely populated area where the paladins train in an ascetic lifestyle. Wooden floorboards overhead form a crude barrier between the first floor. A staircase leads back down to the first floor.");
+    set_long("The basement floor of a brick building that consists of the personal training area of the paladins. It is a sparsely populated area where the paladins train in an ascetic lifestyle. Wooden floorboards overhead form a crude barrier between the first floor. A staircase leads back up to the first floor.");
     set_smell("default", "The air is damp and cool.");
     set_listen("default", "Floorboards quietly creak from footsteps above.");
     set_exits(([
--- lib/domain/Start/human/room/intro_paladin.c
+++ lib/domain/Start/human/room/intro_paladin.c
@@ -8,7 +8,7 @@ void create () {
         "no attack": 1,
     ]));
     set_short("a paladin's hall");
-    set_long("The inside of a wide brick building that consists of a tall, ornate room. There is an open door to the east facing the street. Along the walls are stained glass windows consisting of paladins in various combat poses while the ceiling is arched high above. Several pews are positioned before stations that are used for prayer. A staircase leads down to the second floor.");
+    set_long("The inside of a wide brick building that consists of a tall, ornate room. There is an open door to the east facing the street. Along the walls are stained glass windows consisting of paladins in various combat poses while the ceiling is arched high above. Several pews are positioned before stations that are used for prayer. A staircase leads down to the basement.");
     set_listen("default", "Several paladins occasionally break into chant from the pews.");
     set_smell("default", "The fragrance of incense lightly hangs in the air.");
     set_exits(([
@@ -18,7 +18,7 @@ void create () {
     set_looks(([
         ({ "pews", "stations" }): "Wooden pews used by the paladins to conduct prayer, along with stations used by a paladin to lead others through prayer.",
         "ceiling": "The high ceilings arch above with strong wooden beams holding it the roof in place.",
-        "windows": "The windows are spaced along the side walls and reach towards the base of the arched ceiling and consist of paladins posed in various positions.",
+        "windows": "The windows are spaced along the side walls and reach towards the base of the arched ceiling. They consist of paladins pictured in various positions.",
         "door": "The front of the building consists of a door that swings open to the street, but no window.",
         "stairs": "The wooden staircase leads down to the basement of the building, where the paladins like to dwell to remind them of their duty.",
     ]));
