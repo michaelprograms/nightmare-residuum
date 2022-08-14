09ccd22239f318e4c92b4cd909fe4e676d4aae57
Michael Angeli <michael.programs@gmail.com>
2022-08-13 22:52:52 -0400

    update full command to allow healing of others

--- lib/cmd/immortal/full.c
+++ lib/cmd/immortal/full.c
@@ -1,7 +1,16 @@
 void command (string input, mapping flags) {
-    object tc = this_character();
-    tc->set_hp(tc->query_max_hp());
-    tc->set_sp(tc->query_max_sp());
-    tc->set_mp(tc->query_max_mp());
-    write("Health vitals returned to full.\n");
+    object tc = this_character(), target = tc;
+
+    if (input && tc->query_immortal()) {
+        if (find_character(input)) target = find_character(input);
+        else if(present(input, environment(tc))) target = present(input, environment(tc));
+    }
+
+    target->set_hp(target->query_max_hp());
+    target->set_sp(target->query_max_sp());
+    target->set_mp(target->query_max_mp());
+    message("action", "Vitals returned to full.\n", tc);
+    if (target != tc) {
+        message("action", tc->query_cap_name() + " returns your vitals to full.\n", target);
+    }
 }
