d463f1903b23533870d94368b2bb8ccfb0710406
Michael Angeli <michael.programs@gmail.com>
2022-08-13 22:53:03 -0400

    fix a grammatical mistake in suspicious teen

--- lib/domain/Start/human/npc/teen_suspicious.c
+++ lib/domain/Start/human/npc/teen_suspicious.c
@@ -8,7 +8,7 @@ void create () {
     set_name("teen");
     set_short("a suspicious teen");
     set_gender(element_of(({ "male", "female" })));
-    set_long("A natural born teenager who stares hard back at you, with a fierce attitude set upon " + possessive() + " face.");
+    set_long("A natural born teenager who stares hard back at you, with a fierce attitude set upon " + possessive(this_object()) + " face.");
     set_level(5);
     set_species("human");
 
