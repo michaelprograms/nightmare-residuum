c7c626a9d3d3ea91be5ed45abf802dbc203dffa4
Michael Angeli <michael.programs@gmail.com>
2022-08-26 22:04:14 -0400

    update plasma snail short

--- lib/domain/Start/human/monster/plasma_snail.c
+++ lib/domain/Start/human/monster/plasma_snail.c
@@ -1,18 +1,18 @@
 inherit STD_MONSTER;
 
-#define PLASMA_COLORS ({"%^BLUE%^BOLD%^", "%^BLUE%^BOLD%^", "%^BLUE%^BOLD%^", "%^CYAN%^BOLD%^", "%^GREEN%^BOLD%^", })
+#define PLASMA_COLORS ({ "%^BLUE%^", "%^BLUE%^", "%^BLUE%^", "%^WHITE%^", "%^CYAN%^", "%^GREEN%^" })
 
 void create() {
-    string plasma = "";
-    ::create();
-    foreach (string s in ({ "p", "l", "a", "s", "m", "a" })) {
-        plasma += element_of(PLASMA_COLORS) + s;
+    string plasmaSnail = "%^RESET%^BOLD%^";
+    foreach (string s in ({"p","l","a","s","m","a"," ","s","n","a","i","l"})) {
+        plasmaSnail += element_of(PLASMA_COLORS) + s;
     }
-    plasma = "%^RESET" + plasma + "%^DEFAULT%^";
+    plasmaSnail += "%^DEFAULT%^";
+    ::create();
     set_id(({ "snail", "plasma snail" }));
     set_name("plasma snail");
-    set_short("a " + plasma + " snail");
-    set_long("A snail swirling with a colorful array of " + plasma + ".");
+    set_short("a " + plasmaSnail);
+    set_long("A snail swirling with a colorful array of plasma.");
     set_level(3);
     set_species("snail");
     set_ability_list(({ "zap" }));
