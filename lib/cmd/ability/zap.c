d4d08ec45ed81d897a3bec8d779ed27836e40877
Michael Angeli <michael.programs@gmail.com>
2022-09-06 21:44:17 -0400

    update zap ability

--- lib/cmd/ability/zap.c
+++ lib/cmd/ability/zap.c
@@ -5,8 +5,7 @@ inherit STD_ABILITY;
 void create () {
     ::create();
     set_skill_powers(([
-        "psionic": 8,
-        "ranged": 2,
+        "psionic": 10,
     ]));
     set_help_text("Zap your opponent with your mind.");
 }
