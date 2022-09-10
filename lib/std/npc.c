aec7096cffab987f6090aae242aaf1c0670ccc06
Michael Angeli <michael.programs@gmail.com>
2022-09-09 20:55:30 -0400

    update npc & monster skills

--- lib/std/npc.c
+++ lib/std/npc.c
@@ -20,20 +20,20 @@ void set_level (int l) {
     set_stat("agility", l * 5);
     set_stat("luck", to_int(l * 2.5) + random(l + 1));
 
-    set_skill("blade attack", l * 3);
-    set_skill("blade defense", l * 2);
-    set_skill("blunt attack", l * 3);
-    set_skill("blunt defense", l * 2);
-    set_skill("knife attack", l * 3);
-    set_skill("knife defense", l * 2);
-    set_skill("melee attack", l * 3);
-    set_skill("melee defense", l * 2);
-    set_skill("psionic attack", l * 3);
-    set_skill("psionic defense", l * 2);
-    set_skill("projectile attack", l * 3);
-    set_skill("projectile defense", l * 2);
-    set_skill("ranged attack", l * 3);
-    set_skill("ranged defense", l * 2);
+    set_skill("blade attack", (l+1) * 4);
+    set_skill("blade defense", (l+1) * 3);
+    set_skill("blunt attack", (l+1) * 4);
+    set_skill("blunt defense", (l+1) * 3);
+    set_skill("knife attack", (l+1) * 4);
+    set_skill("knife defense", (l+1) * 3);
+    set_skill("melee attack", (l+1) * 4);
+    set_skill("melee defense", (l+1) * 3);
+    set_skill("psionic attack", (l+1) * 4);
+    set_skill("psionic defense", (l+1) * 3);
+    set_skill("projectile attack", (l+1) * 4);
+    set_skill("projectile defense", (l+1) * 3);
+    set_skill("ranged attack", (l+1) * 4);
+    set_skill("ranged defense", (l+1) * 3);
 
     ::set_level(l);
 }
