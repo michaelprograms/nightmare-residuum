dc9e932377f0c3ddeb4adc012365fc15bee3bb8e
Michael Angeli <michael.programs@gmail.com>
2022-09-12 23:16:04 -0400

    update inventory command to show autoload status

--- lib/cmd/character/inventory.c
+++ lib/cmd/character/inventory.c
@@ -1,24 +1,82 @@
+mapping process_section (object *items, string label, object target) {
+    mapping section;
+
+    if (sizeof(items)) {
+        section = ([
+            "header": ({ label, "Autoload", }),
+            "items": ({ }),
+            "columns": 2,
+        ]);
+        foreach (object *obs in unique_array(items, (: $1->query_short() :))) {
+            string autoload;
+            int a = sizeof(filter(obs->query_autoload(target), (: $1 :)));
+
+            if (sizeof(obs) == 1) {
+                autoload = (a ? "Yes" : "No");
+            } else {
+                autoload = (a == sizeof(obs) ? "All" : (a ? "Some" : "None"));
+            }
+
+            section["items"] += ({
+                consolidate(sizeof(obs), obs[0]->query_short()),
+                autoload,
+            });
+        }
+    }
+
+    return section;
+}
+
+mapping *process_inventory (object target) {
+    object *weapon = ({ }), *armor = ({ }), *items = ({ });
+    mapping *inventory = ({ });
+
+    foreach (object item in target->query_item_contents()) {
+        if (item->is_weapon()) {
+            weapon += ({ item });
+        } else if (item->is_armor()) {
+            armor += ({ item });
+        } else {
+            items += ({ item });
+        }
+    }
+
+    if (sizeof(weapon)) {
+        inventory += ({ process_section(weapon, "Weapons", target) });
+    }
+    if (sizeof(armor)) {
+        inventory += ({ process_section(armor, "Weapons", target) });
+    }
+    if (sizeof(items)) {
+        inventory += ({ process_section(items, "Items", target) });
+    }
+
+    if (!sizeof(inventory)) {
+        inventory += ({
+            ([
+                "items": ({ "You are not carrying any items." }),
+                "columns": 1,
+                "align": "center",
+            ])
+        });
+    }
+
+    return inventory;
+}
+
 void command (string input, mapping flags) {
     object tc = this_character(), target = tc;
-    object *inv;
-    string *coins = ({ }), *items = ({ });
     string *border;
+    mapping *items = ({ });
     mapping footer;
+    string *coins = ({ });
 
     if (input && tc->query_immortal()) {
         if (find_character(input)) target = find_character(input);
         else if(present(input, environment(tc))) target = present(input, environment(tc));
     }
 
-    inv = unique_array(all_inventory(target), (: $1->query_short("%^RESET%^") :));
-    if (sizeof(inv)) {
-        string *shorts = map(sort_array(map(inv, (: capitalize(consolidate(sizeof($1), $1[0]->query_short("%^RESET%^"))) :)), 1), (: $1 :));
-        foreach (string short in shorts) {
-            items += ({ short });
-        }
-    } else {
-        items = ({ "You have no items." });
-    }
+    items = process_inventory(target);
 
     foreach (string currency in target->query_currencies()) {
         int n;
@@ -26,26 +84,22 @@ void command (string input, mapping flags) {
             coins += ({ n + " " + currency });
         }
     }
+    footer = ([
+        "columns": 1,
+        "align": "center",
+    ]);
     if (sizeof(coins)) {
-        footer = ([
-            "header": "Currenc" + (sizeof(coins) > 1 ? "ies" : "y"),
-            "items": ({
-                 conjunction(coins),
-            }),
-            "columns": 1,
-            "align": "center",
-        ]);
+        footer["header"] = "Currencies";
+        footer["items"] = ({ conjunction(coins), });
+    } else {
+        footer["header"] = "Currency";
+        footer["items"] = ({ "You are not carrying any money.", });
     }
 
     border = format_border(([
         "title": "INVENTORY",
         "subtitle": target->query_cap_name(),
-        "body": ([
-            "header": "Carried Items",
-            "items": items,
-            "columns": 1,
-            "align": "left",
-        ]),
+        "body": items,
         "footer": footer,
     ]));
     foreach (string line in border) {
