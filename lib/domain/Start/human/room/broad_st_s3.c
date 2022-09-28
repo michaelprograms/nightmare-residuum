d7400ad2f2e1fc4b9d973990b8459635d71fd365
Michael Angeli <michael.programs@gmail.com>
2022-09-27 21:48:18 -0400

    move pub in human start area to intersection

--- lib/domain/Start/human/room/broad_st_s3.c
+++ lib/domain/Start/human/room/broad_st_s3.c
@@ -5,14 +5,18 @@ inherit STD_ROOM;
 void create () {
     ::create();
     set_short("a village street");
-    set_long("A small street traveling north to south through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. A framework of hexagons glimmer overhead.");
+    set_long("A small street traveling north to south through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. A building on the northeast corner of the intersection has benches set up and large open windows. A framework of hexagons glimmer overhead.");
     set_exits(([
         "north": HUMAN_ROOM + "broad_st_s2.c",
         "south": HUMAN_ROOM + "broad_st_s4.c",
         "west": HUMAN_ROOM + "walnut_st_w1.c",
         "east": HUMAN_ROOM + "walnut_st_e1.c",
+        "enter northeast": HUMAN_ROOM + "store_pub.c",
     ]));
     set_looks(([
         "street": "A village street labeled S Broad St.",
+    ]));
+    set_reset(([
+        HUMAN_ITEM + "sign_pub.c": 1,
     ]));
 }
--- lib/domain/Start/human/room/store_pub.c
+++ lib/domain/Start/human/room/store_pub.c
@@ -8,16 +8,17 @@ void create () {
         "no attack": 1,
     ]));
     set_short("a village pub");
-    set_long("The inside of a brick building that consists of an open room. There are several open doors to the east facing the street with benches out front. Several tall chairs are placed around the inside in front of a large counter with a large flat surface. Various bottles are stored on some shelves on the wall behind the counter.");
+    set_long("The inside of a brick building that consists of an open room. A doorway in the southwest corner of the building exits out to the road intersection. There are several large windows facing west to the street with benches out front. Several tall chairs are placed around the inside in front of a large counter with a large flat surface. Various bottles are stored on some shelves on the wall behind the counter.");
     set_exits(([
-        "out east": HUMAN_ROOM + "broad_st_s1.c",
+        "out southwest": HUMAN_ROOM + "broad_st_s3.c",
     ]));
     set_looks(([
         ({ "bottles", "shelves" }): "Some bottles of liquid with varying levels of opacity. They look to be alcoholic.",
         "chairs": "Chairs in front of the counter for customers to be seated while being served.",
         "benches": "Some benches are placed in front of the building for patrons to sit outside.",
         "counter": "A wooden counter along the side of the room and allowing more room for patrons to enter and sit.",
-        "doors": "The front of the building consists of several doors that swing open and open the building up to the street.",
+        "doorway": "A doorway to the southwest leads out to the intersection of the two village streets.",
+        "windows": "The long side of this corner building consists of several windows that swing open and open the building up to the street.",
     ]));
     set_reset(([
         // HUMAN_NPC + "vendor_barkeep.c": 1,
