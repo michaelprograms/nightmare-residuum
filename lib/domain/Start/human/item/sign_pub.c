5290f48d2cd0311ae717757d46d31355b858ecc1
Michael Angeli <michael.programs@gmail.com>
2022-09-27 21:51:09 -0400

    add pub sign

---
+++ lib/domain/Start/human/item/sign_pub.c
@@ -0,0 +1,10 @@
+inherit STD_ITEM;
+
+void create () {
+    ::create();
+    set_name("sign");
+    set_id(({"sign"}));
+    set_short("a wooden sign painted with the word %^YELLOW%^PUB%^DEFAULT%^");
+    set_long("A wooden sign outside the building on the northeast corner painted with the word PUB in bright yellow paint.");
+    set_no_get(1);
+}
