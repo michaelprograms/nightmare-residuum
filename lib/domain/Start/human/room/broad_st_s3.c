727c9bcd946f82436894cb975c5891fded9d5f82
Michael Angeli <michael.programs@gmail.com>
2022-09-28 22:25:47 -0400

    add listen/smell to room

--- lib/domain/Start/human/room/broad_st_s3.c
+++ lib/domain/Start/human/room/broad_st_s3.c
@@ -6,6 +6,8 @@ void create () {
     ::create();
     set_short("a village street");
     set_long("A small street traveling north to south through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. A building on the northeast corner of the intersection has benches set up and large open windows. A framework of hexagons glimmer overhead.");
+    set_listen("default", "The chattering of a small crowd in and outside the pub.");
+    set_smell("default", "Fried food from the pub fills the air.");
     set_exits(([
         "north": HUMAN_ROOM + "broad_st_s2.c",
         "south": HUMAN_ROOM + "broad_st_s4.c",
