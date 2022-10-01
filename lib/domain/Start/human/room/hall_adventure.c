5912ca1bbf500b472d96372b184486fcaf536a7f
Michael Angeli <michael.programs@gmail.com>
2022-09-30 21:36:02 -0400

    make adventurer hall no attack

--- lib/domain/Start/human/room/hall_adventure.c
+++ lib/domain/Start/human/room/hall_adventure.c
@@ -7,6 +7,9 @@ inherit STD_ROOM;
 void create () {
     room::create();
     train::create();
+    set_properties(([
+        "no attack": 1,
+    ]));
     set_short("a village training hall");
     set_long("The inside of a brick building that consists of a large open hall. The doorway on the west wall is held open by ropes attached to hooks. There is training equipment of all sorts placed throughout the room.");
     set_exits(([
