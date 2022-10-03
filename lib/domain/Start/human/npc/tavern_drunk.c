9761ff43a247c4f81c418ca449754fe3d7f42c7b
Michael Angeli <michael.programs@gmail.com>
2022-10-02 22:40:07 -0400

    update the drunk

--- lib/domain/Start/human/npc/tavern_drunk.c
+++ lib/domain/Start/human/npc/tavern_drunk.c
@@ -8,11 +8,14 @@ void create () {
     set_name("drunk");
     set_short("a drunk outside the tavern");
     set_gender(element_of(({ "male", "female" })));
-    set_long("A drunk who stares through you with crossed eyes and a blank face, while hiccuping");
+    set_long("A drunk who stares through you with crossed eyes and a blank face, while hiccuping.");
     set_level(5);
     set_species("human");
     set_ability_list(({ "bonk" }));
     set_ability_chance(25);
-;
+
+    set_inventory(([
+        HUMAN_WEAPON "broken_bottle.c": "wield bottle",
+    ]));
     add_currency("copper", 1 + random(10));
 }
---
+++ lib/domain/Start/human/weapon/broken_bottle.c
@@ -0,0 +1,12 @@
+inherit STD_WEAPON;
+
+void create () {
+    ::create();
+    set_name("broken bottle");
+    set_id(({ "broken bottle", "bottle" }));
+    set_short("a broken bottle");
+    set_long("A broken bottle with a sharp glass edge.");
+    set_type("knife");
+    set_value(2);
+    set_wc(1);
+}
