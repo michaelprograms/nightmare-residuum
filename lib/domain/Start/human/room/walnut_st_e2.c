472f11d87c81d7154a67246dd61dced5fcd0c53c
Michael Angeli <michael.programs@gmail.com>
2022-10-25 21:01:55 -0400

    fix bad exit

--- lib/domain/Start/human/room/walnut_st_e2.c
+++ lib/domain/Start/human/room/walnut_st_e2.c
@@ -8,7 +8,7 @@ void create () {
     set_long("A small street traveling west to east through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. An open grassy area is to the east. A framework of hexagons glimmer overhead.");
     set_exits(([
         "west": HUMAN_ROOM + "walnut_st_e1.c",
-        "east": HUMAN_ROOM + "park_southeast.c",
+        "east": HUMAN_ROOM + "park_se.c",
     ]));
     set_looks(([
         "street": "A village street labeled E Walnut St.",
