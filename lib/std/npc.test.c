9bc3a98009b8460374740c73a0e505a3f5f7043a
Michael Angeli <michael.programs@gmail.com>
2022-11-02 21:41:07 -0400

    add tests for NPC aggressive

--- lib/std/npc.test.c
+++ lib/std/npc.test.c
@@ -38,3 +38,45 @@ void test_set_level () {
         assert(testOb->query_skill("melee defense"), ">", 0),
     }) :));
 }
+
+nosave object __Room, __Character;
+void test_aggressive (function done) {
+    expect_function("set_aggressive", testOb);
+    expect_function("query_aggressive", testOb);
+    expect_function("handle_receive_living_in_env", testOb);
+
+    expect("aggressive is settable and queryable", (: ({
+        assert(testOb->query_aggressive(), "==", 0),
+
+        testOb->set_aggressive(10),
+        assert(testOb->query_aggressive(), "==", 10),
+
+        testOb->set_aggressive(123),
+        assert(testOb->query_aggressive(), "==", 123),
+    }) :));
+
+    __Room = new(STD_ROOM);
+    __Character = new(STD_CHARACTER);
+    __Character->set_level(1);
+    __Character->set_stat("charisma", 1);
+
+    expect("aggressive pre-requisites are met", (: ({
+        assert(__Character->query_level(), "==", 1),
+        assert(__Character->query_stat("charisma"), "==", 1),
+
+        assert(testOb->handle_move(__Room), "==", 1),
+        assert(__Character->handle_move(__Room),  "==", 1),
+    }) :));
+
+    call_out_walltime(function (function done) {
+        expect("aggressive initiates combat", (: ({
+            assert(testOb->query_hostile(__Character), "==", 1),
+            assert(__Character->query_hostile(testOb), "==", 1),
+        }) :));
+
+        if (__Room) destruct(__Room);
+        if (__Character) destruct(__Character);
+
+        evaluate(done);
+    }, 0, done);
+}
