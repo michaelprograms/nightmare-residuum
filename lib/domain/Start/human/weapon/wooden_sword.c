5cb2631eda03ec94713f2a8bba7d1919d7b58ca7
Michael Angeli <michael.programs@gmail.com>
2022-11-08 19:21:59 -0500

    fix a typo in wooden sword

--- lib/domain/Start/human/weapon/wooden_sword.c
+++ lib/domain/Start/human/weapon/wooden_sword.c
@@ -4,7 +4,7 @@ void create () {
     ::create();
     set_name("wooden sword");
     set_id(({ "wooden sword", "sword" }));
-    set_short("an wooden sword");
+    set_short("a wooden sword");
     set_long("A wooden sword used for training.");
     set_type("blade");
     set_wc(1);
