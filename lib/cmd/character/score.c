6fdf73ab93c74ab2ffbb2310d84e3d98dc6c3355
Michael Angeli <michael.programs@gmail.com>
2022-11-12 20:14:42 -0500

    update score command

--- lib/cmd/character/score.c
+++ lib/cmd/character/score.c
@@ -17,17 +17,58 @@ void command (string input, mapping flags) {
             }),
             "columns": 2,
         ]),
-        "body": ([
-            "items": ({
-                sprintf("%12s", "Species") + ": " + capitalize(target->query_gender()) + " " + capitalize(target->query_species()),
-                sprintf("%12s", "Class") + ": " + capitalize(target->query_class()),
-                sprintf("%12s", "Level") + ": " + target->query_level(),
-                sprintf("%12s", "Experience") + ": " + format_integer(target->query_experience()),
-                sprintf("%12s", "Victories") + ": " + target->query_victory(),
-                sprintf("%12s", "Defeats") + ": " + target->query_defeat(),
-            }),
-            "columns": 2,
-        ]),
+        "body": ({
+            ([
+                "header": ({
+                    "Species",
+                    "Class",
+                    "Level",
+                }),
+                "items": ({
+                    capitalize(target->query_gender()) + " " + capitalize(target->query_species()),
+                    capitalize(target->query_class()),
+                    target->query_level(),
+                }),
+                "columns": 3,
+                "align": "center",
+            ]),
+            ([
+                "header": ({
+                    "Experience",
+                    "Victories",
+                    "Defeats",
+                }),
+                "items": ({
+                    format_integer(target->query_experience()),
+                    target->query_victory(),
+                    target->query_defeat(),
+                }),
+                "columns": 3,
+                "align": "center",
+            ]),
+            ([
+                "header": ({
+                    "STR",
+                    "PER",
+                    "END",
+                    "CHA",
+                    "INT",
+                    "AGI",
+                    "LUCK",
+                }),
+                "items": ({
+                    target->query_stat("strength"),
+                    target->query_stat("perception"),
+                    target->query_stat("endurance"),
+                    target->query_stat("charisma"),
+                    target->query_stat("intelligence"),
+                    target->query_stat("agility"),
+                    target->query_stat("luck"),
+                }),
+                "columns": 7,
+                "align": "center",
+            ]),
+        }),
         "footer": ([
             "items": ({
                 "Connected time: " + time_from_seconds(target->query_connection_time()),
