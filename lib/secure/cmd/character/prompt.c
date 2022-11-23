f6782d9ecde2f3e30a42465a4b10507d6b1619cd
Michael Angeli <michael.programs@gmail.com>
2022-11-23 10:15:53 -0500

    fix a warning in prompt command

--- lib/secure/cmd/character/prompt.c
+++ lib/secure/cmd/character/prompt.c
@@ -5,7 +5,7 @@ void create () {
     set_help_text("The prompt command is used to set the text that appears before each input you type.");
 }
 
-void command (string input) {
+void command (string input, mapping flags) {
     object shell = this_user()->query_shell();
 
     if (!input) {
