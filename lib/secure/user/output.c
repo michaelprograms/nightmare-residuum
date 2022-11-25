5e7dd06a6f22be2a15e6f60295189e1507aa43bf
Michael Angeli <michael.programs@gmail.com>
2022-11-24 20:06:42 -0500

    simplify user/output pager logic

--- lib/secure/user/output.c
+++ lib/secure/user/output.c
@@ -5,5 +5,5 @@ void handle_pager (mixed arg) {
     if (arrayp(arg)) lines = arg;
     else if (stringp(arg)) lines = explode(arg, "\n");
 
-    new("/secure/std/pager.c")->start(lines, this_user());
+    new("/secure/std/pager.c")->start(lines, this_object());
 }
