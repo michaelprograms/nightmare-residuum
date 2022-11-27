76ecba96d7e9bae475ac92947fa8bec27f74b033
Michael Angeli <michael.programs@gmail.com>
2022-11-27 15:38:27 -0500

    add an idle command to view how long other characters are idle for

---
+++ lib/cmd/character/idle.c
@@ -0,0 +1,17 @@
+inherit STD_COMMAND;
+
+void create () {
+    set_syntax("idle [character]");
+    set_help_text("The idle command is used to view how long ago a connected character last sent a command.");
+}
+
+void command (string input, mapping flags) {
+    object tc = this_character(), target;
+
+    if (!sizeof(input) || !(target = find_character(input))) {
+        message("action", "Syntax: <idle [character]>\n", tc);
+        return;
+    }
+
+    message("action", target->query_cap_name() + " has been idle for " + time_from_seconds(query_idle(target->query_user()), 2) + ".\n", tc);
+}
