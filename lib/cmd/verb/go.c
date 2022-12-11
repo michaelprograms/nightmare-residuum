9078f22c0f497c2c943aa33dce3db60ea5473bb6
Michael Angeli <michael.programs@gmail.com>
2022-12-11 00:42:30 -0500

    update verb/go for postures

--- lib/cmd/verb/go.c
+++ lib/cmd/verb/go.c
@@ -12,10 +12,10 @@ mixed can_go () {
 }
 
 varargs mixed can_go_str (string dir) {
-    object env;
+    object po = previous_object(), env;
     string exit, *split = explode(dir, " ");
 
-    if (!(env = environment(previous_object()))) return "You are nowhere.";
+    if (!(env = environment(po))) return "You are nowhere.";
     if (dir == "enter") {
         if (exit = env->query_default_enter()) dir = exit;
         else return "Go enter which way?";
@@ -26,11 +26,15 @@ varargs mixed can_go_str (string dir) {
         else return "Go out which way?";
     }
     if (!env->query_exit(dir)) return "You cannot go that way.";
+    if (po->query_posture() == "laying") return "You cannot move while laying down.";
+
     return 1;
 }
 varargs void do_go_str (string dir) {
-    object env = environment(previous_object());
+    object po = previous_object(), env = environment(po);
     string exit;
+
+    if (po->query_posture() == "sitting") po->do_command("stand");
 
     if (dir == "enter") {
         if (exit = env->query_default_enter()) dir = exit;
@@ -39,5 +43,5 @@ varargs void do_go_str (string dir) {
         if (exit = env->query_default_out()) dir = exit;
         else return;
     }
-    env->handle_go(previous_object(), "walk", dir);
+    env->handle_go(po, "walk", dir);
 }
