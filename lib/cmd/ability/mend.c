10d7c7c9aaab18a09c029df4e46ef916e93f2304
Michael Angeli <michael.programs@gmail.com>
2022-12-25 23:20:41 -0500

    add a mend heal ability

--- lib/cmd/ability/mend.c
+++ lib/cmd/ability/mend.c
@@ -6,29 +6,27 @@ void create () {
     ::create();
     set_ability_type("utility");
     set_ability_requirements(([
-        "mystic": ([
+        "psionist": ([
             "level": 1,
         ]),
     ]));
     set_skill_powers(([
         "medicine": 5,
-        "theurgy": 10,
     ]));
-    set_help_text("Chant soothing words towards a target to restore vitals.");
+    set_help_text("Use psionic powers to mend a target's health.");
 }
 
 void handle_utility (object source, object target, string limb) {
     int n;
 
-    n = 1 + random(5);
+    n = 1 + random(10);
     n += random(source->query_skill("medicine"));
-    n += random(to_int(source->query_skill("theurgy") / 2));
 
     if (target->query_hp() < target->query_max_hp()) {
         message("action", possessive_noun(target->query_cap_name()) + " wounds heal slightly.", environment(target), target);
         message("action", "Your wounds heal slightly.", target);
     }
-    target->heal(n);
+    target->add_hp(n);
 
     if (source->query_immortal() || source->query_property("debug")) {
         message("action", "%^CYAN%^Heal:%^RESET%^ " + n, source);
