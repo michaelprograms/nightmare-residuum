399212ae13430db8ff622b0fa54bdb3d35336936
Michael Angeli <michael.programs@gmail.com>
2022-12-31 17:44:44 -0500

    update stats command with Max and Improve columns

--- lib/cmd/character/stats.c
+++ lib/cmd/character/stats.c
@@ -5,6 +5,18 @@ void create () {
     set_help_text("The stats command is used to view the primary stats of your character.");
 }
 
+private int query_improve_percent (object target, string stat) {
+    int exp = target->query_experience();
+    int i, cost, pct;
+    for (i = 0; i < 100; i ++) {
+        if (exp <= 0) break;
+        cost = D_EXPERIENCE->query_stat_cost(stat, target->query_stat_base(stat), target->query_class(), target->query_species());
+        pct += max(({ min(({ exp * 100 / max(({ cost, 1 })), 100 })), 0 }));
+        exp -= cost;
+    }
+    return pct;
+}
+
 void command (string input, mapping flags) {
     object tc = this_character(), target = tc;
     string *items = ({ });
@@ -16,12 +28,16 @@ void command (string input, mapping flags) {
 
     foreach (string stat in ({ "strength", "perception", "endurance", "charisma", "intelligence", "agility", "luck", })) {
         int statBase = target->query_stat_base(stat);
+        int statMax = D_CLASS->query_max_stat(target->query_class(), stat, target->query_level());
         int statBonus = target->query_stat_bonus(stat);
+        int pct = query_improve_percent(target, stat);
         items += ({
             capitalize(stat),
-            sprintf("%4s", ""+statBase),
-            sprintf("%4s", ""+statBonus),
-            format_integer(D_EXPERIENCE->query_stat_cost(stat, statBase, target->query_class(), target->query_species())) + " xp"
+            sprintf("%5s", ""+statBase),
+            sprintf("%5s", ""+statMax),
+            sprintf("%5s", ""+statBonus),
+            sprintf("%12s", format_integer(D_EXPERIENCE->query_stat_cost(stat, statBase, target->query_class(), target->query_species()))),
+            sprintf("%8s", format_integer(pct)+"%"),
         });
     }
 
@@ -29,9 +45,10 @@ void command (string input, mapping flags) {
         "title": "STATS",
         "subtitle": target->query_cap_name(),
         "body": ([
-            "header": ({ "Stat", "Base", "Bonus", "Cost", }),
+            "header": ({ "Stat", "Base", "Max", "Bonus", "Cost", "Improve", }),
             "items": items,
-            "columns": ({ 2, 1, 1, 2, }),
+            "columns": ({ 2, 1, 1, 1, 2, 1, }),
+            "align": "left",
         ]),
         "footer": ([
             "items": ({
