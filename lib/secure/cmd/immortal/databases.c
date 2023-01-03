a229a4352d81f22b4f2acc546449af1f28647a45
Michael Angeli <michael.programs@gmail.com>
2023-01-03 00:16:29 -0500

    create databases command

---
+++ lib/secure/cmd/immortal/databases.c
@@ -0,0 +1,21 @@
+inherit STD_COMMAND;
+
+void create () {
+    set_syntax("databases");
+    set_help_text("The databases command is used to view the entire list of databases.");
+}
+
+void command (string input, mapping flags) {
+    string *status = explode(db_status(), "\n") - ({ "" });
+    if (!sizeof(status)) {
+        status += ({ "No databases connected" });
+    }
+    border(([
+        "title": "DATABASES",
+        "body": ([
+            "items": status,
+            "columns": 1,
+            "align": "center",
+        ]),
+    ]));
+}
