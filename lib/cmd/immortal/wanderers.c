0dc5327999e3712f9425172c61b0e202803f7c72
Michael Angeli <michael.programs@gmail.com>
2023-02-17 20:19:34 -0500

    add wanderers command

---
+++ lib/cmd/immortal/wanderers.c
@@ -0,0 +1,40 @@
+inherit STD_COMMAND;
+
+void create () {
+    set_syntax("wanderers ([name filter]|[file filter])");
+    set_help_text("The wanderers command is used to view the list of objects with wanderers.\n\nUsing a name or file filter will show the list of objects that match either filter.");
+}
+
+void command (string input, mapping flags) {
+    object *obs;
+    string *items = ({ });
+
+    if (!input) {
+        obs = filter(heart_beats(), (: $1 && $1->query_wander() :));
+    } else {
+        obs = filter(heart_beats(), (:
+            $1 && $1->query_wander() &&
+            ($1->query_key_name() == $(input) || regexp(file_name($1), $(input)))
+        :));
+    }
+
+    foreach (object ob in obs) {
+        items += ({
+            file_name(ob),
+            ob->query_wanders(),
+            ob->query_next_wander(),
+        });
+    }
+
+    border(([
+        "title": "WANDERERS",
+        "subtitle": input,
+        "body": ({
+            ([
+                "header": ({ "Object", "Wanders", "Next", }),
+                "items": items,
+                "columns": ({ 5, 1, 1, }),
+            ])
+        }),
+    ]));
+}
