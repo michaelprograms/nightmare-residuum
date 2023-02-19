966270b9d0c906d520f24fb2e9e533f31225a90d
Michael Angeli <michael.programs@gmail.com>
2023-02-18 22:16:30 -0500

    update help command to check for files in /doc/help directory

--- lib/cmd/character/help.c
+++ lib/cmd/character/help.c
@@ -7,8 +7,8 @@ void create () {
 
 void command (string input, mapping flags) {
     string path;
-    object file;
-    string result, *items;
+    object ob;
+    string result, *parts, *items;
 
     if (!input) {
         message("action", "Syntax: help [command]", this_character());
@@ -21,20 +21,34 @@ void command (string input, mapping flags) {
         (path = D_COMMAND->query_ability(input))
     ) {
         path += "/" + input + ".c";
-        file = find_object(path) || load_object(path);
-        if (function_exists("handle_help", file)) {
+        ob = find_object(path) || load_object(path);
+        if (function_exists("handle_help", ob)) {
             result = call_other(path, "handle_help", this_character());
+        }
+    } else {
+        path = "/doc/help/" + input;
+        if (file_size(path) > 0) {
+            result = read_file(path);
+            parts = explode(result, ">");
+            result = "";
+            parts[0..<2] = map(parts[0..<2], (: $1 + ">" :));
+            foreach (string part in parts) {
+                result += pcre_replace_callback(part, "(<[a-z]+\\s?.+>)", (: format_syntax($1) :));
+            }
         }
     }
 
     if (sizeof(result)) {
         items = explode(result, "\n");
+    } else {
+        items = ({ "No help page found for that topic." });
     }
+
     border(([
         "title": "HELP",
         "subtitle": lower_case(input),
         "body": ([
-            "items": sizeof(items) ? items : ({ "No help page found for that topic." }),
+            "items": items,
             "columns": 1,
             "align": sizeof(items) ? "left" : "center",
         ]),
