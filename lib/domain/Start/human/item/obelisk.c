4edb2a660e0886a76e4e6b023c271a327623d7d3
Michael Angeli <michael.programs@gmail.com>
2023-02-19 20:43:27 -0500

    fix color bleeding from start obelisk

--- lib/domain/Start/human/item/obelisk.c
+++ lib/domain/Start/human/item/obelisk.c
@@ -4,7 +4,7 @@ void create () {
     ::create();
     set_name("obelisk");
     set_id(({"obelisk"}));
-    set_short("a tall %^BOLD%^WHITE%^obelisk");
+    set_short("a tall %^BOLD%^WHITE%^obelisk%^DEFAULT%^");
     set_long("An obelisk constructed of an unknown material with no visible texture. The flat white surface reflects lights from above. It towers over everything in the vicinity.");
     set_no_get(1);
 }
