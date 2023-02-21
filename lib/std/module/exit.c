202d3a15bd96d60ef446ad42f5d2c2907db72297
Michael Angeli <michael.programs@gmail.com>
2023-02-20 19:33:06 -0500

    update M_EXIT to allow climbs

--- lib/std/module/exit.c
+++ lib/std/module/exit.c
@@ -1,5 +1,8 @@
-nosave private mapping __Exits = ([]);
-nosave private mapping __Door = ([]); // @TODO
+nosave private mapping __Exits = ([ ]);
+nosave private mapping __Climbs = ([ ]);
+nosave private mapping __Door = ([ ]); // @TODO
+
+/* ----- exits ----- */
 
 mapping query_exits () { return copy(__Exits); }
 string *query_exit_directions () { return keys(__Exits); }
@@ -37,14 +40,14 @@ varargs void set_exit (string dir, string dest, function before, function after,
     if (!stringp(dir)) error("Bad argument 1 to exit->set_exit");
     if (!stringp(dest)) error("Bad argument 2 to exit->set_exit");
 
-    if (!mapp(__Exits[dir])) __Exits[dir] = ([]);
+    if (!mapp(__Exits[dir])) __Exits[dir] = ([ ]);
     __Exits[dir]["room"] = dest;
     if (functionp(before)) __Exits[dir]["before"] = before;
     if (functionp(after)) __Exits[dir]["after"] = after;
     if (stringp(reverse)) __Exits[dir]["reverse"] = reverse;
 }
 void set_exits (mapping exits) {
-    __Exits = ([]);
+    __Exits = ([ ]);
     foreach (mixed dir, mixed dest in exits) {
         if (arrayp(dir)) {
             foreach (string real_dir in dir) {
@@ -62,14 +65,8 @@ void remove_exit (string dir) {
     map_delete(__Exits, dir);
 }
 
-// -----------------------------------------------------------------------------
-
 mixed handle_go (object ob, string method, string dir) {
     mapping exit;
-
-    // if(query_verb() == "go" && interactive(ob)) {
-    //     // @TODO check standng/sitting?
-    // }
 
     dir = format_exit_verbose(dir);
 
@@ -80,6 +77,78 @@ mixed handle_go (object ob, string method, string dir) {
         } else if (__Exits["out " + dir] && !__Exits["enter " + dir]) {
             dir = "out " + dir;
             exit = __Exits[dir];
+        }
+    }
+
+    if (!exit || environment(ob) != this_object()) {
+        return 0;
+    } else if (exit["before"] && !(evaluate(exit["before"], ob, dir))) {
+        return 0;
+    } else if (exit["room"]) {
+        if ((regexp(exit["room"], "#[0-9]+") && find_object(exit["room"])) || (file_size(exit["room"]) > 0)) {
+            ob->handle_go(exit["room"], method, dir, exit["reverse"]);
+            ob->describe_environment();
+            if (exit["after"]) {
+                evaluate(exit["after"], ob, dir);
+            }
+            return 1;
+        } else {
+            message("action", "Something prevents you from going in that direction.", ob);
+            return 0;
+        }
+    } else {
+        return 0;
+    }
+}
+
+/* ----- climbs ----- */
+
+mapping query_climbs () { return copy(__Climbs); }
+string *query_climb_directions () { return keys(__Climbs); }
+string query_climb (string dir) {
+    if (__Climbs[dir]) {
+        return __Climbs[dir]["room"];
+    }
+    return 0;
+}
+
+varargs void set_climb (string dir, string dest, function before, function after, string reverse) {
+    if (!stringp(dir)) error("Bad argument 1 to exit->set_climb");
+    if (!stringp(dest)) error("Bad argument 2 to exit->set_climb");
+
+    if (!mapp(__Climbs[dir])) __Climbs[dir] = ([ ]);
+    __Climbs[dir]["room"] = dest;
+    if (functionp(before)) __Climbs[dir]["before"] = before;
+    if (functionp(after)) __Climbs[dir]["after"] = after;
+    if (stringp(reverse)) __Climbs[dir]["reverse"] = reverse;
+}
+void set_climbs (mapping climbs) {
+    __Climbs = ([ ]);
+    foreach (mixed dir, mixed dest in climbs) {
+        if (arrayp(dir)) {
+            foreach (string real_dir in dir) {
+                if (arrayp(dest)) set_climb(real_dir, dest...);
+                else set_climb(real_dir, dest);
+            }
+        } else {
+            if (stringp(dest)) set_climb(dir, dest);
+            else if (arrayp(dest)) set_climb(dir, dest...);
+        }
+    }
+}
+
+mixed handle_climb (object ob, string method, string dir) {
+    mapping exit;
+
+    dir = format_exit_verbose(dir);
+
+    if (!(exit = __Climbs[dir])) {
+        if (__Climbs["enter " + dir] && !__Climbs["out " + dir]) {
+            dir = "enter " + dir;
+            exit = __Climbs[dir];
+        } else if (__Climbs["out " + dir] && !__Climbs["enter " + dir]) {
+            dir = "out " + dir;
+            exit = __Climbs[dir];
         }
     }
 
