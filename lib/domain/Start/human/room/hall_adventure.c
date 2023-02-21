c20d98b7a3e51272d130d5d49c3116f4ad71f529
Michael Angeli <michael.programs@gmail.com>
2023-02-20 20:56:09 -0500

    fix a lingering bug from changing start area

--- lib/domain/Start/human/room/hall_adventure.c
+++ lib/domain/Start/human/room/hall_adventure.c
@@ -4,12 +4,11 @@ inherit STD_ROOM;
 
 void create () {
     room::create();
-    train::create();
     set_properties(([
         "no attack": 1,
     ]));
-    set_short("a village training hall");
-    set_long("The inside of a brick building that consists of a large open hall. The doorway on the west wall is held open by ropes attached to hooks. There is training equipment of all sorts placed throughout the room.");
+    set_short("a large hall");
+    set_long("The inside of a brick building that consists of a large open hall. The doorway on the west wall is held open by ropes attached to hooks.");
     set_exits(([
         "out west": HUMAN_ROOM + "broad_st_n1.c",
     ]));
