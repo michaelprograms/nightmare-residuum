4d7d4691864401cf2b6cac90b367a9487692a60c
Michael Angeli <michael.programs@gmail.com>
2023-03-19 19:27:28 -0400

    start describing start area museum

--- lib/domain/Start/human/room/museum_1.c
+++ lib/domain/Start/human/room/museum_1.c
@@ -4,12 +4,13 @@ inherit STD_ROOM;
 
 void create () {
     ::create();
-    set_short("a historical building");
-    set_long("Inside a building made out of large stone blocks. The walls are far apart and the room's ceiling is high above.");
+    set_short("a spacious lobby");
+    set_long("The entrance to a building consisting of a spacious lobby with high ceilings and polished floors. The walls are made of stone blocks and covered with some tapestries. A grand staircase along the back of the lobby leads up towards another floor that looks down upon the lobby.");
     set_exits(([
-        "out southeast": HUMAN_ROOM + "parkway_4.c",
+        "southeast": HUMAN_ROOM + "parkway_4.c",
         "east": HUMAN_ROOM + "museum_e1.c",
         "south": HUMAN_ROOM + "museum_w1.c",
+        "up": HUMAN_ROOM + "museum_2.c",
     ]));
     set_looks(([
 
--- lib/domain/Start/human/room/parkway_4.c
+++ lib/domain/Start/human/room/parkway_4.c
@@ -9,7 +9,7 @@ void create () {
     set_listen("default", "Birds chirp from the trees.");
     set_smell("default", "Fresh air smelling of grass.");
     set_exits(([
-        "enter northwest": HUMAN_ROOM + "museum_1.c",
+        "northwest": HUMAN_ROOM + "museum_1.c",
         "southeast": HUMAN_ROOM + "park_nw.c",
     ]));
     set_looks(([
