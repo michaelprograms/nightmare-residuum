4df9326d9d10f6b8267896589546a20c3e17e89b
Michael Angeli <michael.programs@gmail.com>
2023-03-21 22:24:05 -0400

    fix exits on 2nd floor museum lobby

--- lib/domain/Start/human/room/museum_2.c
+++ lib/domain/Start/human/room/museum_2.c
@@ -7,10 +7,9 @@ void create () {
     set_short("a balcony over a lobby");
     set_long("Above the entrance to a building consisting of a spacious lobby. The ceilings are high above the polished floors below. The walls are made of stone blocks and covered with some tapestries. A grand staircase leads down towards the lobby below. Hallways branch to the wings of the building.");
     set_exits(([
-        "southeast": HUMAN_ROOM + "parkway_4.c",
-        "east": HUMAN_ROOM + "museum_e1.c",
-        "south": HUMAN_ROOM + "museum_w1.c",
-        "up": HUMAN_ROOM + "museum_2.c",
+        "east": HUMAN_ROOM + "museum_2e1.c",
+        "south": HUMAN_ROOM + "museum_2w1.c",
+        "down": HUMAN_ROOM + "museum_1.c",
     ]));
     set_looks(([
         ({ "building", "ceiling", "walls" }): "A building of large proportions. The lobby is a large room with high ceilings and distant walls.",
