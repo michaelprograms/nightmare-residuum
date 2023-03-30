3d1c1c4799939471454c223b0725ab044cebb8f3
Michael Angeli <michael.programs@gmail.com>
2023-03-29 20:50:46 -0400

    add building to room descriptions

--- lib/domain/Start/human/room/second_st_n5.c
+++ lib/domain/Start/human/room/second_st_n5.c
@@ -5,7 +5,7 @@ inherit STD_ROOM;
 void create () {
     ::create();
     set_short("a village street");
-    set_long("A small street traveling north to south through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. A framework of hexagons glimmer overhead.");
+    set_long("A small street traveling north to south through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. A large marble building is to the west. A framework of hexagons glimmer overhead.");
     set_exits(([
         "north": HUMAN_ROOM + "second_st_n6.c",
         "south": HUMAN_ROOM + "second_st_n4.c",
