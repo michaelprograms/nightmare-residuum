9b1b6e487229a1bf59298e7dbf1c3651dbfd57af
Michael Angeli <michael.programs@gmail.com>
2023-04-01 14:31:22 -0400

    update museum vault chamber

--- lib/domain/Start/human/room/museum_vault.c
+++ lib/domain/Start/human/room/museum_vault.c
@@ -6,12 +6,13 @@ void create () {
     ::create();
     set_properties(([ "indoors": 1, ]));
     set_short("a chamber inside a building");
-    set_long("Inside of a room with bright lights shining down. The concrete floor looks old here and unpolished, with a circular vault entrance located in the center of the chamber. The walls are made of stone blocks. Archways lead out of the chamber to the east and south.");
+    set_long("Inside of a room with bright lights shining down. The concrete floor looks old here and unpolished, with a circular vault entrance located in the center of the chamber. The walls are made of stone blocks and covered with tapestries. An archway leads out of the chamber into a hallway to the east.");
     set_exits(([
-        "south": HUMAN_ROOM + "museum_1_w1.c",
         "east": HUMAN_ROOM + "museum_1_n1.c",
     ]));
     set_looks(([
-       "vault": "A circular vault exit sits in the center of the chamber's floor. It is sealed and there don't seem to be any controls on this side of the vault door.",
+       ({ "vault", "floor", }): "A circular vault exit sits in the center of the chamber's floor. It is sealed and there are no visible controls on the chamber.",
+       "chamber": "This chamber is a large, empty room with a vault entrance in the center of the floor.",
+       ({ "archway", "hallway", }): "A hallway leading north and south is visible on the other side of an archway in the stone walls.",
     ]));
 }
