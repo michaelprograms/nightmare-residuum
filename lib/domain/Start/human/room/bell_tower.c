3c204b19c40a759001a624b48a9cc9315fad0e98
Michael Angeli <michael.programs@gmail.com>
2023-04-06 21:56:57 -0400

    update room short

--- lib/domain/Start/human/room/bell_tower.c
+++ lib/domain/Start/human/room/bell_tower.c
@@ -4,7 +4,7 @@ inherit STD_ROOM;
 
 void create () {
     ::create();
-    set_short("a village bell tower");
+    set_short("a bell tower above a village");
     set_long("A tower above a vaulted roof of a temple. The wooden boards provided a small platform to walk around a large bell hung in the center. The sides of the tower are open. The village street can be seen below. A framework of hexagons glimmer overhead.");
     set_listen("default", "The whispering chants of hymns can be heard below.");
     set_smell("default", "A breeze of freshened air flows past.");
