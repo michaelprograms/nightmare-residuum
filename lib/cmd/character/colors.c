af137283cfcf81e10ae376b4085328dda41bd073
Michael Diavolo <michael.programs@gmail.com>
2023-04-17 00:23:23 +0000

    add colors command to display all ansi codes

---
+++ lib/cmd/character/colors.c
@@ -0,0 +1,55 @@
+inherit STD_COMMAND;
+
+void create () {
+    set_syntax("colors");
+    set_help_text("The colors command is used to view the list of supported colors.");
+}
+
+void command (string input, mapping flags) {
+    string *keys = D_ANSI->query_keys();
+
+    string *styles = ({ "BOLD", "BOLD_OFF", "ITALIC", "ITALIC_OFF", "UNDERLINE", "UNDERLINE_OFF", "STRIKE", "STRIKE_OFF", "INVERSE", "INVERSE_OFF", "RESET", "-", });
+    string *basicColors = ({ "RED", "ORANGE", "YELLOW", "BLUE", "CYAN", "GREEN", "MAGENTA", "BLACK", "WHITE", "-", "-", "-", "B_RED", "B_ORANGE", "B_YELLOW", "B_BLUE", "B_CYAN", "B_GREEN", "B_MAGENTA", "B_BLACK", "B_WHITE", "-", "-", "-", });
+    string *xtermColors = keys - styles - basicColors;
+
+    styles = map(styles, (: ($1 != "-" ? "%^"+$1+"%^" : "")+sprintf("%15|s", $1)+" %^RESET%^" :));
+
+    basicColors = map(basicColors, (: ($1 != "-" ? "%^"+$1+"%^" : "")+($1 == "BLACK" ? "B_WHITE%^" : "")+($1 == "B_WHITE" ? "BLACK%^" : "")+sprintf("%15|s", $1)+" %^RESET%^" :));
+
+    xtermColors = filter(xtermColors, (: !regexp($1, "^B_") :));
+    xtermColors = sort_array(xtermColors, function(string a, string b) {
+        if(regexp(a, "[0-9]") && !regexp(b, "[0-9]")) return 1;
+        if(!regexp(a, "[0-9]") && regexp(b, "[0-9]")) return -1;
+        return strcmp(a, b);
+    });
+    xtermColors = xtermColors[0..<25] + ({ "-", "-" }) + xtermColors[<24..];
+    xtermColors = map(xtermColors, (: ($1 != "-" ? "%^"+$1+"%^" : "")+(member_array($1, ({"AAA", "G01","G02","G03","G04","C16"})) > -1 ? "B_WHITE%^" : "")+sprintf("%10|s", $1)+" %^RESET%^" :));
+
+    border(([
+        "title": "COLORS",
+        "header": ([
+            "items": ({
+                "You are set to use " + (this_user()->query_account()->query_setting("ansi") == "on" ? this_user()->query_terminal_color() : "no") + " colors",
+            }),
+            "columns": 1,
+            "align": "center",
+        ]),
+        "body": ({
+            ([
+                "header": ({ "Styles" }),
+                "items": styles,
+                "columns": 4,
+            ]),
+            ([
+                "header": ({ "Basic Colors" }),
+                "items": basicColors,
+                "columns": 4,
+            ]),
+            ([
+                "header": ({ "XTerm Colors" }),
+                "items": xtermColors,
+                "columns": 6,
+            ]),
+        }),
+    ]));
+}
