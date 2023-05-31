f72de59ffa8280a529ce8a2804033a98d8dd066b
Michael Programs <michael.programs@gmail.com>
2023-05-31 19:16:44 -0400

    cleanup module/property and add remove_properties function

--- lib/std/module/property.c
+++ lib/std/module/property.c
@@ -1,4 +1,4 @@
-private mapping __Properties = ([]);
+private mapping __Properties = ([ ]);
 
 mapping query_properties ();
 mixed query_property (string key);
@@ -8,40 +8,59 @@ mixed add_property (string key, mixed value);
 int remove_property (string key);
 
 mapping query_properties () {
-    if (!mapp(__Properties)) __Properties = ([]);
+    if (!mapp(__Properties)) __Properties = ([ ]);
+
     return __Properties;
 }
 mixed query_property (string key) {
     if (!stringp(key) || key == "") error("Bad argument 1 to property->query_property");
-    if (!mapp(__Properties)) __Properties = ([]);
+    if (!mapp(__Properties)) __Properties = ([ ]);
+
     return __Properties[key];
 }
-mapping set_properties (mapping properties) {
-    if (!mapp(properties) || !sizeof(properties)) error("Bad argument 1 to property->set_properties");
-    if (!mapp(__Properties)) __Properties = ([]);
-    if (sizeof(__Properties)) __Properties += properties;
-    else __Properties = properties;
-    return __Properties;
-}
+
 mixed set_property (string key, mixed value) {
     if (!stringp(key) || key == "") error("Bad argument 1 to property->set_property");
     if (undefinedp(value) || value == "") error("Bad argument 2 to property->set_property");
     if (arrayp(value) || mapp(value)) error("Bad argument 2 to property->set_property");
-    if (!mapp(__Properties)) __Properties = ([]);
+    if (!mapp(__Properties)) __Properties = ([ ]);
+
     return __Properties[key] = value;
 }
 mixed add_property (string key, mixed value) {
     if (!stringp(key) || key == "") error("Bad argument 1 to property->add_property");
     if (undefinedp(value) || value == "") error("Bad argument 2 to property->add_property");
     if (arrayp(value) || mapp(value)) error("Bad argument 2 to property->add_property");
-    if (!mapp(__Properties)) __Properties = ([]);
-    if (__Properties[key]) __Properties[key] += value;
-    else __Properties[key] = value;
+    if (!mapp(__Properties)) __Properties = ([ ]);
+
+    if (__Properties[key]) {
+        __Properties[key] += value;
+    } else {
+        __Properties[key] = value;
+    }
     return __Properties[key];
 }
+mapping set_properties (mapping properties) {
+    if (!mapp(properties) || !sizeof(properties)) error("Bad argument 1 to property->set_properties");
+    if (!mapp(__Properties)) __Properties = ([ ]);
+
+    if (sizeof(__Properties)) {
+        __Properties += copy(properties);
+    } else {
+        __Properties = copy(properties);
+    }
+    return __Properties;
+}
+
 int remove_property (string key) {
     if (!stringp(key) || key == "") error("Bad argument 1 to property->remove_property");
-    if (!mapp(__Properties)) __Properties = ([]);
-    if (!undefinedp(__Properties[key])) map_delete(__Properties, key);
+    if (!mapp(__Properties)) __Properties = ([ ]);
+
+    if (!undefinedp(__Properties[key])) {
+        map_delete(__Properties, key);
+    }
     return !__Properties[key];
+}
+mapping remove_properties () {
+    return __Properties = ([ ]);
 }
--- lib/std/module/property.test.c
+++ lib/std/module/property.test.c
@@ -16,10 +16,8 @@ string *test_order () {
 
 void test_property_single () {
     expect_function("query_property", testOb);
-    expect_function("query_properties", testOb);
     expect_function("set_property", testOb);
     expect_function("set_property", testOb);
-    expect_function("set_properties", testOb);
     expect_function("add_property", testOb);
     expect_function("remove_property", testOb);
 
@@ -44,20 +42,31 @@ void test_property_single () {
     }) :));
 }
 
-private mapping tmp;
 private mapping m1 = ([ "test_key1": "test_value" ]);
 private mapping m2 = ([ "test_key2": to_float(123) ]);
 
 void test_properties_multiple () {
+    expect_function("query_properties", testOb);
+    expect_function("set_properties", testOb);
+    expect_function("remove_properties", testOb);
+
     expect("set and query properties are handled", (: ({
-        assert(testOb->query_properties(), "==", (: identify(([])) :)),
-        assert(tmp = m1, "==", identify(m1)),
-        assert(testOb->set_properties(m1), "==", identify(tmp)),
-        assert(testOb->query_properties(), "==", identify(tmp)),
-        assert(tmp = tmp + m2, "==", identify(m1 + m2)),
-        assert(testOb->set_properties(m2), "==", identify(tmp)),
-        assert(testOb->query_properties(), "==", identify(tmp)),
+        assert(testOb->query_properties(), "==", ([ ])),
+        assert(testOb->set_properties(m1), "==", identify(m1)),
+        assert(testOb->query_properties(), "==", identify(m1)),
+        assert(testOb->set_properties(m2), "==", identify(m1+m2)),
+        assert(testOb->query_properties(), "==", identify(m1+m2)),
         assert(testOb->remove_property("test_key2"), "==", 1),
         assert(testOb->query_properties(), "==", identify(m1)),
+        assert(testOb->remove_property("test_key1"), "==", 1),
+        assert(testOb->query_properties(), "==", ([ ])),
+    }) :));
+
+    expect("remove properties is handled", (: ({
+        assert(testOb->remove_properties(), "==", ([ ])),
+        assert(testOb->query_properties(), "==", ([ ])),
+        assert(testOb->set_properties(m1), "==", identify(m1)),
+        assert(testOb->set_properties(m2), "==", identify(m1+m2)),
+        assert(testOb->remove_properties(), "==", ([ ])),
     }) :));
 }
