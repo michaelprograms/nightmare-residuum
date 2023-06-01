8bba9df5ceebe2b14741364b581e8c7f03c485d8
Michael Programs <michael.programs@gmail.com>
2023-06-01 00:23:07 -0400

    update human start tank to download class

--- lib/domain/Start/human/room/tank.c
+++ lib/domain/Start/human/room/tank.c
@@ -3,11 +3,12 @@
 inherit STD_ROOM;
 
 nosave private mapping syntax = ([
-    "become": format_syntax("female|male|neither"),
+    "female|male|neither": format_syntax("female|male|neither"),
     "randomize": format_syntax("randomize"),
     "done": format_syntax("done"),
     "look": format_syntax("look"),
-    "retry": format_syntax("retry"),
+    "download": format_syntax("download [class]"),
+    "download full": format_syntax("download warrior|mystic|scoundrel|ranger|psionist|paladin"),
 ]);
 
 string prepare_long_footer () {
@@ -25,9 +26,10 @@ string prepare_long_footer () {
         ]),
         "body": ([
             "items": ({
-                sprintf("%10s %-20s", "Species:", tc->query_species()),
-                sprintf("%10s %-20i", "Age:", 18),
-                sprintf("%10s %-20s", "Gender:", tc->query_gender()),
+                sprintf("%10s %-20i", "AGE:", 18), // @TODO
+                sprintf("%10s %-20s", "SPECIES:", tc->query_species()),
+                sprintf("%10s %-20s", "GENDER:", tc->query_gender()),
+                sprintf("%10s %-20s", "CLASS:", tc->query_class()),
             }),
             "columns": 1,
             "align": "center",
@@ -35,15 +37,16 @@ string prepare_long_footer () {
         "footer": ([
             "header": ({ "Options" }),
             "items": ({
-                syntax["become"],
+                syntax["female|male|neither"],
+                syntax["download"],
+                "",
+                "",
                 syntax["randomize"],
-                "",
-                "",
                 syntax["done"],
             }),
             "columns": 2,
         ]),
-        "borderColors": ({ ({ 65, 105, 225 }), ({ 65, 105, 225 }) }),
+        "borderColors": ({ ({ 65, 105, 225 }), ({ 65, 105, 225 }) }), // Royal Blue
     ]));
 }
 
@@ -57,10 +60,11 @@ void create () {
     parse_add_rule("female", "");
     parse_add_rule("male", "");
     parse_add_rule("neither", "");
+    parse_add_rule("download", "");
+    parse_add_rule("download", "STR");
     parse_add_rule("randomize", "");
     parse_add_rule("done", "");
 }
-
 
 /* ----- parser rules: female, male, neither ----- */
 
@@ -78,6 +82,7 @@ void do_female () {
     tc->set_gender("female");
     message("action", "You tap the button on the display.", tc);
     message("action", "A shock arcs through your body!", tc);
+    message("system", prepare_long_footer(), tc);
 }
 mixed can_male () {
     return environment(this_character()) == this_object();
@@ -93,6 +98,7 @@ void do_male () {
     tc->set_gender("male");
     message("action", "You tap the button on the display.", tc);
     message("action", "A shock arcs through your body!", tc);
+    message("system", prepare_long_footer(), tc);
 }
 mixed can_neither () {
     return environment(this_character()) == this_object();
@@ -108,6 +114,35 @@ void do_neither () {
     tc->set_gender("neither");
     message("action", "You tap the button on the display.", tc);
     message("action", "A shock arcs through your body!", tc);
+    message("system", prepare_long_footer(), tc);
+}
+
+/* ----- parser rule: download ----- */
+mixed can_download () {
+    return environment(this_character()) == this_object();
+}
+void do_download () {
+    message("action", "Syntax: " + syntax["download full"], this_character());
+}
+mixed can_download_str (mixed args...) {
+    return environment(this_character()) == this_object();
+}
+void do_download_str (mixed args...) {
+    object tc = this_character();
+    string str;
+
+    if (sizeof(args)) {
+        str = args[0];
+    }
+
+    if (member_array(str, ({ "warrior", "mystic", "scoundrel", "ranger", "psionist", "paladin", })) == -1) {
+        return do_download();
+    }
+
+    tc->set_class(str);
+    message("action", "You tap the button on the display.", tc);
+    message("action", "A shock arcs through your body!", tc);
+    message("system", prepare_long_footer(), tc);
 }
 
 /* ----- parser rule: randomize ----- */
@@ -117,12 +152,12 @@ mixed can_randomize () {
 }
 void do_randomize () {
     object tc = this_character();
-    string gender;
 
-    write("You tap the randomize button on the display.\n");
-    write("A shock arcs through your body!\n");
-
-    tc->set_gender(gender = ({ "female", "male", "neither" })[random(3)]);
+    tc->set_gender(element_of(({ "female", "male", "neither" })));
+    tc->set_class(element_of(({ "warrior", "mystic", "scoundrel", "ranger", "psionist", "paladin", })));
+    message("action", "You tap the button on the display.", tc);
+    message("action", "A shock arcs through your body!", tc);
+    message("system", prepare_long_footer(), tc);
 }
 
 /* ----- parser rule: done ----- */
@@ -133,6 +168,12 @@ mixed can_done () {
 void do_done () {
     object tc = this_character();
     string err = "%^RED%^BOLD%^ERR-"+(1001+random(9000))+"-"+(1+random(9))+"%^RESET%^";
+
+    if (tc->query_class() == "adventurer") {
+        message("action", "You press the done button and nothing happens.", tc);
+        message("say", "A robotic voice chimes: Specimen must have a class before process is complete.", environment(tc));
+        return;
+    }
 
     message("action", "You press the done button and the tank glass pops open.", tc);
     message("say", "A robotic voice chimes: Warning, automatic knowledge transfer was not completed, specimen may ... " + err + "! please ret-retry-y...", environment(tc));
