88cf826a343a6dc2881f9829fe0ba1beb10e9684
Michael Programs <michael.programs@gmail.com>
2023-06-18 23:02:22 -0400

    update museum room

--- lib/domain/Start/human/room/museum_1_n2.c
+++ lib/domain/Start/human/room/museum_1_n2.c
@@ -6,7 +6,7 @@ void create () {
     ::create();
     set_properties(([ "indoors": 1, ]));
     set_short("a historical building");
-    set_long("The northern hallway of a building with high ceilings and polished floors. The walls are made of stone blocks and covered with tapestries. The end of the hallway comes to a staircase leading up to the second floor.");
+    set_long("The northern hallway of a building with high ceilings and polished floors. The walls are made of stone blocks and covered with tapestries, except for the western wall which is covered in a large painting. The end of the hallway comes to a staircase leading up to the second floor.");
     set_exits(([
         "south": HUMAN_ROOM + "museum_1_n1.c",
         "up": HUMAN_ROOM + "museum_2_n2.c",
@@ -15,5 +15,6 @@ void create () {
         ({ "building", "ceiling", "walls" }): "A building of large proportions. This hallway is a large room with high ceilings and distant walls.",
         "floor": "The floor is a polished white stone in large blocks.",
         "hallway": "It leads away from the lobby and turns.",
+        ({ "painting", "wall" }): "A painting takes up most of the western wall of the hallway.",
     ]));
 }
