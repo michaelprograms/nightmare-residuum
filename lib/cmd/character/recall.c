6818ae17b4a894f603fc847bf5633aaa99e55353
Michael Programs <michael.programs@gmail.com>
2023-07-17 20:25:13 -0400

    add cmd/character/recall command to return to Origin

---
+++ lib/cmd/character/recall.c
@@ -0,0 +1,25 @@
+inherit STD_COMMAND;
+
+void create () {
+    ::create();
+    set_syntax("recall");
+    set_help_text("The recall command is used to return the Origin point.");
+}
+
+void teleport (object tc) {
+    if (!tc) return;
+
+    tc->handle_go("/domain/Origin/room/center.c", "teleport", "away");
+}
+
+void command (string input, mapping flags) {
+    object tc = this_character();
+
+    if (sizeof(tc->query_present_hostiles())) {
+        message("action", "You cannot recall while hostiles opponents are nearby.", tc);
+        return;
+    }
+    message("go", "You begin to teleport...", tc);
+    message("go", tc->query_cap_name() + " begins to teleport...", environment(tc), tc);
+    call_out((: teleport :), 2, tc);
+}
