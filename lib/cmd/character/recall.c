8d4b9c0fbb9241cadbdd7aa341bbd2580beaeb4b
Michael Programs <michael.programs@gmail.com>
2023-07-18 20:44:15 -0400

    update cmd/character/recall to check environment before

--- lib/cmd/character/recall.c
+++ lib/cmd/character/recall.c
@@ -19,6 +19,10 @@ void command (string input, mapping flags) {
         message("action", "You cannot recall while hostiles opponents are nearby.", tc);
         return;
     }
+    if (base_name(environment(tc)) == "/domain/Origin/room/center") {
+        message("action", "You are at the Origin recall point already.", tc);
+        return;
+    }
     message("go", "You begin to teleport...", tc);
     message("go", tc->query_cap_name() + " begins to teleport...", environment(tc), tc);
     call_out((: teleport :), 2, tc);
