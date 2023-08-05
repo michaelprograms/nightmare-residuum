9070858ae3f1d5bf959aea127789d6ea4398d27a
Michael Programs <michael.programs@gmail.com>
2023-08-04 21:43:47 -0400

    add museum to partially adjacent rooms

--- lib/domain/Start/human/room/chestnut_st_w6.c
+++ lib/domain/Start/human/room/chestnut_st_w6.c
@@ -5,13 +5,14 @@ inherit STD_ROOM;
 void create () {
     ::create();
     set_short("a village street");
-    set_long("A small street traveling east through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. A framework of hexagons glimmer overhead. A concrete wall at the base of the hexagon dome blocks further passage west.");
+    set_long("A small street traveling east through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. A large marble building is to the north-northeast. A concrete wall at the base of the hexagon dome blocks further passage west. A framework of hexagons glimmer overhead.");
     set_exits(([
         "east": HUMAN_ROOM + "chestnut_st_w5.c",
         "south": HUMAN_ROOM + "first_st_n2.c",
     ]));
     set_looks(([
         "street": "A village street labeled W Chestnut St.",
+        "building": "A large marble building that takes spans the width of the street block. It looks to be a couple stories tall with an ornate crowning around the roof line. Some windows are spaced out along the side of the building. Columns span the entranceway.",
         ({ "framework", "hexagons"}): "Far above, the hexagons form a geodesic dome.",
     ]));
 }
--- lib/domain/Start/human/room/second_st_n6.c
+++ lib/domain/Start/human/room/second_st_n6.c
@@ -5,7 +5,7 @@ inherit STD_ROOM;
 void create () {
     ::create();
     set_short("a village street");
-    set_long("A small street traveling south through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. A wide brick building with an arched, gated entrance takes up on most of the west side of the street. A framework of hexagons glimmer overhead. A concrete wall at the base of the hexagon dome blocks further passage north.");
+    set_long("A small street traveling south through brick dwellings covered in dark ivy. Several birch trees branch over the street and dwellings. A wide brick building with an arched, gated entrance takes up on most of the west side of the street. A large marble building is to the west-southwest. A concrete wall at the base of the hexagon dome blocks further passage north. A framework of hexagons glimmer overhead.");
     set_listen("default", "Occasionally several faint thuds can be heard from the west.");
     set_smell("default", "A wiff of wildflowers comes from somewhere.");
     set_exits(([
@@ -15,6 +15,7 @@ void create () {
     ]));
     set_looks(([
         "street": "A street sign is labeled N. Second St.",
+        "building": "A large marble building that takes spans the width of the street block. It looks to be a couple stories tall with an ornate crowning around the roof line. Some windows are spaced out along the side of the building. Columns span the entranceway.",
         "building": "The building looks to be a ranger hall.",
         ({ "framework", "hexagons"}): "Far above, the hexagons form a geodesic dome.",
     ]));
