b56ba478c0b6794f27dafbdd28ff2632a927d7f3
Michael Programs <michael.programs@gmail.com>
2023-08-07 20:31:26 -0400

    update the center of Origin

--- lib/domain/Origin/room/center.c
+++ lib/domain/Origin/room/center.c
@@ -8,12 +8,17 @@ void create () {
         "no attack": 1,
     ]));
     set_short("Origin Point");
-    set_long("A central room with exits leading from the corners. The walls are a utility corridor white and lacking in any form of markers.");
+    set_long("A central chamber with hallways branching from the corners of the room. The walls are a utility corridor white and lacking in any form of markers. A tank stands on the north side of the chamber.");
     set_exits(([
         "northwest": ORIGIN_ROOM + "northwest.c",
         "northeast": ORIGIN_ROOM + "northeast.c",
         "southeast": ORIGIN_ROOM + "southeast.c",
         "southwest": ORIGIN_ROOM + "southwest.c",
+    ]));
+    set_looks(([
+        "chamber": "The center of the Origin Point is a large chamber. A hallway branches out diagonally from each corner of the room.",
+        "hallway": "Four hallways of very utilitarian hallway design, simple but built to last.",
+        "tank": "It appears to be a chamber for cloning a living organism.",
     ]));
     set_reset(([
         ORIGIN_NPC + "greeter.c": 1,
