f308b5334d0696e3311377b9cddecd833f527a2c
Michael Programs <michael.programs@gmail.com>
2023-08-11 21:45:42 -0400

    update cmd/immortal/home to have help text

--- lib/cmd/immortal/home.c
+++ lib/cmd/immortal/home.c
@@ -1,8 +1,20 @@
+inherit STD_COMMAND;
+
+void create () {
+    ::create();
+    set_syntax("home [immortal]");
+    set_help_text("The home command can be used by an immortal to return to their personal workroom, or the workroom of the named immortal if it exists.");
+}
+
 void command (string input, mapping flags) {
     if (!input) {
         input = "/realm/" + this_character()->query_key_name() + "/workroom.c";
     } else {
         input = "/realm/" + input + "/workroom.c";
+    }
+    if(file_size(input) < 1) {
+        message("action", "home: "+input+" not found.", this_character());
+        return;
     }
     this_character()->handle_go(input, "teleport", "away");
     this_character()->describe_environment();
