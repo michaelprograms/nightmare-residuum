9b3d3932245bc12503773f36d5a5c75549a055ab
Michael Programs <michael.programs@gmail.com>
2023-08-17 18:31:19 -0400

    add help text to cmd/immortal/parse

--- lib/cmd/immortal/parse.c
+++ lib/cmd/immortal/parse.c
@@ -1,3 +1,12 @@
+inherit STD_COMMAND;
+
+void create () {
+    ::create();
+    set_syntax("parse");
+    set_help_text("The parse command can be used by an immortal to see debug information about how a verb is being parsed.");
+}
+
+
 void command (string input, mapping flags) {
     if (!input) {
         write("parse: input required.\n");
