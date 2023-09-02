b5845afe9dbac1ad219a182d98a5b6b86b52b8dc
Michael Programs <michael.programs@gmail.com>
2023-09-01 22:32:55 -0400

    expand adventurer hall description

--- lib/domain/Start/human/room/hall_adventure.c
+++ lib/domain/Start/human/room/hall_adventure.c
@@ -9,7 +9,7 @@ void create () {
         "indoors": 1,
     ]));
     set_short("a large hall");
-    set_long("The inside of a brick building that consists of a large open hall. The doorway on the west wall is held open by ropes attached to hooks.");
+    set_long("The inside of a brick building that consists of a large open hall. The doorway on the west wall is held open by ropes attached to hooks. Adventurers lounge around the hall between journeys.");
     set_exits(([
         "out west": HUMAN_ROOM + "broad_st_n1.c",
     ]));
