c6a6205083bf6b95096f30c962fa35d3c121d04b
Michael Programs <michael.programs@gmail.com>
2023-09-27 20:40:15 -0400

    update std/module/clean->clean_up comments

--- lib/std/module/clean.c
+++ lib/std/module/clean.c
@@ -26,10 +26,19 @@ int handle_remove () {
     return internal_remove();
 }
 
+// Be wary of calling functions in other objects in clean_up, which may cause
+// that object's clean_up timer to be reset.
 int clean_up (mixed *args...) {
-    if (environment() || __NoClean) {
+    // Once an object has an environment it can never lose it. Defer clean_up
+    // to the object's environment.
+    if (environment()) {
         return clean_never();
     }
+    // Objects (mainly rooms or containers) can be flagged to never clean.
+    if (__NoClean) {
+        return clean_never();
+    }
+    // @TODO deprecrate this
     if (this_object()->query_parent()) {
         return clean_later();
     }
