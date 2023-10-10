f0f597f270cbc4d9c1a0ae328ea17c0390365fba
Michael Programs <michael.programs@gmail.com>
2023-10-10 19:52:07 -0400

    update std/resource to use the ID checked by virtual room

--- lib/std/resource.c
+++ lib/std/resource.c
@@ -4,7 +4,7 @@ inherit M_LEVELABLE;
 void create () {
     ::create();
     set_name("resource node");
-    set_id(({"resource", "resource node", "node" }));
+    set_id(({"resource", "resource node", "node", "resource_node", }));
     set_short("a resource node");
     set_long("A resource node.");
     set_no_get(1);
