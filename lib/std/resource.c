298d399b11c46e47d5e57dc3743b935941647962
Michael Programs <michael.programs@gmail.com>
2023-10-11 18:23:24 -0400

    work on new std/resource

--- lib/std/resource.c
+++ lib/std/resource.c
@@ -1,6 +1,15 @@
 inherit STD_ITEM;
 inherit M_LEVELABLE;
 
+mapping __Resource = ([
+    1: "tin",
+    2: "copper",
+    3: "silver",
+    4: "iron",
+    5: "gold",
+    6: "platinum",
+]);
+
 void create () {
     ::create();
     set_name("resource node");
@@ -8,4 +17,18 @@ void create () {
     set_short("a resource node");
     set_long("A resource node.");
     set_no_get(1);
+}
+
+void set_type (string type) {
+    set_short("a rock containing " + type + " ore");
+    set_long("A rock containing a strip of " + type + " ore.");
+    set_id(({ type + " ore", type }));
+}
+
+void set_level (int l) {
+    levelable::set_level(l);
+
+    if (__Resource[l]) {
+        set_type(__Resource[l]);
+    }
 }
