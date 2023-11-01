48d3dffb11dda5580bd309dade2019b2bb73b4a2
Michael Programs <michael.programs@gmail.com>
2023-10-31 21:43:19 -0400

    add basic tests for std/resource/resource

--- lib/std/resource/resource.test.c
+++ lib/std/resource/resource.test.c
@@ -7,3 +7,24 @@ void before_each_test () {
 void after_each_test () {
     if (objectp(testOb)) destruct(testOb);
 }
+
+void test_resource () {
+    expect("is_resource exists", (: ({
+        assert(testOb->is_resource(), "==", 1),
+    }) :));
+}
+
+void test_type () {
+    expect_function("query_type", testOb);
+    expect_function("set_type", testOb);
+
+    expect("type is settable and queryable", (: ({
+        assert(testOb->query_type(), "==", UNDEFINED),
+
+        testOb->set_type("test"),
+        assert(testOb->query_type(), "==", "test"),
+
+        testOb->set_type("test2"),
+        assert(testOb->query_type(), "==", "test2"),
+    }) :));
+}
