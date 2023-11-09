23f9eafa09b90d0334eebdb121e6e1f2a0b4c5df
Michael Programs <michael.programs@gmail.com>
2023-11-08 22:00:47 -0500

    update fluffos docs

--- update-docs.sh
+++ update-docs.sh
@@ -11,8 +11,7 @@ libDocs=lib/doc
 
 
 
-types=("apply" "efun" "lpc")    # exclude concepts, driver, stdlib, and zh-CN
-
+types=("apply" "efun")    # exclude concepts, driver, stdlib, and zh-CN
 for type in ${types[@]} ; do
     mkdir -p ${libDocs}/${type} # ensure mudlib dir exists
 
@@ -22,8 +21,39 @@ for type in ${types[@]} ; do
         for file in ${dir}/*.md ; do
             filename=${file##*/}    # grab everything after last /
 
-            [[ ${filename} =~ ^__ ]] && continue    # ignore obsolete apply __INIT
+            [[ ${filename} =~ ^__ ]] && continue        # ignore obsolete apply __INIT
             [[ ${filename} =~ ^README ]] && continue    # ignore README
+            [[ ${filename} =~ ^index ]] && continue     # ignore index
+
+            # new doc file path
+            newDoc=${libDocs}/${type}/${filename%.md}
+            tmpDoc=${newDoc}.tmp
+
+            cp ${file} ${tmpDoc}        # copy to temporary file
+            sed -i '1,4 d' ${tmpDoc}    # trim beginning of file
+            # update doc if new file is different
+            cmp --silent ${tmpDoc} ${newDoc} || echo Updating ${newDoc} && cp ${tmpDoc} ${newDoc}
+            rm ${tmpDoc}                # remove temporary file
+
+        done
+    done
+done
+
+types=("lpc/constructs" "lpc/preprocessor" "lpc/types")
+for type in ${types[@]} ; do
+    mkdir -p ${libDocs}/${type} # ensure mudlib dir exists
+
+    echo type ${type}
+    for dir in ${driverDocs}/${type}/ ; do
+        dir=${dir%*/}           # remove trailing /
+        echo dir ${dir}
+
+        for file in ${dir}/*.md ; do
+            filename=${file##*/}    # grab everything after last /
+            echo filename ${filename}
+            [[ ${filename} =~ ^__ ]] && continue        # ignore obsolete apply __INIT
+            [[ ${filename} =~ ^README ]] && continue    # ignore README
+            [[ ${filename} =~ ^index ]] && continue     # ignore index
 
             # new doc file path
             newDoc=${libDocs}/${type}/${filename%.md}
