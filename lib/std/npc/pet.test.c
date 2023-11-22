79c1c4e34bec677c73dd265fa09624766411c8c5
Michael Programs <michael.programs@gmail.com>
2023-11-21 21:19:25 -0500

    add test to NPC_PET

--- lib/std/npc/pet.test.c
+++ lib/std/npc/pet.test.c
@@ -7,3 +7,24 @@ void before_each_test () {
 void after_each_test () {
     if (objectp(testOb)) destruct(testOb);
 }
+
+string query_key_name () {
+    return "/std/npc/pet.test.c";
+}
+
+void test_owner () {
+    expect_function("set_owner", testOb);
+    expect_function("query_owner", testOb);
+    expect_function("query_owner_name", testOb);
+
+    expect("owner is settable and queryable", (: ({
+        // no owner set
+        assert(testOb->query_owner(), "==", UNDEFINED),
+        assert(testOb->query_owner_name(), "==", 0),
+
+        testOb->set_owner(this_object()),
+        // owner set
+        assert(testOb->query_owner(), "==", this_object()),
+        assert(testOb->query_owner_name(), "==", "/std/npc/pet.test.c"),
+    }) :));
+}
