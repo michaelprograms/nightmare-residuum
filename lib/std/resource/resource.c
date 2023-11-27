501e9a8f81ded092a50b37e4bb0b6e96593ae445
Michael Programs <michael.programs@gmail.com>
2023-11-26 19:02:27 -0500

    add default name/ids to resource item

--- lib/std/resource/resource.c
+++ lib/std/resource/resource.c
@@ -8,3 +8,11 @@ string query_type () {
 void set_type (string type) {
     __ResourceType = type;
 }
+
+void create () {
+    ::create();
+    set_name("resource");
+    set_id(({ "resource", }));
+    set_short("a resource");
+    set_long("A resource.");
+}
