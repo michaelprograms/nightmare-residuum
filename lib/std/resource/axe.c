bc860f379502d90b991162b25fc72919ef162bae
Michael Programs <michael.programs@gmail.com>
2023-11-26 19:02:05 -0500

    add axe and pickaxe resource tools

---
+++ lib/std/resource/axe.c
@@ -0,0 +1,10 @@
+inherit STD_ITEM;
+
+void create () {
+    ::create();
+    set_name("axe");
+    set_id(({ "axe" }));
+    set_short("a axe");
+    set_long("A axe to harvest wood resources with.");
+    set_autoload(1);
+}
---
+++ lib/std/resource/pickaxe.c
@@ -0,0 +1,10 @@
+inherit STD_ITEM;
+
+void create () {
+    ::create();
+    set_name("pickaxe");
+    set_id(({ "pickaxe" }));
+    set_short("a pickaxe");
+    set_long("A pickaxe to harvest ore resources with.");
+    set_autoload(1);
+}
