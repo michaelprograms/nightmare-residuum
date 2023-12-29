077668915720059a987d12b4aa90227e76379e94
Michael Programs <michael.programs@gmail.com>
2023-12-29 18:08:43 -0500

    update Origin greeter to greet characters

--- lib/domain/Origin/npc/greeter.c
+++ lib/domain/Origin/npc/greeter.c
@@ -8,3 +8,19 @@ void create () {
     set_long("A creature seemingly constucted of synthetic components. It consists of a large elongated torso structure supported by four jointed legs, all encased in a scale-like exoskeloton. There are two articulated arms at each end of the structure, almost looking like a type of crustacean. The creature does seem to have a discernable head or face.");
     set_species("custodian");
 }
+
+string *names = ({ });
+
+void handle_receive_living_in_env (object living) {
+    string name;
+    ::handle_receive_living_in_env(living);
+
+    if (!living->is_character()) return;
+
+    name = living->query_cap_name();
+    if (member_array(name, names) == -1) {
+        do_command("say Welcome to our facility, " + name + ".");
+        names += ({ name });
+        call_out((: names -= ({ $1 }) :), 30, name);
+    }
+}
