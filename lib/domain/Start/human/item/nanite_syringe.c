04329f913b254e5c49d8f8b9ff8a032756f175d2
Michael Programs <michael.programs@gmail.com>
2024-01-18 18:31:27 -0500

    update nanite_syringe type

--- lib/domain/Start/human/item/nanite_syringe.c
+++ lib/domain/Start/human/item/nanite_syringe.c
@@ -4,9 +4,10 @@ void create () {
     ::create();
     set_name("syringe");
     set_id(({ "syringe", "nanites" }));
-    set_short("a syringe of nanites");
-    set_long("A syringe of nanites.");
-    set_type("nanites");
+    set_adjective(({ "healing" }));
+    set_short("a syringe of healing nanites");
+    set_long("A syringe of healing nanites.");
+    set_type("healing nanites");
     set_strength(5);
     set_value(10);
 }
