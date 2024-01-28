a914f251b271641169bd1fe3c4ce538e3aee2a7e
Michael Programs <michael.programs@gmail.com>
2024-01-28 14:48:26 -0500

    update cmd/character/ability for modes 'mine' or '[class]'

--- lib/cmd/character/abilities.c
+++ lib/cmd/character/abilities.c
@@ -1,24 +1,52 @@
 inherit STD_COMMAND;
 
+#define ABILITY_DIR "/cmd/ability"
+
 void create () {
     ::create();
-    set_syntax("abilities");
-    set_help_text("The abilities command is used to view the list of abilities your character can use.");
+    set_syntax("abilities (mine|npc|[class])");
+    set_help_text("The abilities command is used to view the list of abilities, with filters for abilities your character can use, the generic NPC ability list, or the list of abilities for a class.");
 }
 
 void command (string input, mapping flags) {
-    string subtitle;
+    object po = previous_object();
     mixed *body = ({ });
     string *abilities = ({ });
     mapping b;
+    string subtitle;
+    string mode;
+
+    if (sizeof(input)) {
+        input = lower_case(input);
+        if (input == "mine") {
+            subtitle = "mine";
+            mode = "mine";
+        } else if (member_array(input, ({ "warrior", "mystic", "scoundrel", "ranger", "psionist", "paladin", "NPC" })) > -1) {
+            subtitle = input;
+            mode = "class";
+        }
+    }
 
     // @TODO handle cases for input == mine, input == [class]
-    foreach (string action in D_COMMAND->query_abilities("/cmd/ability")) {
-        abilities += ({ action });
+    foreach (string action in D_COMMAND->query_abilities(ABILITY_DIR)) {
+        if (mode == "mine") {
+            object ability = load_object(ABILITY_DIR + "/" + action + ".c");
+            if (ability->verify_ability_requirements(po)) {
+                abilities += ({ action });
+            }
+        } else if (mode == "class") {
+            object ability = load_object(ABILITY_DIR + "/" + action + ".c");
+            mapping reqs = ability->query_ability_requirements();
+            if (reqs[input]) {
+                abilities += ({ action });
+            }
+        } else {
+            // no input, show all abilities
+            abilities += ({ action });
+        }
     }
 
     b = ([
-        "header": ({ "Abilities" }),
         "columns": 4,
         "items": ({ }),
     ]);
@@ -28,7 +56,7 @@ void command (string input, mapping flags) {
     body += ({ b });
 
     border(([
-        "title": "COMMANDS",
+        "title": "ABILITIES",
         "subtitle": subtitle,
         "body": body,
     ]));
