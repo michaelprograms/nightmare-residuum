70ffe265a27d2c6bfd1d5db0697c261f30e3a66a
Michael Programs <michael.programs@gmail.com>
2024-02-18 22:06:48 -0500

    add STD_USER->gmcp_update_character function

---
+++ lib/std/user/gmcp.c
@@ -0,0 +1,23 @@
+mapping __GMCPModuleData = ([ ]);
+
+int gmcp_enabled() {
+    if (this_object()->query_setting("gmcp") != "on") {
+        return 0;
+    }
+    return has_gmcp(this_object());
+}
+
+void gmcp_update_character (string module, mapping data) {
+    if (!gmcp_enabled()) {
+        return;
+    }
+
+    if (!mapp(__GMCPModuleData)) {
+        __GMCPModuleData = ([ ]);
+    }
+    if (!mapp(__GMCPModuleData[module])) {
+        __GMCPModuleData[module] = ([ ]);
+    }
+    __GMCPModuleData[module] += data;
+    send_gmcp(module + " " + json_encode(data));
+}
