ec69cb97eb5a62d46cbb0d0619d688d7093da53c
Michael Programs <michael.programs@gmail.com>
2024-02-21 18:04:03 -0500

    format std/user/gmcp.c

--- lib/std/user/gmcp.c
+++ lib/std/user/gmcp.c
@@ -1,6 +1,6 @@
 mapping __GMCPModuleData = ([ ]);
 
-int gmcp_enabled() {
+int gmcp_enabled () {
     if (this_object()->query_setting("gmcp") != "on") {
         return 0;
     }
@@ -11,7 +11,6 @@ void gmcp_send_update (string module, mapping data) {
     if (!gmcp_enabled()) {
         return;
     }
-
     if (!mapp(__GMCPModuleData)) {
         __GMCPModuleData = ([ ]);
     }
