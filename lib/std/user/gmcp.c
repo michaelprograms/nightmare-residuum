8f5288437e56cbe369c96d00acd09795bb98b26b
Michael Programs <michael.programs@gmail.com>
2024-02-22 18:06:14 -0500

    add apply::gmcp to user/gmcp to respond to requests

--- lib/std/user/gmcp.c
+++ lib/std/user/gmcp.c
@@ -21,3 +21,32 @@ void gmcp_send_update (string module, mapping data) {
 
     send_gmcp(module + " " + json_encode(data));
 }
+
+void gmcp (string request) {
+    if (request == "Char.Vitals.Get") {
+        object char = this_object()->query_character();
+        if (!char) {
+            return;
+        }
+
+        char->gmcp_update_vitals();
+    } else if (request == "Room.Info.Get") {
+        object char = this_object()->query_character();
+        object env;
+        if (!char) {
+            return;
+        }
+        env = environment(char);
+
+        gmcp_send_update("Room.Info",  env ? ([
+            "name": env->query_short(),
+            "exits": env->query_exit_directions(),
+            "area": explode(base_name(env), "/")[1],
+        ]) : ([
+            "name": "",
+            "exits": "",
+            "area": "",
+        ]));
+    }
+
+}
