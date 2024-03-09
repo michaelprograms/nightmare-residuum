0d8ecee964f89e6a5900215a99f30cf0d063d5c3
Michael Programs <michael.programs@gmail.com>
2024-03-08 20:01:11 -0500

    add std/living->set_level test

--- lib/std/living.test.c
+++ lib/std/living.test.c
@@ -28,3 +28,38 @@ void test_species () {
         assert(testOb->query_species(), "==", "human"),
     }) :));
 }
+
+void test_level () {
+    expect_function("set_level", testOb);
+
+    // @TODO check limbs max damage
+    expect("level updates vitals", (: ({
+        testOb->set_species("human"),
+        assert(testOb->query_species(), "==", "human"),
+        assert(testOb->query_level(), "==", 1),
+        assert(testOb->query_hp(), "==", 22),
+        assert(testOb->query_sp(), "==", 12),
+        assert(testOb->query_mp(), "==", 12),
+        assert(testOb->query_max_hp(), "==", 22),
+        assert(testOb->query_max_sp(), "==", 12),
+        assert(testOb->query_max_mp(), "==", 12),
+
+        // adjust HP
+        testOb->set_hp(1),
+        testOb->set_sp(1),
+        testOb->set_mp(1),
+        assert(testOb->query_hp(), "==", 1),
+        assert(testOb->query_sp(), "==", 1),
+        assert(testOb->query_mp(), "==", 1),
+
+        // advance level
+        testOb->set_level(2),
+        // vitals are updated and healed
+        assert(testOb->query_hp(), "==", 33),
+        assert(testOb->query_sp(), "==", 18),
+        assert(testOb->query_mp(), "==", 18),
+        assert(testOb->query_max_hp(), "==", 33),
+        assert(testOb->query_max_sp(), "==", 18),
+        assert(testOb->query_max_mp(), "==", 18),
+    }) :));
+}
