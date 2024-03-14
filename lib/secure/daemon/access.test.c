d6b70cc9b40a1b0ca4b49639f0067e6fa31ec4ee
Michael Programs <michael.programs@gmail.com>
2024-03-13 21:40:26 -0400

    update D_ACCESS to assert_equal

--- lib/secure/daemon/access.test.c
+++ lib/secure/daemon/access.test.c
@@ -15,21 +15,21 @@ void test_query_file_privs () {
     expect_function("query_file_privs", testOb);
 
     expect("query_file_privs handles files", (: ({
-        assert(testOb->query_file_privs("/etc/welcome"), "==", ACCESS_ALL),
-        assert(testOb->query_file_privs("/daemon/ansi.c"), "==", ACCESS_MUDLIB),
-        assert(testOb->query_file_privs("/daemon/something.c"), "==", ACCESS_MUDLIB),
-        assert(testOb->query_file_privs("/std/object.c"), "==", ACCESS_ASSIST),
-        assert(testOb->query_file_privs("/std/module/save.c"), "==", ACCESS_ASSIST),
-        assert(testOb->query_file_privs("/secure/daemon/access.c"), "==", ACCESS_SECURE),
-        assert(testOb->query_file_privs("/secure/daemon/master.c"), "==", ACCESS_SECURE),
-        assert(testOb->query_file_privs("/realm/user/workroom.c"), "==", "user"),
-        assert(testOb->query_file_privs("/realm/user/header.h"), "==", "user"),
-        assert(testOb->query_file_privs("/domain/Area/room/room1.c"), "==", "Area"),
-        assert(testOb->query_file_privs("/domain/Area/include/area.h"), "==", "Area"),
-        assert(testOb->query_file_privs("/domain/CAPITALIZED/file.c"), "==", "Capitalized"),
-        assert(testOb->query_file_privs(""), "==", 0),
-        assert(testOb->query_file_privs("/"), "==", 0),
-        assert(testOb->query_file_privs("/nonexistantpath/file.c"), "==", 0),
+        assert_equal(testOb->query_file_privs("/etc/welcome"), ACCESS_ALL),
+        assert_equal(testOb->query_file_privs("/daemon/ansi.c"), ACCESS_MUDLIB),
+        assert_equal(testOb->query_file_privs("/daemon/something.c"), ACCESS_MUDLIB),
+        assert_equal(testOb->query_file_privs("/std/object.c"), ACCESS_ASSIST),
+        assert_equal(testOb->query_file_privs("/std/module/save.c"), ACCESS_ASSIST),
+        assert_equal(testOb->query_file_privs("/secure/daemon/access.c"), ACCESS_SECURE),
+        assert_equal(testOb->query_file_privs("/secure/daemon/master.c"), ACCESS_SECURE),
+        assert_equal(testOb->query_file_privs("/realm/user/workroom.c"), "user"),
+        assert_equal(testOb->query_file_privs("/realm/user/header.h"), "user"),
+        assert_equal(testOb->query_file_privs("/domain/Area/room/room1.c"), "Area"),
+        assert_equal(testOb->query_file_privs("/domain/Area/include/area.h"), "Area"),
+        assert_equal(testOb->query_file_privs("/domain/CAPITALIZED/file.c"), "Capitalized"),
+        assert_equal(testOb->query_file_privs(""), 0),
+        assert_equal(testOb->query_file_privs("/"), 0),
+        assert_equal(testOb->query_file_privs("/nonexistantpath/file.c"), 0),
     }) :));
 }
 
@@ -50,12 +50,12 @@ void test_query_allowed () {
     basicOb = new(STD_OBJECT);
     expect("query_allowed handles valid requests", (: ({
         // query read/write on path with permissions
-        assert(testOb->query_allowed(testOb, "file_size", "/tmp/path", "read"), "==", 1),
-        assert(testOb->query_allowed(testOb, "write_file", "/tmp/path", "write"), "==", 1),
+        assert_equal(testOb->query_allowed(testOb, "file_size", "/tmp/path", "read"), 1),
+        assert_equal(testOb->query_allowed(testOb, "write_file", "/tmp/path", "write"), 1),
 
         // query read/write on path without permissions
-        assert(testOb->query_allowed($(basicOb), "read_file", "/tmp/void/path", "read"), "==", 0),
-        assert(testOb->query_allowed($(basicOb), "write_file", "/tmp/void/path", "write"), "==", 0),
+        assert_equal(testOb->query_allowed($(basicOb), "read_file", "/tmp/void/path", "read"), 0),
+        assert_equal(testOb->query_allowed($(basicOb), "write_file", "/tmp/void/path", "write"), 0),
     }) :));
     destruct(basicOb);
 }
