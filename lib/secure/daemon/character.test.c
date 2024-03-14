edf48ff7bd774616684154f142d2ff300262cf92
Michael Programs <michael.programs@gmail.com>
2024-03-13 21:42:06 -0400

    update D_CHARACTER to assert_equal

--- lib/secure/daemon/character.test.c
+++ lib/secure/daemon/character.test.c
@@ -13,15 +13,15 @@ void test_valid_name () {
     expect_function("query_valid_name", testOb);
 
     expect("query_valid_name handles names", (: ({
-        assert(testOb->query_valid_name("valid"), "==", 1),
-        assert(testOb->query_valid_name("abcdefghijklm"), "==", 1),
-        assert(testOb->query_valid_name("nopqrstuvwxyz"), "==", 1),
-        assert(testOb->query_valid_name("a'b c-d"), "==", 1), // valid decorators
+        assert_equal(testOb->query_valid_name("valid"), 1),
+        assert_equal(testOb->query_valid_name("abcdefghijklm"), 1),
+        assert_equal(testOb->query_valid_name("nopqrstuvwxyz"), 1),
+        assert_equal(testOb->query_valid_name("a'b c-d"), 1), // valid decorators
 
-        assert(testOb->query_valid_name(""), "==", 0),
-        assert(testOb->query_valid_name("abc"), "==", 0), // too short
-        assert(testOb->query_valid_name("abcdefghijklmnopqrstuvwxyz"), "==", 0), // too long
-        assert(testOb->query_valid_name("!@#$%^&*()"), "==", 0), // invalid chars
+        assert_equal(testOb->query_valid_name(""), 0),
+        assert_equal(testOb->query_valid_name("abc"), 0), // too short
+        assert_equal(testOb->query_valid_name("abcdefghijklmnopqrstuvwxyz"), 0), // too long
+        assert_equal(testOb->query_valid_name("!@#$%^&*()"), 0), // invalid chars
     }) :));
 }
 
