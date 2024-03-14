c829b33300a8a7d909f4fd711c5b965f99aca64c
Michael Programs <michael.programs@gmail.com>
2024-03-13 21:42:41 -0400

    update master to assert_equal

--- lib/secure/daemon/master.test.c
+++ lib/secure/daemon/master.test.c
@@ -26,18 +26,18 @@ void test_applies () {
 
     expect("connect returns a valid user object", (: ({
         assert(file_name(userOb = testOb->connect(0)), "regex", STD_USER[0..<3]+"#[0-9]+"),
-        assert(userOb->query_character(), "==", 0),
-        assert(userOb->query_shell(), "==", 0),
-        assert(destruct(userOb), "==", 0),
+        assert_equal(userOb->query_character(), 0),
+        assert_equal(userOb->query_shell(), 0),
+        assert_equal(destruct(userOb), 0),
     }) :));
 
     expect_function("get_mud_stats", testOb);
 
     expect("get_mud_stats returns stats", (: ({
-        assert(mapp(mudStats = testOb->get_mud_stats()), "==", 1),
-        assert(mudStats["NAME"], "==", mud_name()),
-        assert(member_array("PLAYERS", keys(mudStats)) > -1, "==", 1),
-        assert(member_array("UPTIME", keys(mudStats)) > -1, "==", 1),
+        assert_equal(mapp(mudStats = testOb->get_mud_stats()), 1),
+        assert_equal(mudStats["NAME"], mud_name()),
+        assert_equal(member_array("PLAYERS", keys(mudStats)) > -1, 1),
+        assert_equal(member_array("UPTIME", keys(mudStats)) > -1, 1),
     }) :));
 }
 
@@ -45,9 +45,9 @@ void test_startup_applies () {
     expect_function("epilog", testOb);
 
     expect("epilog returns preload array", (: ({
-        assert(typeof(testOb->epilog(0)), "==", "array"),
-        assert(sizeof(testOb->epilog(0)) > 0, "==", 1),
-        assert(sizeof(testOb->epilog(1)) > 0, "==", 0),
+        assert_equal(typeof(testOb->epilog(0)), "array"),
+        assert_equal(sizeof(testOb->epilog(0)) > 0, 1),
+        assert_equal(sizeof(testOb->epilog(1)) > 0, 0),
     }) :));
 
     expect_function("flag", testOb);
@@ -64,17 +64,17 @@ void test_build_applies () {
     expect_function("get_include_path", testOb);
 
     expect("get_include_path handles paths", (: ({
-        assert(testOb->get_include_path("/test/Test"), "==", ({ ":DEFAULT:" })),
-        assert(testOb->get_include_path("/std/module"), "==", ({ ":DEFAULT:" })),
-        assert(testOb->get_include_path("/secure/module"), "==", ({ ":DEFAULT:" })),
+        assert_equal(testOb->get_include_path("/test/Test"), ({ ":DEFAULT:" })),
+        assert_equal(testOb->get_include_path("/std/module"), ({ ":DEFAULT:" })),
+        assert_equal(testOb->get_include_path("/secure/module"), ({ ":DEFAULT:" })),
 
-        assert(testOb->get_include_path("/domain/Name"), "==", ({ ":DEFAULT:", "/domain/Name/include" })),
-        assert(testOb->get_include_path("/domain/Name/test"), "==", ({ ":DEFAULT:", "/domain/Name/include" })),
-        assert(testOb->get_include_path("/domain/Name/test/dir"), "==", ({ ":DEFAULT:", "/domain/Name/include" })),
+        assert_equal(testOb->get_include_path("/domain/Name"), ({ ":DEFAULT:", "/domain/Name/include" })),
+        assert_equal(testOb->get_include_path("/domain/Name/test"), ({ ":DEFAULT:", "/domain/Name/include" })),
+        assert_equal(testOb->get_include_path("/domain/Name/test/dir"), ({ ":DEFAULT:", "/domain/Name/include" })),
 
-        assert(testOb->get_include_path("/realm/Name"), "==", ({ ":DEFAULT:", "/realm/Name/include" })),
-        assert(testOb->get_include_path("/realm/Name/test"), "==", ({ ":DEFAULT:", "/realm/Name/include" })),
-        assert(testOb->get_include_path("/realm/Name/test/dir"), "==", ({ ":DEFAULT:", "/realm/Name/include" })),
+        assert_equal(testOb->get_include_path("/realm/Name"), ({ ":DEFAULT:", "/realm/Name/include" })),
+        assert_equal(testOb->get_include_path("/realm/Name/test"), ({ ":DEFAULT:", "/realm/Name/include" })),
+        assert_equal(testOb->get_include_path("/realm/Name/test/dir"), ({ ":DEFAULT:", "/realm/Name/include" })),
     }) :));
 }
 
@@ -103,9 +103,9 @@ void test_ed_applies () {
     expect_function("save_ed_setup", testOb);
 
     expect("retrieve_ed_setup", (: ({
-        assert(testOb->retrieve_ed_setup(this_object()), "==", 0),
-        assert(testOb->save_ed_setup(this_object(), 123), "==", 1),
-        assert(testOb->retrieve_ed_setup(this_object()), "==", 123),
+        assert_equal(testOb->retrieve_ed_setup(this_object()), 0),
+        assert_equal(testOb->save_ed_setup(this_object(), 123), 1),
+        assert_equal(testOb->retrieve_ed_setup(this_object()), 123),
     }) :));
 }
 
@@ -115,10 +115,10 @@ void test_security_applies () {
     // privs_file testing handled by D_ACCESS query_file_privs
 
     expect("valid_override handles requests", (: ({
-        assert(testOb->valid_override("/secure/sefun/override"), "==", 1),
-        assert(testOb->valid_override("/insecure"), "==", 0),
-        assert(testOb->valid_override("/std/user/input", "input_to"), "==", 1),
-        assert(testOb->valid_override("/std/user/input", "get_char"), "==", 1),
+        assert_equal(testOb->valid_override("/secure/sefun/override"), 1),
+        assert_equal(testOb->valid_override("/insecure"), 0),
+        assert_equal(testOb->valid_override("/std/user/input", "input_to"), 1),
+        assert_equal(testOb->valid_override("/std/user/input", "get_char"), 1),
     }) :));
 
     // expect_function("valid_bind", testOb);
@@ -127,23 +127,23 @@ void test_security_applies () {
     // expect_function("valid_object", testOb);
 
     expect("valid_database handles calls", (: ({
-        assert(testOb->valid_database(0, 0, 0), "==", 1),
+        assert_equal(testOb->valid_database(0, 0, 0), 1),
     }) :));
 
     expect("valid_read handles calls", (: ({
         assert(file_name(basicOb = new(STD_OBJECT)), "regex", STD_OBJECT[0..<3]+"#[0-9]+"),
 
-        assert(testOb->valid_read(0, 0, 0), "==", 0),
-        assert(testOb->valid_read(0, testOb, "read_file"), "==", 0),
+        assert_equal(testOb->valid_read(0, 0, 0), 0),
+        assert_equal(testOb->valid_read(0, testOb, "read_file"), 0),
 
-        assert(testOb->valid_read("/", testOb, "read_file"), "==", 1),
-        assert(testOb->valid_read("/tmp/void/doesntexist", basicOb, "read_file"), "==", 0),
+        assert_equal(testOb->valid_read("/", testOb, "read_file"), 1),
+        assert_equal(testOb->valid_read("/tmp/void/doesntexist", basicOb, "read_file"), 0),
     }) :));
 
     expect("valid_write handles calls", (: ({
-        assert(testOb->valid_write("/", testOb, "write_file"), "==", 1),
-        assert(testOb->valid_write("/save", basicOb, "write_file"), "==", 0),
-        assert(destruct(basicOb), "==", 0),
+        assert_equal(testOb->valid_write("/", testOb, "write_file"), 1),
+        assert_equal(testOb->valid_write("/save", basicOb, "write_file"), 0),
+        assert_equal(destruct(basicOb), 0),
     }) :));
 
     // expect_function("valid_save_binary", testOb);
@@ -157,27 +157,27 @@ void test_security_applies () {
 void test_parsing_applies () {
     expect_function("parse_command_id_list", testOb);
     expect("parse_command_id_list returns list of nouns", (: ({
-        assert(implode(testOb->parse_command_id_list(), ","), "==", "thing"),
+        assert_equal(implode(testOb->parse_command_id_list(), ","), "thing"),
     }) :));
 
     expect_function("parse_command_adjectiv_id_list", testOb);
     expect("parse_command_adjectiv_id_list returns list of adjectives", (: ({
-        assert(implode(testOb->parse_command_adjectiv_id_list(), ","), "==", "a,an,the"),
+        assert_equal(implode(testOb->parse_command_adjectiv_id_list(), ","), "a,an,the"),
     }) :));
 
     expect_function("parse_command_plural_id_list", testOb);
     expect("parse_command_plural_id_list returns list of plurals", (: ({
-        assert(implode(testOb->parse_command_plural_id_list(), ","), "==", "things,them,everything"),
+        assert_equal(implode(testOb->parse_command_plural_id_list(), ","), "things,them,everything"),
     }) :));
 
     expect_function("parse_command_prepos_list", testOb);
     expect("parse_command_prepos_list returns list of prepositions", (: ({
-        assert(sizeof(testOb->parse_command_prepos_list()), "==", 50),
+        assert_equal(sizeof(testOb->parse_command_prepos_list()), 50),
     }) :));
 
     expect_function("parse_command_all_word", testOb);
     expect("parse_command_all_word returns all word", (: ({
-        assert(testOb->parse_command_all_word(), "==", "all"),
+        assert_equal(testOb->parse_command_all_word(), "all"),
     }) :));
 
     expect_function("parse_command_users", testOb);
