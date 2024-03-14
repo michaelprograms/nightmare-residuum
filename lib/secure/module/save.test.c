c4c0c89a87d2cc26e89297e834a850b8e2fd36db
Michael Programs <michael.programs@gmail.com>
2024-03-13 21:43:14 -0400

    update D_SAVE to assert_equal

--- lib/secure/module/save.test.c
+++ lib/secure/module/save.test.c
@@ -26,13 +26,13 @@ void test_save_path () {
 
     expect("save handles paths", (: ({
         unguarded((: rm, PATH_TEST_SAVE :)),
-        assert(testOb->query_save_path(), "==", 0),
+        assert_equal(testOb->query_save_path(), 0),
 
         testOb->set_save_path(PATH_TEST_SAVE),
-        assert(testOb->query_save_path(), "==", PATH_TEST_SAVE),
+        assert_equal(testOb->query_save_path(), PATH_TEST_SAVE),
 
         testOb->set_save_path(0),
-        assert(testOb->query_save_path(), "==", 0),
+        assert_equal(testOb->query_save_path(), 0),
 
     }) :));
 }
@@ -43,12 +43,12 @@ void test_save_data () {
 
     expect("save handles data", (: ({
         unguarded((: rm, PATH_TEST_SAVE :)),
-        assert(file_size(PATH_TEST_SAVE), "==", -1),
+        assert_equal(file_size(PATH_TEST_SAVE), -1),
 
         testOb->set_save_path(PATH_TEST_SAVE),
-        assert(testOb->query_save_path(), "==", PATH_TEST_SAVE),
+        assert_equal(testOb->query_save_path(), PATH_TEST_SAVE),
 
         unguarded((: call_other, testOb, "save_data" :)),
-        assert(file_size(PATH_TEST_SAVE), ">", 0),
+        assert_equal(file_size(PATH_TEST_SAVE) > 0, 1),
     }) :));
 }
