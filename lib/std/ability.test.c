d1765cfc5b60e1c1db5f47724cb9e70930ab72af
Michael Programs <michael.programs@gmail.com>
2024-03-14 16:52:29 -0400

    update STD_ABILITY to assert_equal

--- lib/std/ability.test.c
+++ lib/std/ability.test.c
@@ -21,7 +21,7 @@ void test_name () {
     expect_function("query_name", testOb);
 
     expect("handles ability name", (: ({
-        assert(testOb->query_name(), "==", "ability"),
+        assert_equal(testOb->query_name(), "ability"),
     }) :));
 }
 
@@ -30,16 +30,16 @@ void test_type () {
     expect_function("set_type", testOb);
 
     expect("handles setting and querying type", (: ({
-        assert(testOb->query_type(), "==", UNDEFINED),
+        assert_equal(testOb->query_type(), UNDEFINED),
 
         testOb->set_type("attack"),
-        assert(testOb->query_type(), "==", "attack"),
+        assert_equal(testOb->query_type(), "attack"),
 
         testOb->set_type("heal"),
-        assert(testOb->query_type(), "==", "heal"),
+        assert_equal(testOb->query_type(), "heal"),
 
         testOb->set_type("utility"),
-        assert(testOb->query_type(), "==", "utility"),
+        assert_equal(testOb->query_type(), "utility"),
     }) :));
 }
 
@@ -48,13 +48,13 @@ void test_powers () {
     expect_function("set_powers", testOb);
 
     expect("handles setting and querying skill powers", (: ({
-        assert(testOb->query_powers(), "==", ([ ])),
+        assert_equal(testOb->query_powers(), ([ ])),
 
         testOb->set_powers(([ "brawl": 5, ])),
-        assert(testOb->query_powers(), "==", ([ "brawl": 5, ])),
+        assert_equal(testOb->query_powers(), ([ "brawl": 5, ])),
 
         testOb->set_powers(([ "ranged": 5, "psionic": 5, ])),
-        assert(testOb->query_powers(), "==", ([ "ranged": 5, "psionic": 5, ])),
+        assert_equal(testOb->query_powers(), ([ "ranged": 5, "psionic": 5, ])),
     }) :));
 }
 
@@ -66,13 +66,13 @@ void test_weapons () {
     expect_function("query_best_weapon", testOb);
 
     expect("handles setting and querying weapons", (: ({
-        assert(testOb->query_weapons(), "==", ([ ])),
+        assert_equal(testOb->query_weapons(), ([ ])),
 
         testOb->set_weapons(([ "brawl": ({ 1 }), ])),
-        assert(testOb->query_weapons(), "==", ([ "brawl": ({ 1 }), ])),
+        assert_equal(testOb->query_weapons(), ([ "brawl": ({ 1 }), ])),
 
         testOb->set_weapons(([ "brawl": ({ 1 }), "blade": ({ 1, 2 }), "blunt": ({ 1, 2 }), ])),
-        assert(testOb->query_weapons(), "==", ([ "brawl": ({ 1 }), "blade": ({ 1, 2 }), "blunt": ({ 1, 2 }), ])),
+        assert_equal(testOb->query_weapons(), ([ "brawl": ({ 1 }), "blade": ({ 1, 2 }), "blunt": ({ 1, 2 }), ])),
     }) :));
 
     liv = new(STD_LIVING);
@@ -87,31 +87,31 @@ void test_weapons () {
     weaponBlunt->set_name("test blunt");
 
     expect("handles setting and querying weapons", (: ({
-        assert($(liv)->query_species(), "==", "human"),
-        assert($(liv)->query_wielded_weapons(), "==", ({ })),
+        assert_equal($(liv)->query_species(), "human"),
+        assert_equal($(liv)->query_wielded_weapons(), ({ })),
 
         testOb->set_weapons(([ "brawl": ({ 1 }), ])),
-        assert(testOb->query_best_weapon($(liv)), "==", UNDEFINED),
+        assert_equal(testOb->query_best_weapon($(liv)), UNDEFINED),
 
         // start with 1 handed
         testOb->set_weapons(([ "blunt": ({ 1 }), ])),
         // not wielded yet
-        assert(testOb->query_best_weapon($(liv)), "==", UNDEFINED),
+        assert_equal(testOb->query_best_weapon($(liv)), UNDEFINED),
         // wield blade
-        assert($(liv)->handle_wield($(weaponBlade)), "==", 1),
-        assert(testOb->query_best_weapon($(liv)), "==", UNDEFINED),
+        assert_equal($(liv)->handle_wield($(weaponBlade)), 1),
+        assert_equal(testOb->query_best_weapon($(liv)), UNDEFINED),
         // wield blunt
-        assert($(liv)->handle_wield($(weaponBlunt)), "==", 1),
-        assert(testOb->query_best_weapon($(liv)), "==", $(weaponBlunt)),
+        assert_equal($(liv)->handle_wield($(weaponBlunt)), 1),
+        assert_equal(testOb->query_best_weapon($(liv)), $(weaponBlunt)),
         // change to 2 handed
         testOb->set_weapons(([ "blunt": ({ 2 }), ])),
-        assert(testOb->query_best_weapon($(liv)), "==", UNDEFINED),
-        assert($(liv)->handle_unwield($(weaponBlade)), "==", 1),
-        assert($(liv)->handle_unwield($(weaponBlunt)), "==", 1),
+        assert_equal(testOb->query_best_weapon($(liv)), UNDEFINED),
+        assert_equal($(liv)->handle_unwield($(weaponBlade)), 1),
+        assert_equal($(liv)->handle_unwield($(weaponBlunt)), 1),
         $(weaponBlunt)->set_hands(2),
-        assert($(weaponBlunt)->query_hands(), "==", 2),
-        assert($(liv)->handle_wield($(weaponBlunt)), "==", 1),
-        assert(testOb->query_best_weapon($(liv)), "==", $(weaponBlunt)),
+        assert_equal($(weaponBlunt)->query_hands(), 2),
+        assert_equal($(liv)->handle_wield($(weaponBlunt)), 1),
+        assert_equal(testOb->query_best_weapon($(liv)), $(weaponBlunt)),
     }) :));
 
     if (liv) liv->handle_remove();
@@ -125,13 +125,13 @@ void test_targets () {
 
     expect("handles setting and querying targets", (: ({
         // default
-        assert(testOb->query_targets(), "==", 1),
+        assert_equal(testOb->query_targets(), 1),
 
         testOb->set_targets(2),
-        assert(testOb->query_targets(), "==", 2),
+        assert_equal(testOb->query_targets(), 2),
 
         testOb->set_targets(10),
-        assert(testOb->query_targets(), "==", 10),
+        assert_equal(testOb->query_targets(), 10),
     }) :));
 }
 
@@ -141,16 +141,16 @@ void test_cooldown () {
 
     expect("handles setting and querying cooldown", (: ({
         // default
-        assert(testOb->query_cooldown(), "==", 1),
+        assert_equal(testOb->query_cooldown(), 1),
 
         testOb->set_cooldown(2),
-        assert(testOb->query_cooldown(), "==", 2),
+        assert_equal(testOb->query_cooldown(), 2),
 
         testOb->set_cooldown(10),
-        assert(testOb->query_cooldown(), "==", 10),
+        assert_equal(testOb->query_cooldown(), 10),
 
         testOb->set_cooldown(0),
-        assert(testOb->query_cooldown(), "==", 0),
+        assert_equal(testOb->query_cooldown(), 0),
     }) :));
 }
 
@@ -160,15 +160,15 @@ void test_difficulty_factor () {
 
     expect("handles difficulty factor", (: ({
         // defaults to 100
-        assert(testOb->query_difficulty_factor(), "==", 100),
+        assert_equal(testOb->query_difficulty_factor(), 100),
 
         // check lowering difficulty factor
         testOb->set_difficulty_factor(90),
-        assert(testOb->query_difficulty_factor(), "==", 90),
+        assert_equal(testOb->query_difficulty_factor(), 90),
 
         // check raising difficulty factor
         testOb->set_difficulty_factor(110),
-        assert(testOb->query_difficulty_factor(), "==", 110),
+        assert_equal(testOb->query_difficulty_factor(), 110),
     }) :));
 }
 
@@ -186,41 +186,41 @@ void test_direct_verb_lib () {
 
     expect("handles ", (: ({
         // not enough arguments
-        assert(testOb->direct_verb_liv(), "==", 0),
-        assert(testOb->direct_verb_liv(1), "==", 0),
+        assert_equal(testOb->direct_verb_liv(), 0),
+        assert_equal(testOb->direct_verb_liv(1), 0),
 
         // arguments but no type
-        assert(testOb->direct_verb_liv(1, 1), "==", 0),
+        assert_equal(testOb->direct_verb_liv(1, 1), 0),
         // no type invalid against source
-        assert(testOb->direct_verb_liv(1, this_object()), "==", 0),
+        assert_equal(testOb->direct_verb_liv(1, this_object()), 0),
         // no type invalid against target
-        assert(testOb->direct_verb_liv(1, $(ob)), "==", 0),
+        assert_equal(testOb->direct_verb_liv(1, $(ob)), 0),
 
         // set attack type
         testOb->set_type("attack"),
-        assert(testOb->query_type(), "==", "attack"),
+        assert_equal(testOb->query_type(), "attack"),
         // attack type but no living
-        assert(testOb->direct_verb_liv(1, 1), "==", 0),
+        assert_equal(testOb->direct_verb_liv(1, 1), 0),
         // attack invalid against source
-        assert(testOb->direct_verb_liv(1, this_object()), "==", 0),
+        assert_equal(testOb->direct_verb_liv(1, this_object()), 0),
         // attack target
-        assert(testOb->direct_verb_liv(1, $(ob)), "==", 1),
+        assert_equal(testOb->direct_verb_liv(1, $(ob)), 1),
 
         // set heal type
         testOb->set_type("heal"),
-        assert(testOb->query_type(), "==", "heal"),
+        assert_equal(testOb->query_type(), "heal"),
         // heal source
-        assert(testOb->direct_verb_liv(1, this_object()), "==", 1),
+        assert_equal(testOb->direct_verb_liv(1, this_object()), 1),
         // heal target
-        assert(testOb->direct_verb_liv(1, $(ob)), "==", 1),
+        assert_equal(testOb->direct_verb_liv(1, $(ob)), 1),
 
         // set utility type
         testOb->set_type("utility"),
-        assert(testOb->query_type(), "==", "utility"),
+        assert_equal(testOb->query_type(), "utility"),
         // utility source
-        assert(testOb->direct_verb_liv(1, this_object()), "==", 1),
+        assert_equal(testOb->direct_verb_liv(1, this_object()), 1),
         // utility target
-        assert(testOb->direct_verb_liv(1, $(ob)), "==", 1),
+        assert_equal(testOb->direct_verb_liv(1, $(ob)), 1),
     }) :));
 
     destruct(ob);
@@ -232,15 +232,15 @@ void test_ability_requirements () {
 
     expect("handles setting and querying requirements", (: ({
         // default value
-        assert(testOb->query_ability_type(), "==", UNDEFINED),
+        assert_equal(testOb->query_ability_type(), UNDEFINED),
 
         // check updated
         testOb->set_ability_requirements(([ "anyone": ([ "level": 1, ]), ])),
-        assert(testOb->query_ability_requirements(), "==", ([ "anyone": ([ "level": 1, ]), ])),
+        assert_equal(testOb->query_ability_requirements(), ([ "anyone": ([ "level": 1, ]), ])),
 
         // check updated
         testOb->set_ability_requirements(([ "scoundrel": ([ "level": 5, ]), ])),
-        assert(testOb->query_ability_requirements(), "==", ([ "scoundrel": ([ "level": 5, ]), ])),
+        assert_equal(testOb->query_ability_requirements(), ([ "scoundrel": ([ "level": 5, ]), ])),
     }) :));
 }
 
@@ -252,23 +252,23 @@ void test_handle_help () {
 
         // should contain Type section always
         testOb->set_type("utility"),
-        assert(testOb->query_type(), "==", "utility"),
-        assert(regexp(testOb->handle_help(this_object()), "Type"), "==", 1),
-        assert(regexp(testOb->handle_help(this_object()), "Utility"), "==", 1),
+        assert_equal(testOb->query_type(), "utility"),
+        assert_equal(regexp(testOb->handle_help(this_object()), "Type"), 1),
+        assert_equal(regexp(testOb->handle_help(this_object()), "Utility"), 1),
 
         // should contain ability requirements
-        assert(regexp(testOb->handle_help(this_object()), "Anyone Requirements"), "==", 0),
+        assert_equal(regexp(testOb->handle_help(this_object()), "Anyone Requirements"), 0),
         testOb->set_ability_requirements(([ "anyone": ([ "level": 1, "stats": ([ "strength": 1, ]), "skills": ([ "brawl attack": 1, ]) ]), ])),
-        assert(regexp(testOb->handle_help(this_object()), "Anyone Requirements"), "==", 1),
-        assert(regexp(testOb->handle_help(this_object()), "Level 1, 1 strength, 1 brawl attack"), "==", 1),
+        assert_equal(regexp(testOb->handle_help(this_object()), "Anyone Requirements"), 1),
+        assert_equal(regexp(testOb->handle_help(this_object()), "Level 1, 1 strength, 1 brawl attack"), 1),
 
         // should contain weapon requirements
-        assert(regexp(testOb->handle_help(this_object()), "Weapons"), "==", 0),
+        assert_equal(regexp(testOb->handle_help(this_object()), "Weapons"), 0),
         testOb->set_weapons(([ "brawl": ({ 1 }), ])),
-        assert(regexp(testOb->handle_help(this_object()), "Weapons"), "==", 1),
-        assert(regexp(testOb->handle_help(this_object()), "one handed brawl"), "==", 1),
+        assert_equal(regexp(testOb->handle_help(this_object()), "Weapons"), 1),
+        assert_equal(regexp(testOb->handle_help(this_object()), "one handed brawl"), 1),
         // updated weapon requirements
         testOb->set_weapons(([ "blunt": ({ 1, 2 }), ])),
-        assert(regexp(testOb->handle_help(this_object()), "one handed blunt, two handed blunt"), "==", 1),
+        assert_equal(regexp(testOb->handle_help(this_object()), "one handed blunt, two handed blunt"), 1),
     }) :));
 }
