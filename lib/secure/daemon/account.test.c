efee31c203f02daebfe5cf910d6740495c4eec99
Michael Programs <michael.programs@gmail.com>
2024-03-13 21:41:11 -0400

    update D_ACCOUNT to assert_equal

--- lib/secure/daemon/account.test.c
+++ lib/secure/daemon/account.test.c
@@ -13,24 +13,24 @@ void test_valid_name () {
     expect_function("query_valid_name", testOb);
 
     expect("query_valid_name handles names", (: ({
-        assert(testOb->query_valid_name("valid"), "==", 1),
-        assert(testOb->query_valid_name("VALID"), "==", 1),
-        assert(testOb->query_valid_name("V4L1D"), "==", 1),
-        assert(testOb->query_valid_name("Valid123"), "==", 1),
-        assert(testOb->query_valid_name("valid_"), "==", 1),
-        assert(testOb->query_valid_name("valid@"), "==", 1),
-        assert(testOb->query_valid_name("valid."), "==", 1),
-        assert(testOb->query_valid_name("valid-"), "==", 1),
+        assert_equal(testOb->query_valid_name("valid"), 1),
+        assert_equal(testOb->query_valid_name("VALID"), 1),
+        assert_equal(testOb->query_valid_name("V4L1D"), 1),
+        assert_equal(testOb->query_valid_name("Valid123"), 1),
+        assert_equal(testOb->query_valid_name("valid_"), 1),
+        assert_equal(testOb->query_valid_name("valid@"), 1),
+        assert_equal(testOb->query_valid_name("valid."), 1),
+        assert_equal(testOb->query_valid_name("valid-"), 1),
 
-        assert(testOb->query_valid_name(""), "==", 0),
-        assert(testOb->query_valid_name("no"), "==", 0),
-        assert(testOb->query_valid_name("nonononononononononononononononononononononononononononononononom"), "==", 0),
-        assert(testOb->query_valid_name("invalid!@#"), "==", 0),
-        assert(testOb->query_valid_name(",<.>;:'\"[{]}\\|=+!#$%^&*()~`"), "==", 0),
-        assert(testOb->query_valid_name("_valid"), "==", 0),
-        assert(testOb->query_valid_name("@valid"), "==", 0),
-        assert(testOb->query_valid_name(".valid"), "==", 0),
-        assert(testOb->query_valid_name("-valid"), "==", 0),
+        assert_equal(testOb->query_valid_name(""), 0),
+        assert_equal(testOb->query_valid_name("no"), 0),
+        assert_equal(testOb->query_valid_name("nonononononononononononononononononononononononononononononononom"), 0),
+        assert_equal(testOb->query_valid_name("invalid!@#"), 0),
+        assert_equal(testOb->query_valid_name(",<.>;:'\"[{]}\\|=+!#$%^&*()~`"), 0),
+        assert_equal(testOb->query_valid_name("_valid"), 0),
+        assert_equal(testOb->query_valid_name("@valid"), 0),
+        assert_equal(testOb->query_valid_name(".valid"), 0),
+        assert_equal(testOb->query_valid_name("-valid"), 0),
     }) :));
 }
 
@@ -46,15 +46,15 @@ void test_exists () {
     expect_function("query_exists", testOb);
 
     expect("query_exists handles invalid accounts", (: ({
-        assert(testOb->query_exists(""), "==", 0),
-        assert(testOb->query_exists("testaccountinvalid"), "==", 0),
+        assert_equal(testOb->query_exists(""), 0),
+        assert_equal(testOb->query_exists("testaccountinvalid"), 0),
     }) :));
 
     expect("query_exists handles valid accounts", (: ({
         // create a mock account file
-        assert(unguarded((: mkdirs("/save/account/a") :)), "==", 1),
-        assert(unguarded((: write_file("/save/account/a/accounttest.o", "accounttest.o mock save", 1) :)), "==", 1),
-        assert(testOb->query_exists("accounttest"), "==", 1),
-        assert(unguarded((: rm("/save/account/a/accounttest.o") :)), "==", 1),
+        assert_equal(unguarded((: mkdirs("/save/account/a") :)), 1),
+        assert_equal(unguarded((: write_file("/save/account/a/accounttest.o", "accounttest.o mock save", 1) :)), 1),
+        assert_equal(testOb->query_exists("accounttest"), 1),
+        assert_equal(unguarded((: rm("/save/account/a/accounttest.o") :)), 1),
     }) :));
 }
