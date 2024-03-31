4e6b0ad5c403f3b20a3826e7ffcbbd84d7058460
Michael Programs <michael.programs@gmail.com>
2024-03-30 23:28:30 -0400

    add missing parser apply for unlock to key

--- lib/std/item/key.c
+++ lib/std/item/key.c
@@ -2,6 +2,16 @@ inherit STD_ITEM;
 
 int is_key () { return 1; }
 
+int direct_lock_str_with_obj (mixed args...) {
+    object key;
+    if (sizeof(args) > 1 && objectp(args[1])) {
+        key = args[1];
+    }
+    if (key && key->is_key()) {
+        return 1;
+    }
+    return 0;
+}
 int direct_unlock_str_with_obj (mixed args...) {
     object key;
     if (sizeof(args) > 1 && objectp(args[1])) {
