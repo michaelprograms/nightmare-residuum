9650f0b1da16ac08e89a1bbb5e7b37690eee0724
Michael Programs <michael.programs@gmail.com>
2024-04-20 16:02:54 -0400

    adjust secure/shell files for test coverage

--- lib/secure/shell/variable.test.c
+++ lib/secure/shell/variable.test.c
@@ -3,23 +3,24 @@ inherit M_TEST;
 private int action = 0;
 
 private nosave object testOb;
+private nosave string testFile;
+void before_all_tests () {
+    testFile = D_TEST->create_coverage(replace_string(base_name(), ".test", ".c"));
+}
 void before_each_test () {
-    if (objectp(testOb)) destruct(testOb);
-    testOb = clone_object("/secure/shell/variable.c");
+    testOb = clone_object(testFile);
 }
 void after_each_test () {
     if (objectp(testOb)) destruct(testOb);
 }
-
+void after_all_tests () {
+    rm(testFile);
+}
 string *test_order () {
     return ({ "test_set_variables", "test_set_variable_hooks" });
 }
 
 void test_set_variables () {
-    expect_function("query_variable", testOb);
-    expect_function("set_variable", testOb);
-    expect_function("unset_variable", testOb);
-
     expect("variables handles set/query", (: ({
         assert_equal(testOb->query_variable("testkey1"), UNDEFINED),
 
@@ -49,8 +50,6 @@ void test_set_variables () {
 }
 
 void test_set_variable_hooks () {
-    expect_function("set_variable_hook", testOb);
-
     testOb->set_variable_hook("testkey", function (mixed value) {
         action ++;
         return value;
