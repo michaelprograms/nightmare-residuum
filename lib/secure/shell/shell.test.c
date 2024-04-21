64b02513ada4bac51d533d3c60842810ddbbe276
Michael Programs <michael.programs@gmail.com>
2024-04-21 14:46:12 -0400

    clean secure/shell and secure/std tests for automatic testOb

--- lib/secure/shell/shell.test.c
+++ lib/secure/shell/shell.test.c
@@ -1,18 +1,1 @@
 inherit M_TEST;
-
-private int action = 0;
-
-private nosave object testOb;
-private nosave string testFile;
-void before_all_tests () {
-    testFile = D_TEST->create_coverage(replace_string(base_name(), ".test", ".c"));
-}
-void before_each_test () {
-    testOb = clone_object(testFile);
-}
-void after_each_test () {
-    if (objectp(testOb)) destruct(testOb);
-}
-void after_all_tests () {
-    rm(testFile);
-}
