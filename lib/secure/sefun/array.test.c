48940aa5fa97f764cf941f2dea98ca7873020f59
Michael Programs <michael.programs@gmail.com>
2024-04-21 14:45:30 -0400

    cleanup sefun tests for automatic testOb

--- lib/secure/sefun/array.test.c
+++ lib/secure/sefun/array.test.c
@@ -1,20 +1,5 @@
 inherit M_TEST;
 
-private nosave object testOb;
-private nosave string testFile;
-void before_all_tests () {
-    testFile = D_TEST->create_coverage(replace_string(base_name(), ".test", ".c"));
-}
-void before_each_test () {
-    testOb = clone_object(testFile);
-}
-void after_each_test () {
-    if (objectp(testOb)) destruct(testOb);
-}
-void after_all_tests () {
-    rm(testFile);
-}
-
 void test_distinct_array () {
     expect("distinct_array handles inputs", (: ({
         assert_equal(testOb->distinct_array(({ 1, 2, 2, 3, 2, 1, 3, 2, 1 })), ({ 3, 2, 1 })),
--- lib/secure/sefun/grammar.test.c
+++ lib/secure/sefun/grammar.test.c
@@ -1,20 +1,5 @@
 inherit M_TEST;
 
-private nosave object testOb;
-private nosave string testFile;
-void before_all_tests () {
-    testFile = D_TEST->create_coverage(replace_string(base_name(), ".test", ".c"));
-}
-void before_each_test () {
-    testOb = clone_object(testFile);
-}
-void after_each_test () {
-    if (objectp(testOb)) destruct(testOb);
-}
-void after_all_tests () {
-    rm(testFile);
-}
-
 void test_remove_article () {
     expect("articles are trimmed", (: ({
         assert_equal(testOb->remove_article(""), ""),
