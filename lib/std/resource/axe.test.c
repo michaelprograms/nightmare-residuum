94d411b65f9048942e1971aa8b298a6d2319e18e
Michael Programs <michael.programs@gmail.com>
2024-04-28 21:01:05 -0400

    create tests for resource tools

--- lib/std/resource/axe.test.c
+++ lib/std/resource/axe.test.c
@@ -1,1 +1,7 @@
 inherit M_TEST;
+
+void test_axe () {
+    expect("axe should be setup with name and id", (: ({
+        assert_equal(testOb->id("axe"), 1),
+    }) :));
+}
--- lib/std/resource/pickaxe.test.c
+++ lib/std/resource/pickaxe.test.c
@@ -1,1 +1,7 @@
 inherit M_TEST;
+
+void test_pickaxe () {
+    expect("pickaxe should be setup with name and id", (: ({
+        assert_equal(testOb->id("pickaxe"), 1),
+    }) :));
+}
