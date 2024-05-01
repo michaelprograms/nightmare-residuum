66cc27cb66a321f0cf4ac312c1bed5685e49bd0f
Michael Programs <michael.programs@gmail.com>
2024-05-01 18:18:47 -0400

    cleanup D_COMMAND and add maximum test coverage

--- lib/secure/daemon/command.c
+++ lib/secure/daemon/command.c
@@ -5,27 +5,76 @@ nosave private mapping __Abilities = ([ ]);
 nosave private mapping __Commands = ([ ]);
 nosave private mapping __Verbs = ([ ]);
 
-string *query_paths () { return __Paths; }
-mapping query_debug_abiltiies () { return __Abilities; }
-mapping query_debug_commands () { return __Commands; }
-mapping query_debug_verbs () { return __Verbs; }
-void scan (string *paths, string type);
-void scan_all ();
-
-void create () {
-    set_no_clean(1);
-    __Paths = ({ });
-    __Abilities = ([ ]);
-    __Commands = ([ ]);
-    __Verbs = ([ ]);
-    scan_all();
+string *query_paths () {
+    return __Paths;
 }
+
+/* ----- scanning ----- */
+
+void scan (string *paths, string type) {
+    foreach (string path in paths) {
+        if (file_size(path) != -2) continue;
+        foreach (string file in get_dir(path + "/*.c")) {
+            string cmd = file[0..<3];
+            if (type == "ability") {
+                if (!arrayp(__Abilities[cmd])) {
+                    __Abilities[cmd] = ({ });
+                }
+                __Abilities[cmd] += ({ path });
+                // load all verb rules
+                load_object(path + "/" + file);
+            } else if (type == "command") {
+                if (!arrayp(__Commands[cmd])) {
+                    __Commands[cmd] = ({ });
+                }
+                __Commands[cmd] += ({ path });
+            } else if (type == "verb") {
+                if (!arrayp(__Verbs[cmd])) {
+                    __Verbs[cmd] = ({ });
+                }
+                __Verbs[cmd] += ({ path });
+                // load all verb rules
+                load_object(path + "/" + file);
+            }
+        }
+        __Paths = distinct_array(__Paths + ({ path }));
+    }
+}
+
+/* ----- querying single ----- */
+
+string query_ability (string ability) {
+    if (__Abilities[ability]) {
+        return __Abilities[ability][0];
+    }
+    return 0;
+}
+string query_command (string command) {
+    if (__Commands[command]) {
+        if (!previous_object()->query_immortal() && regexp(__Commands[command][0], "/immortal")) {
+            return 0;
+        }
+        return __Commands[command][0];
+    }
+    return 0;
+}
+string query_verb (string verb) {
+    if (__Verbs[verb] && __Verbs[verb][0]) {
+        // verbs have to be loaded for parsing rules to apply
+        load_object(__Verbs[verb][0] + "/" + verb + ".c");
+    }
+    return __Verbs[verb] ? __Verbs[verb][0] : 0;
+}
+
+/* ----- querying all ----- */
 
 varargs string *query_abilities (string str) {
     string *abilities, *tmp;
     int i;
 
-    if (!str) return keys(__Abilities);
+    if (!str) {
+        return keys(__Abilities);
+    }
     i = sizeof(abilities = keys(__Abilities));
     tmp = ({ });
     while (i--) {
@@ -39,7 +88,9 @@ varargs string *query_commands (string str) {
     string *cmds, *tmp;
     int i;
 
-    if (!str) return keys(__Commands);
+    if (!str) {
+        return keys(__Commands);
+    }
     i = sizeof(cmds = keys(__Commands));
     tmp = ({ });
     while (i--) {
@@ -53,7 +104,9 @@ varargs string *query_verbs (string str) {
     string *verbs, *tmp;
     int i;
 
-    if (!str) return keys(__Verbs);
+    if (!str) {
+        return keys(__Verbs);
+    }
     i = sizeof(verbs = keys(__Verbs));
     tmp = ({ });
     while (i--) {
@@ -64,50 +117,15 @@ varargs string *query_verbs (string str) {
     return tmp;
 }
 
-string query_ability (string ability) {
-    return __Abilities[ability] ? __Abilities[ability][0] : 0;
-}
-string query_command (string command) {
-    if (__Commands[command]) {
-        if (!this_user()->query_character()->query_immortal() && regexp(__Commands[command][0], "/immortal")) return 0;
-        return __Commands[command][0];
-    } else return 0;
-}
-string query_verb (string verb) {
-    if (__Verbs[verb] && __Verbs[verb][0]) {
-        // verbs have to be loaded for parsing rules to apply
-        load_object(__Verbs[verb][0] + "/" + verb + ".c");
-    }
-    return __Verbs[verb] ? __Verbs[verb][0] : 0;
-}
+/* ----- applies ----- */
 
-void scan (string *paths, string type) {
-    if (stringp(paths)) paths = ({ paths });
-    else if (!arrayp(paths)) return;
-    foreach (string path in paths) {
-        if (file_size(path) != -2) continue;
-        foreach (string file in get_dir(path + "/*.c")) {
-            string cmd = file[0..<3];
-            if (type == "ability") {
-                if (!arrayp(__Abilities[cmd])) __Abilities[cmd] = ({ });
-                __Abilities[cmd] += ({ path });
-                // load all verb rules
-                load_object(path + "/" + file);
-            } else if (type == "command") {
-                if (!arrayp(__Commands[cmd])) __Commands[cmd] = ({ });
-                __Commands[cmd] += ({ path });
-            } else if (type == "verb") {
-                if (!arrayp(__Verbs[cmd])) __Verbs[cmd] = ({ });
-                __Verbs[cmd] += ({ path });
-                // load all verb rules
-                load_object(path + "/" + file);
-            }
-        }
-        __Paths = distinct_array(__Paths + ({ path }));
-    }
-}
+void create () {
+    set_no_clean(1);
+    __Paths = ({ });
+    __Abilities = ([ ]);
+    __Commands = ([ ]);
+    __Verbs = ([ ]);
 
-void scan_all () {
     scan(({
         "/cmd/ability",
     }), "ability");
--- lib/secure/daemon/command.test.c
+++ lib/secure/daemon/command.test.c
@@ -1,1 +1,37 @@
 inherit M_TEST;
+
+nosave private int __ImmortalFlag = 0;
+int query_immortal () {
+    return __ImmortalFlag;
+}
+
+void test_paths_and_files () {
+    expect("daemon has paths", (: ({
+        assert_equal(sizeof(testOb->query_paths()) > 0, 1),
+    }) :));
+    expect("daemon scanned paths", (: ({
+        assert_equal(sizeof(testOb->query_abilities()) > 0, 1),
+        assert_equal(sizeof(testOb->query_abilities("/cmd/ability")) > 0, 1),
+
+        assert_equal(sizeof(testOb->query_commands()) > 0, 1),
+        assert_equal(sizeof(testOb->query_commands("/cmd/character")) > 0, 1),
+
+        assert_equal(sizeof(testOb->query_verbs()) > 0, 1),
+        assert_equal(sizeof(testOb->query_verbs("/cmd/verb")) > 0, 1),
+    }) :));
+    expect("daemon scanned files", (: ({
+        assert_equal(testOb->query_ability("zap"), "/cmd/ability"),
+        assert_equal(testOb->query_ability("invalid"), 0),
+
+        assert_equal(testOb->query_command("quit"), "/cmd/character"),
+        assert_equal(testOb->query_command("invalid"), 0),
+
+        assert_equal(testOb->query_command("ed"), 0),
+        __ImmortalFlag = 1,
+        assert_equal(testOb->query_command("ed"), "/secure/cmd/immortal"),
+        __ImmortalFlag = 0,
+
+        assert_equal(testOb->query_verb("say"), "/cmd/verb"),
+        assert_equal(testOb->query_verb("invalid"), 0),
+    }) :));
+}
