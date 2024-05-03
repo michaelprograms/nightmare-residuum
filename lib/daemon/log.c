d1aab7be2674acd77e441169ee53a9925faa0d7e
Michael Programs <michael.programs@gmail.com>
2024-05-02 21:05:26 -0400

    add maximum test coverage for D_LOG

--- lib/daemon/log.c
+++ lib/daemon/log.c
@@ -1,4 +1,3 @@
-
 int log_unique (string filename, string entry) {
     string *lines;
 
@@ -6,41 +5,38 @@ int log_unique (string filename, string entry) {
         return 0;
     }
 
-    filename = replace_string(filename+"", " ", "_");
+    filename = replace_string(filename, " ", "_");
     if (!regexp(filename, "^/log/unique/")) {
         filename = "/log/unique/" + filename;
     }
     if (regexp(entry, "\n$")) {
         entry = entry[0..<2];
     }
-    if (!mkdirs(filename + ".txt")) { // dummy extension
-        return 0;
-    }
+    mkdirs(filename + ".txt"); // dummy extension
 
     lines = explode(read_file(filename) || "", "\n");
-    if (member_array(entry, lines) == -1) {
-        return write_file(filename, entry + "\n");
+    if (member_array(entry, lines) > -1) {
+        return 0;
     }
-    return 0;
+    return write_file(filename, entry + "\n");
 }
 
 int log (string filename, string entry) {
-    if (!filename || filename == "") {
+    if (!filename || filename == "" || !entry || entry == "") {
         return 0;
     }
 
-    filename = replace_string(filename+"", " ", "_");
+    filename = replace_string(filename, " ", "_");
     if (!regexp(filename, "^/log/")) {
         filename = "/log/" + filename;
     }
     if (!regexp(entry, "\n$")) {
         entry += "\n";
     }
-    if (!mkdirs(filename + ".txt")) { // dummy extension
-        return 0;
-    }
-    if (file_size(filename) > 20000) { // 20 kb
-        rename(filename, filename+"-"+time());
+    mkdirs(filename + ".txt"); // dummy extension
+
+    if (file_size(filename) >= 20000) { // 20 kb
+        rename(filename, filename + "-" + time());
     }
     return write_file(filename, entry);
 }
--- lib/daemon/log.test.c
+++ lib/daemon/log.test.c
@@ -1,1 +1,48 @@
 inherit M_TEST;
+
+void test_log () {
+    expect("log should handle invalid input", (: ({
+        assert_equal(testOb->log("", ""), 0),
+        assert_equal(testOb->log("test", ""), 0),
+    }):));
+
+    expect("log should log", (: ({
+        assert_equal(file_size("/log/test"), -1),
+        assert_equal(testOb->log("test", "test1\n"), 1),
+        assert_equal(testOb->log("test", "test2"), 1),
+        assert_equal(read_file("/log/test"), "test1\ntest2\n"),
+        // clean file
+        assert_equal(rm("/log/test"), 1),
+    }):));
+
+    expect("log should roll", (: ({
+        // queue up log roll
+        assert_equal(write_file("/log/test", sprintf("%20000s", ".")), 1),
+        assert_equal(file_size("/log/test"), 20000),
+        // log roll
+        assert_equal(testOb->log("test", "."), 1),
+        assert_equal(file_size("/log/test-"+time()), 20000),
+        assert_equal(file_size("/log/test"), 2),
+        // clean files
+        assert_equal(rm("/log/test"), 1),
+        assert_equal(rm("/log/test-"+time()), 1),
+    }) :));
+}
+
+void test_log_unique () {
+    expect("log_unique should handle invalid input", (: ({
+        assert_equal(testOb->log_unique("", ""), 0),
+        assert_equal(testOb->log_unique("test", ""), 0),
+    }):));
+
+    expect("log_unique should log", (: ({
+        assert_equal(file_size("/log/unique/test"), -1),
+        assert_equal(testOb->log_unique("test", "test1\n"), 1),
+        assert_equal(testOb->log_unique("test", "test1"), 0), // already added
+        assert_equal(testOb->log_unique("test", "test2"), 1),
+        assert_equal(testOb->log_unique("test", "test2"), 0), // already added
+        assert_equal(read_file("/log/unique/test"), "test1\ntest2\n"),
+        // clean file
+        assert_equal(rm("/log/unique/test"), 1),
+    }):));
+}
