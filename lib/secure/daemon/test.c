eb837873ba54763d0142a2b715f1669db60949c1
Michael Programs <michael.programs@gmail.com>
2024-05-03 23:08:24 -0400

    fix D_TEST uncovered lines for single file to only display for single file

--- lib/secure/daemon/test.c
+++ lib/secure/daemon/test.c
@@ -251,7 +251,9 @@ void display_results (mapping results) {
     if (testStartTime > 0) {
         write("\n" + sprintf("%-20s", results["numTests"]+" tests:") + "\e[33m" + sprintf("%7.2f", time/1000000.0) + " ms" + "\e[0m" + "\n\n");
 
-        write("\n" + sprintf("%-20s", "Uncovered:") + convert_to_ranges(query_unhit_lines()) + "\n");
+        if (!sizeof(__TestFiles)) {
+            write(sprintf("%-20s", "Uncovered:") + convert_to_ranges(query_unhit_lines()) + "\n\n");
+        }
     }
 
     if (sizeof(results["failLog"]) > 0) {
