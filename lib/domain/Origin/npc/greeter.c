5053b0f8e161144240eca35d0de9274b00594ef5
Michael Programs <michael.programs@gmail.com>
2024-05-05 10:59:27 -0400

    fix typos in greeter

--- lib/domain/Origin/npc/greeter.c
+++ lib/domain/Origin/npc/greeter.c
@@ -5,7 +5,7 @@ void create () {
     set_name("R-0111010");
     set_id(({ "synthetic creature", "synthetic", "creature", }));
     set_short("a synthetic creature");
-    set_long("A creature seemingly constucted of synthetic components. It consists of a large elongated torso structure supported by four jointed legs, all encased in a scale-like exoskeloton. There are two articulated arms at each end of the structure, almost looking like a type of crustacean. The creature does seem to have a discernable head or face.");
+    set_long("A creature seemingly constructed of synthetic components. It consists of a large elongated torso structure supported by four jointed legs, all encased in a scale-like exoskeleton. There are two articulated arms at each end of the structure, almost looking like a type of crustacean. The creature does not seem to have a discernable head or face.");
     set_species("custodian");
 }
 
