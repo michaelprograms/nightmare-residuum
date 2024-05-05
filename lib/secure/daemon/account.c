2bee3cb86e8c3e43f3373e5b10a7d5c0558218ad
Michael Programs <michael.programs@gmail.com>
2024-05-05 11:43:10 -0400

    cleanup secure/daemon/account and maximize test coverage

--- lib/secure/daemon/account.c
+++ lib/secure/daemon/account.c
@@ -2,7 +2,7 @@
 
 int query_valid_name (string name) {
     int l;
-    if (!name || (l = strlen(name)) < 4 || l > 64) {
+    if (!name || (l = sizeof(name)) < 4 || l > 64) {
         return 0;
     }
     if (regexp(name, "^[a-zA-Z0-9]+[a-zA-Z0-9_@\\.\\-]+$")) {
@@ -11,8 +11,9 @@ int query_valid_name (string name) {
 }
 
 string query_save_path (string name) {
-    if (strlen(name) < 4) {
-        error("Bad argument 1 to account->query_save_path");
+    int l;
+    if (!name || (l = sizeof(name)) < 4 || l > 64) {
+        return 0;
     }
     return sprintf(DIR_SAVE_ACCOUNT+"/%c/%s.o", name[0], name);
 }
--- lib/secure/daemon/account.test.c
+++ lib/secure/daemon/account.test.c
@@ -1,16 +1,7 @@
 inherit M_TEST;
 
 void test_valid_name () {
-    expect("query_valid_name handles names", (: ({
-        assert_equal(testOb->query_valid_name("valid"), 1),
-        assert_equal(testOb->query_valid_name("VALID"), 1),
-        assert_equal(testOb->query_valid_name("V4L1D"), 1),
-        assert_equal(testOb->query_valid_name("Valid123"), 1),
-        assert_equal(testOb->query_valid_name("valid_"), 1),
-        assert_equal(testOb->query_valid_name("valid@"), 1),
-        assert_equal(testOb->query_valid_name("valid."), 1),
-        assert_equal(testOb->query_valid_name("valid-"), 1),
-
+    expect("query_valid_name handles invalid names", (: ({
         assert_equal(testOb->query_valid_name(""), 0),
         assert_equal(testOb->query_valid_name("no"), 0),
         assert_equal(testOb->query_valid_name("nonononononononononononononononononononononononononononononononom"), 0),
@@ -21,11 +12,27 @@ void test_valid_name () {
         assert_equal(testOb->query_valid_name(".valid"), 0),
         assert_equal(testOb->query_valid_name("-valid"), 0),
     }) :));
+    expect("query_valid_name handles valid names", (: ({
+        assert_equal(testOb->query_valid_name("valid"), 1),
+        assert_equal(testOb->query_valid_name("VALID"), 1),
+        assert_equal(testOb->query_valid_name("V4L1D"), 1),
+        assert_equal(testOb->query_valid_name("Valid123"), 1),
+        assert_equal(testOb->query_valid_name("valid_"), 1),
+        assert_equal(testOb->query_valid_name("valid@"), 1),
+        assert_equal(testOb->query_valid_name("valid."), 1),
+        assert_equal(testOb->query_valid_name("valid-"), 1),
+    }) :));
 }
 
 void test_save_path () {
+    expect("query_save_path handles invalid names", (: ({
+        assert_equal(testOb->query_save_path(""), 0),
+        assert_equal(testOb->query_save_path("no"), 0),
+        assert_equal(testOb->query_save_path("nonononononononononononononononononononononononononononononononom"), 0),
+    }) :));
     expect("query_save_path handles valid names", (: ({
         assert_regex(testOb->query_save_path("name"), "^/save/account/n/name"),
+        assert_regex(testOb->query_save_path("tester"), "^/save/account/t/tester"),
     }) :));
 }
 
