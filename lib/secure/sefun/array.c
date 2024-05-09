a79796cbb184f5ad91ed6160a08f240fe9fc431c
Michael Programs <michael.programs@gmail.com>
2024-05-09 19:35:37 -0400

    format sefun/array

--- lib/secure/sefun/array.c
+++ lib/secure/sefun/array.c
@@ -1,9 +1,12 @@
 mixed *distinct_array (mixed *arr) {
-    int i, maxi;
+    int i, l;
     mapping tmp;
-    if (!arrayp(arr) || !sizeof(arr)) error("Bad argument 1 to array->distinct_array");
-    tmp = allocate_mapping(maxi = sizeof(arr));
-    for (i = 0; i < maxi; i ++) {
+    if (!arrayp(arr) || !sizeof(arr)) {
+        error("Bad argument 1 to array->distinct_array");
+    }
+    l = sizeof(arr);
+    tmp = allocate_mapping(l);
+    for (i = 0; i < l; i ++) {
         tmp[arr[i]] = 1;
     }
     return keys(tmp);
@@ -12,9 +15,15 @@ mixed *distinct_array (mixed *arr) {
 mixed reduce (mixed *arr, function fn, mixed result) {
     int i, l;
 
-    if (!arrayp(arr) || !sizeof(arr)) error("Bad argument 1 to array->reduce");
-    if (!functionp(fn)) error("Bad argument 2 to array->reduce");
-    if (undefinedp(result)) error("Bad argument 3 to array->reduce");
+    if (!arrayp(arr) || !sizeof(arr)) {
+        error("Bad argument 1 to array->reduce");
+    }
+    if (!functionp(fn)) {
+        error("Bad argument 2 to array->reduce");
+    }
+    if (undefinedp(result)) {
+        error("Bad argument 3 to array->reduce");
+    }
 
     l = sizeof(arr);
     for (i = 0; i < l; i ++) {
