b38031ca3b79fcbe1f4cc826e771e8d0182adcb3
Michael Programs <michael.programs@gmail.com>
2024-05-13 00:28:55 -0400

    start tests for std/ability/requiremrents

--- lib/std/ability/requirements.test.c
+++ lib/std/ability/requirements.test.c
@@ -1,1 +1,13 @@
 inherit M_TEST;
+
+void test_ability_requirements () {
+    expect("", (: ({
+        assert_equal(testOb->query_ability_requirements(), UNDEFINED),
+
+        testOb->set_ability_requirements(([ ])),
+        assert_equal(testOb->query_ability_requirements(), ([ ])),
+
+        testOb->set_ability_requirements(([ "level": 123, ])),
+        assert_equal(testOb->query_ability_requirements(), ([ "level": 123 ])),
+    }) :));
+}
