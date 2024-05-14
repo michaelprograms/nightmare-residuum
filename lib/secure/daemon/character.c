0821f0c14a26676a0e703ebb2154c0ecadc977f7
Michael Programs <michael.programs@gmail.com>
2024-05-14 18:25:20 -0400

    maximize secure/daemon/character test coverage

--- lib/secure/daemon/character.c
+++ lib/secure/daemon/character.c
@@ -3,9 +3,6 @@
 
 inherit M_CLEAN;
 
-nosave private int __ConnectedConcurrent = 0;
-nosave private int __ConnectedTotal = 0;
-
 int query_valid_name (string name) {
     int l;
     if (!name || (l = strlen(name)) < 4 || l > 18) {
@@ -17,37 +14,30 @@ int query_valid_name (string name) {
     return 0;
 }
 
-int query_valid_save_path (string name, string path) {
-    string *results = pcre_extract(path, "^" + DIR_SAVE_CHARACTER + "/([a-z])/([a-z]+)/([a-z]+)");
-    return results[0] == name[0..0] && results[1] == name && member_array(results[2], VALID_TYPES) > -1;
-}
 varargs string query_save_path (string name, string type) {
-    if (strlen(name) < 4) {
-        error("Bad argument 1 to character->query_save_path");
+    int l;
+    if (!name || (l = sizeof(name)) < 4 || l > 64) {
+        return 0;
     }
-    if (undefinedp(type)) {
-        error("Bad argument 2 to character->query_save_path");
+    if (!stringp(type) || !sizeof(type)) {
+        type = "character";
     }
     return DIR_SAVE_CHARACTER + "/" + name[0..0] + "/" + name + "/" + type + ".o";
 }
 
 varargs int query_exists (string name, string type) {
-    if (!name) {
-        error("Bad argument 1 to character->query_exists");
-    }
     if (!stringp(type) || !sizeof(type)) {
         type = "character";
     }
     return query_valid_name(name) && file_size(query_save_path(name, type)) > -1;
 }
 
-object query_whois_character (string name) {
+private object load_character (string name) {
     object char;
 
     if (!query_exists(name)) {
         return 0;
     }
-
     char = clone_object(STD_CHARACTER);
     char->set_name(capitalize(name));
     char->restore_data();
@@ -55,48 +45,18 @@ object query_whois_character (string name) {
     return char;
 }
 
+object query_character (string name) {
+    return load_character(name);
+}
+
 int query_immortal (string name) {
     object char;
-    int immortal;
+    int immortal = 0;
 
-    if (!query_exists(name)) {
-        return 0;
+    char = load_character(name);
+    if (char) {
+        immortal = char->query_immortal();
+        destruct(char);
     }
-
-    char = clone_object(STD_CHARACTER);
-    char->set_name(capitalize(name));
-    char->restore_data();
-    char->set_save_path(0);
-    immortal = char->query_immortal();
-    destruct(char);
     return immortal;
-}
-
-/* ----- connected info ----- */
-
-int query_connected_concurrent () {
-    return __ConnectedConcurrent;
-}
-int query_connected_total () {
-    return __ConnectedTotal;
-}
-
-int increment_connected_total () {
-    __ConnectedTotal ++;
-}
-
-/* ----- apply ----- */
-
-void heart_beat () {
-    int n = sizeof(characters());
-    if (n > __ConnectedConcurrent) {
-        __ConnectedConcurrent = n;
-    }
-}
-
-void create () {
-    if (!clonep()) {
-        set_heart_beat(1);
-        set_no_clean(1);
-    }
 }
--- lib/secure/daemon/character.test.c
+++ lib/secure/daemon/character.test.c
@@ -17,5 +17,71 @@ void test_valid_name () {
 void test_save_path () {
     expect("query_save_path handles valid names", (: ({
         assert_regex(testOb->query_save_path("name", "type"), "^/save/character/n/name/type"),
+        assert_regex(testOb->query_save_path("tester"), "^/save/character/t/tester/character.o"),
+    }) :));
+
+    expect("query_save_path handles invalid names", (: ({
+        assert_equal(testOb->query_save_path(""), 0),
+        assert_equal(testOb->query_save_path("no"), 0),
+        assert_equal(testOb->query_save_path("nonononononononononononononononononononononononononononononononom"), 0),
+    }) :));
+}
+
+void test_exists () {
+    expect("query_exists handles invalid characters", (: ({
+        assert_equal(testOb->query_exists(""), 0),
+        assert_equal(testOb->query_exists("", "character"), 0),
+
+        assert_equal(testOb->query_exists("testcharacterinvalid"), 0),
+        assert_equal(testOb->query_exists("testcharacterinvalid", "character"), 0),
+    }) :));
+
+    expect("query_exists handles valid characters", (: ({
+        // create mock character file
+        assert_equal(mkdirs("/save/character/c/charactertest"), 1),
+        assert_equal(write_file("/save/character/c/charactertest/character.o", "charactertest.o mock save", 1), 1),
+
+        // test query_exists
+        assert_equal(testOb->query_exists("charactertest"), 1),
+
+        // delete mock character file
+        assert_equal(rm("/save/character/c/charactertest/character.o"), 1),
+        assert_equal(rmdir("/save/character/c/charactertest"), 1),
+    }) :));
+}
+
+object TestCharacter;
+void test_load_character () {
+    expect("query_character returns loaded character", (: ({
+        // character doesn't exist
+        assert_equal(testOb->query_character("charactertest"), 0),
+
+        // create mock character file
+        assert_equal(mkdirs("/save/character/c/charactertest"), 1),
+        assert_equal(write_file("/save/character/c/charactertest/character.o", "charactertest.o mock save", 1), 1),
+
+        // test query_character
+        assert_regex(TestCharacter = testOb->query_character("charactertest"), "/std/character#"),
+
+        // delete mock character file
+        assert_equal(rm("/save/character/c/charactertest/character.o"), 1),
+        assert_equal(rmdir("/save/character/c/charactertest"), 1),
+    }) :));
+    if (TestCharacter) destruct(TestCharacter);
+
+    expect("query_immortal returns returns truthy", (: ({
+        // character isn't immortal / doesn't exist
+        assert_equal(testOb->query_immortal("charactertest"), 0),
+
+        // create mock character file
+        assert_equal(mkdirs("/save/character/c/charactertest"), 1),
+        assert_equal(write_file("/save/character/c/charactertest/character.o", "__Immortal 1", 1), 1),
+
+        // test query_immortal again
+        assert_equal(testOb->query_immortal("charactertest"), 1),
+
+        // delete mock character file
+        assert_equal(rm("/save/character/c/charactertest/character.o"), 1),
+        assert_equal(rmdir("/save/character/c/charactertest"), 1),
     }) :));
 }
