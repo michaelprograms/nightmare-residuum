de53e66b02e61a90e03fdf27dbf23fdd04bd6bf4
Michael Programs <michael.programs@gmail.com>
2024-05-15 23:03:01 -0400

    cleanup cmd/verb/give

--- lib/cmd/verb/give.c
+++ lib/cmd/verb/give.c
@@ -18,7 +18,7 @@ mixed can_give_obj_to_liv (mixed args...) {
 varargs void do_give_obj_to_liv (object ob, object liv) {
     object tc = this_character();
     message("action", "You give " + ob->query_name() + " to " +
-    liv->query_cap_name() + ".\n", tc);
+    liv->query_cap_name() + ".", tc);
     message("action", tc->query_cap_name() + " gives " + ob->query_name() + " to you.", liv);
     message("action", tc->query_cap_name() + " gives " + ob->query_name() + " to " + liv->query_cap_name() + ".", environment(tc), ({ tc, liv }));
     return ob->handle_move(liv);
@@ -37,7 +37,9 @@ mixed can_give_wrd_wrd_to_liv (mixed args...) {
     int amount = to_int(args[0]), n;
     string currency = args[1];
 
-    if (amount < 1) return 0;
+    if (amount < 1) {
+        return 0;
+    }
     if ((n = this_character()->query_currency(currency)) < 1) {
         return "You do not have any " + currency + " to drop.";
     }
@@ -52,7 +54,9 @@ mixed do_give_wrd_wrd_to_liv (mixed args...) {
     object liv = args[2];
     object env = environment(this_character());
 
-    if (!liv) return "You can't give " + amount + " " + currency + " to that."; // default parser response
+    if (!liv) {
+        return "You can't give " + amount + " " + currency + " to that."; // default parser response
+    }
 
     liv->add_currency(currency, amount);
     this_character()->add_currency(currency, amount);
