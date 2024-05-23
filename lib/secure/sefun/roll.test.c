6cb37995fbf827767fb94625a986480ef560c563
Michael Programs <michael.programs@gmail.com>
2024-05-23 17:27:44 -0400

    maximize test coverage of sefun/roll

--- lib/secure/sefun/roll.test.c
+++ lib/secure/sefun/roll.test.c
@@ -15,4 +15,11 @@ void test_roll_die () {
         assert_equal(rollData[0] >= 1, 1), // sum
         assert_equal(rollData[0] <= 100, 1), // sum
     }) :));
+
+    expect("roll_die handles bad inputs", (: ({
+        assert_catch((: testOb->roll_die(UNDEFINED, UNDEFINED) :), "*Bad argument 1 to roll_die\n"),
+
+        assert_catch((: testOb->roll_die(1, UNDEFINED) :), "*Bad argument 2 to roll_die\n"),
+        assert_catch((: testOb->roll_die(1, 1) :), "*Bad argument 2 to roll_die\n"),
+    }) :));
 }
