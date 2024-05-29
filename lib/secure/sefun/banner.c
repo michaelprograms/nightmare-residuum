836fb4bd23c8b74e2c3e9c65f8e16becb38b41c6
Michael Programs <michael.programs@gmail.com>
2024-05-28 20:15:07 -0400

    fix error in new sefun/banner

--- lib/secure/sefun/banner.c
+++ lib/secure/sefun/banner.c
@@ -20,18 +20,18 @@ string query_banner () {
     int pad;
     int r = 25 + 1+random(100);
 
-    tmp = "  " + mud_name() + "  ";
+    tmp = "  " + SEFUN->mud_name() + "  ";
     pad = 40 - sizeof(tmp) / 2;
     __Banner[<2] = __Banner[<2][0..pad-1] + tmp + __Banner[<2][80-pad..79];
 
-    tmp = "  " + version() + "   " + mudlib_version() + "  ";
+    tmp = "  " + SEFUN->version() + "   " + SEFUN->mudlib_version() + "  ";
     pad = 40 - sizeof(tmp) / 2;
     __Banner[<1] = __Banner[<1][0..pad-1] + tmp + __Banner[<1][80-pad+sizeof(tmp)%2..79];
 
     if (userColor == "256") {
-        int *c1 = query_random_color();
+        int *c1 = SEFUN->query_random_color();
         int *c2 = allocate(3, 159+random(32));
-        colors += color_gradient(c1, c2, 34);
+        colors += SEFUN->color_gradient(c1, c2, 34);
         for (int i = sizeof(colors)-1; i > -1; i --) {
             colors[i] = "\e[38;2;"+colors[i]+"m";
             colors += ({ colors[i] });
@@ -47,11 +47,11 @@ string query_banner () {
             } else if (__Banner[i][j..j] == ".") {
                 if (userColor == "256") {
                     dot[0] = 10240 + (
-                        (!random(r) ? 0x1 : 0) |
-                        (!random(r) ? 0x2 : 0) |
-                        (!random(r) ? 0x4 : 0) |
+                        (!random(r) ? 0x1  : 0) |
+                        (!random(r) ? 0x2  : 0) |
+                        (!random(r) ? 0x4  : 0) |
                         (!random(r) ? 0x40 : 0) |
-                        (!random(r) ? 0x8 : 0) |
+                        (!random(r) ? 0x8  : 0) |
                         (!random(r) ? 0x10 : 0) |
                         (!random(r) ? 0x20 : 0) |
                         (!random(r) ? 0x80 : 0)
