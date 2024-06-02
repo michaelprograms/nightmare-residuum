97f78763fb36cb1f1ba342aa897f51f9952c5054
Michael Programs <michael.programs@gmail.com>
2024-06-02 19:07:55 -0400

    start testing std/living/command

--- lib/std/living/command.c
+++ lib/std/living/command.c
@@ -1,3 +1,20 @@
+/*
+** If the result is 0, the parser didn't know the verb so we keep looking.
+** If a 1 was returned, then nothing more needs to be done.
+** If the result is -1 or -2, the parser figured something was wrong.
+*/
+int query_unknown_command (int result) {
+    switch (result) {
+    case 0:
+        return 0; // Parser found no verb, fall through
+    case 1:
+        return 1; // Parser called verb
+    case -1: case -2: default:
+        message("action", "You aren't able to do that.", this_object());
+        return 1;
+    }
+}
+
 varargs int handle_command (string command, int debug) {
     string *split, action, input;
     string cmdPath;
@@ -8,7 +25,7 @@ varargs int handle_command (string command, int debug) {
         return 0;
     }
     if (this_object()->query_immobile()) {
-        message("status", "You are unable to take any actions right now.", this_object());
+        message("action", "You are unable to take any actions right now.", this_object());
         return 1;
     }
 
@@ -31,29 +48,10 @@ varargs int handle_command (string command, int debug) {
     if (stringp(result)) {
         write(result + "\n");
         return 1;
+    } else if (query_unknown_command(result)) {
+        return 1;
     }
 
-    /*
-    ** If the result is 0, the parser didn't know the verb so we keep looking.
-    ** If a 1 was returned, then nothing more needs to be done.
-    ** If the result is -1 or -2, the parser figured something was wrong.
-    **
-    */
-    switch (result) {
-    case 0: // Parser found no verb, fall through
-        break;
-    case 1: // Parser called verb
-        return 1;
-    case -1:
-        write("Nonsense?!\n");
-        return 1;
-    case -2:
-        write("You aren't able to do that.\n");
-        return 1;
-    default:
-        write("Unexpected result.\n");
-        return 1;
-    }
     if (environment() && environment()->query_exit(command)) {
         resultGo = parse_sentence("go " + command);
         if (resultGo == 1) {
--- lib/std/living/command.test.c
+++ lib/std/living/command.test.c
@@ -1,1 +1,14 @@
 inherit M_TEST;
+
+void test_unknown_response () {
+    expect("unknown commands are responded to", (: ({
+        // no verb called
+        assert_equal(testOb->query_unknown_command(0), 0),
+        // verb called
+        assert_equal(testOb->query_unknown_command(1), 1),
+
+        assert_equal(testOb->query_unknown_command(-1), 1),
+        assert_equal(testOb->query_unknown_command(-2), 1),
+        assert_equal(testOb->query_unknown_command(-123), 1),
+    }) :));
+}
