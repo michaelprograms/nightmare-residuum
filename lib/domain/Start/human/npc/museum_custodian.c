dd565a98901ca40ab1d4dc57da94e0333c75563c
Michael Programs <michael.programs@gmail.com>
2024-06-16 22:57:22 -0400

    fix typos

--- lib/domain/Start/human/npc/museum_custodian.c
+++ lib/domain/Start/human/npc/museum_custodian.c
@@ -6,6 +6,6 @@ void create () {
     set_name("R-" + number);
     set_id(({ "synthetic creature", "synthetic", "creature", "r-"+number }));
     set_short("a synthetic creature");
-    set_long("A creature seemingly constucted of synthetic components. It consists of a large elongated torso structure supported by four jointed legs, all encased in a scale-like exoskeloton. There are two articulated arms at each end of the structure, almost looking like a type of crustacean. The creature does seem to have a discernable head or face.");
+    set_long("A creature seemingly constructed of synthetic components. It consists of a large elongated torso structure supported by four jointed legs, all encased in a scale-like exoskeleton. There are two articulated arms at each end of the structure, almost looking like a type of crustacean. The creature does seem to have a discernable head or face.");
     set_species("custodian");
 }
