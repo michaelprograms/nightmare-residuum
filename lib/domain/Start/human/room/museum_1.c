7d414ebde232a3f4ce3f1a1377d985927d7ac519
Michael Programs <michael.programs@gmail.com>
2024-08-03 20:28:54 -0400

    prevent museum entrance without ticket

--- lib/domain/Start/human/room/museum_1.c
+++ lib/domain/Start/human/room/museum_1.c
@@ -2,17 +2,30 @@
 
 inherit STD_ROOM;
 
+int admit_entrance (object ob, string dir) {
+    object npc;
+    if (ob->is_character()) {
+        if (present("museum ticket", ob)) {
+            return 1;
+        } else if (npc = present("museum attendant", environment(ob))) {
+            npc->handle_command("say You need to buy a ticket before you are admitted entrance.");
+            return 0;
+        }
+    }
+    return 1;
+}
+
 void create () {
     ::create();
     set_properties(([ "indoors": 1, "no attack": 1, ]));
     set_short("a spacious lobby");
     set_long("The entrance to a building consisting of a spacious lobby with high ceilings and polished floors. The walls are made of stone blocks and covered with tapestries. A grand staircase along the back of the lobby leads up towards another floor that looks down upon the lobby. Hallways branch to the north and west, heading to the wings of the building.");
     set_exits(([
-        "north": HUMAN_ROOM + "museum_1_n1.c",
-        "west": HUMAN_ROOM + "museum_1_w1.c",
-        "up": HUMAN_ROOM + "museum_2.c",
         "out southeast": HUMAN_ROOM "park_nw.c",
     ]));
+    set_exit("north", HUMAN_ROOM + "museum_1_n1.c", (: admit_entrance :));
+    set_exit("west", HUMAN_ROOM + "museum_1_w1.c", (: admit_entrance :));
+    set_exit("up", HUMAN_ROOM + "museum_2.c", (: admit_entrance :));
     set_looks(([
         ({ "building", "ceiling", "walls" }): "A building of large proportions. The lobby is a large room with high ceilings and distant walls.",
         "lobby": "The entrance to the building, it features several different paths to traverse through.",
