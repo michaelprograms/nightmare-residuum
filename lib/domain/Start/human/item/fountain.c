f2f86214cd14a311c6340ef2b0e674f90ee9282f
Michael Programs <michael.programs@gmail.com>
2024-08-08 20:14:43 -0400

    fix fountain colors to intense cyan

--- lib/domain/Start/human/item/fountain.c
+++ lib/domain/Start/human/item/fountain.c
@@ -5,7 +5,7 @@ void create () {
     set_name("fountain");
     set_id(({ "fountain", "basin", "fountain basin", }));
     set_adjective(({ "wide" }));
-    set_short("a wide %^CYAN%^fountain basin%^DEFAULT%^");
+    set_short("a wide %^I_CYAN%^fountain basin%^DEFAULT%^");
     set_long("A fountain with a wide basin made of a hardened clay. A metallic winged figure stands upon a small pedestal in the center of the basin with water gently spraying out of its mouth.");
     set_no_get(1);
 }
