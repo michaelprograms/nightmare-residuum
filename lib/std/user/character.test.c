66c2fc362ee5050f8d53102cc2e5db499a98cb61
Michael Programs <michael.programs@gmail.com>
2024-08-24 21:27:00 -0400

    expand test coverage of std/user/character

--- lib/std/user/character.test.c
+++ lib/std/user/character.test.c
@@ -1,1 +1,13 @@
 inherit M_TEST;
+
+void test_character_name () {
+    expect("character name sets up character", (: ({
+        assert_equal(testOb->query_character(), UNDEFINED),
+
+        testOb->set_character_name("test"),
+        assert_equal(testOb->query_character()->is_character(), 1),
+
+        testOb->character_exit(),
+        assert_equal(testOb->query_character(), UNDEFINED),
+    }) :));
+}
