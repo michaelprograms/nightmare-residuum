984f97b29ef9c6abd36df6a2fc3a69d88a81e09b
Michael Programs <michael.programs@gmail.com>
2024-09-16 18:47:40 -0400

    expand sefun/border test coverage

--- lib/secure/sefun/border.test.c
+++ lib/secure/sefun/border.test.c
@@ -29,6 +29,10 @@ void test_character_colors () {
 }
 
 void test_border_item () {
+    expect("border header is formatted", (: ({
+        assert_equal(testOb->format_border_item(([ "header": ({ "Header1", "Header2" }), "columns": 2 ]), "256", "L", "R"), ({ "L  %^RESET%^BOLD%^UNDERLINE%^Header1%^RESET%^                             %^RESET%^BOLD%^UNDERLINE%^Header2%^RESET%^                               R" })),
+        assert_equal(testOb->format_border_item(([ "header": ({ "A123456789", "B123456789", "C123456789", "D123456789", "E123456789", "F123456789", "G123456789", "H123456789", "I123456789", "J123456789", "K123456789", "L123456789" }), "columns": 10 ]), "256", "L", "R"), ({ "L  A12345678901234567B12345678901234567C12345678901234567D12345678901234567  R" })),
+    }) :));
     expect("border item is formatted", (: ({
         assert_equal(__Width = 20, 20),
         assert_equal(testOb->format_border_item(([ "items": ({ "1", "2", "3", }) ]), "256", "L", "R"), ({  "L  1     2       R", "L  3             R" })),
