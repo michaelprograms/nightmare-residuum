10a97b5f4460b066f62538368fea32315da63eb9
Michael Programs <michael.programs@gmail.com>
2024-11-14 18:46:38 -0500

    fix D_ACHIEVEMENTS tests

--- lib/daemon/achievements.test.c
+++ lib/daemon/achievements.test.c
@@ -15,8 +15,8 @@ void test_query_achievements_from_room () {
     object room2 = load_object("/domain/Start/human/void.c");
 
     expect("query_achievements_from_room finds achievements", (: ({
-        assert_equal(testOb->query_achievements_from_room("/domain/Start/human/room/square.c"), ({ ([ "description": "Explore the museum's hallways and appreciate the works of art.", "flags": 10, "name": "Art Appreciation" ]) })),
-        assert_equal(testOb->query_achievements_from_room($(room1)), ({ ([ "description": "Explore the museum's hallways and appreciate the works of art.", "flags": 10, "name": "Art Appreciation" ]) })),
+        assert_equal(testOb->query_achievements_from_room("/domain/Start/human/room/square.c"), ({ ([ "description": "Explore the museum's hallways and appreciate the works of art.", "flags": "p1,p2,p3,p4,s1,s2,s3,s4", "name": "Art Appreciation" ]) })),
+        assert_equal(testOb->query_achievements_from_room($(room1)), ({ ([ "description": "Explore the museum's hallways and appreciate the works of art.", "flags": "p1,p2,p3,p4,s1,s2,s3,s4", "name": "Art Appreciation" ]) })),
 
         assert_equal(testOb->query_achievements_from_room("/domain/Nowhere/room/void.c"), 0),
         assert_equal(testOb->query_achievements_from_room($(room2)), 0),
