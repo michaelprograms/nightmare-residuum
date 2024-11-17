31db1d1e7844ceca3a9bd8c0bc0e3a4e72e31d65
Michael Programs <michael.programs@gmail.com>
2024-11-17 15:59:08 -0500

    re-arrange achievement order of events

--- lib/domain/Start/human/item/sculpture_4.c
+++ lib/domain/Start/human/item/sculpture_4.c
@@ -6,13 +6,13 @@ string sculpture_long () {
     string desc = "";
     object tc = this_character();
 
-    tc->set_achievement_flag("Art Appreciation", "s4", (HUMAN_BASE + "achievements.c")->query_achievement_flags("Art Appreciation"));
-
     // if (query_account_setting("screenreader") == "on") {
         desc += "It depicts a sculpture.";
     // } else {
         // @TODO
     // }
+
+    tc->set_achievement_flag("Art Appreciation", "s4", (HUMAN_BASE + "achievements.c")->query_achievement_flags("Art Appreciation"));
     return desc;
 }
 
