6e7d85f693667e34cdbff5c9ba2f1e3345891f52
Michael Programs <michael.programs@gmail.com>
2024-11-23 21:42:30 -0500

    add more descriptions to new stores

--- lib/domain/Start/human/room/store_steak.c
+++ lib/domain/Start/human/room/store_steak.c
@@ -9,6 +9,8 @@ void create () {
     ]));
     set_short("a village restaurant");
     set_long("The inside of a brick building set up as a small restaurant with a few tables and chairs. There are a large glass windows facing the streets to the southeast with doors alongside them. A counter along the northwestern wall stands in front of kitchen area.");
+    set_listen("default", "Something is frying.");
+    set_smell("default", "Freshly fried meat.");
     set_exits(([
         "out southeast": HUMAN_ROOM + "park_se.c",
     ]));
--- lib/domain/Start/human/room/store_vegan.c
+++ lib/domain/Start/human/room/store_vegan.c
@@ -9,6 +9,8 @@ void create () {
     ]));
     set_short("a village restaurant");
     set_long("The inside of a brick building set up as a small restaurant with a few tables and chairs. There are a large glass windows facing the streets to the northwest with doors alongside them. A counter along the southeastern wall stands in front of kitchen area.");
+    set_listen("default", "Something is frying.");
+    set_smell("default", "Freshly fried falafels.");
     set_exits(([
         "out northwest": HUMAN_ROOM + "park_se.c",
     ]));
