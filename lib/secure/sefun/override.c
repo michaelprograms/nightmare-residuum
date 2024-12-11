1d8c3df743eb7e113aacac59dca30f363b63aaf0
Michael Programs <michael.programs@gmail.com>
2024-12-10 22:36:45 -0500

    add lpc function docs to sefun/override

--- lib/secure/sefun/override.c
+++ lib/secure/sefun/override.c
@@ -1,17 +1,39 @@
 object this_character ();
 
+/**
+ * Disabled: Causes the next line of input to be sent to the specified function.
+ *
+ * @param args
+ */
 nomask varargs int input_to (mixed args...) {
     error("efun::input_to disabled");
 }
 
+/**
+ * Disabled: The current player object.
+ * Use this_user() instead.
+ *
+ * @param flag
+ */
 nomask varargs object this_player (int flag) {
     error("efun::this_player disabled");
 }
 
+/**
+ * Returns a list of interactive users that have an account set.
+ * This excludes the login prompt before account name input.
+ *
+ * @returns {STD_USER} an array of interactive user accounts
+ */
 object *users () {
     return filter(efun::users(), (: $1 && $1->query_name() :));
 }
 
+/**
+ * Write text to the current user or to the debug log.
+ *
+ * @param msg the text to receive or display
+ */
 nomask void write (string msg) {
     if (this_character() && this_character()->query_key_name() != "test") {
         this_character()->receive_message("system", msg);
@@ -25,6 +47,14 @@ nomask void write (string msg) {
     }
 }
 
+/**
+ * Deliver typed messages to the targets.
+ *
+ * @param type the type of message
+ * @param message text to display to targets
+ * @param target the recipient(s) of message
+ * @param exclude an exclusion list of recipients
+ */
 varargs void message (mixed type, string message, mixed target, mixed exclude) {
     if (!sizeof(message) || (!objectp(target) && !arrayp(target))) {
         return;
