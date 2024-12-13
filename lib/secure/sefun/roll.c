96328436c1b654ce3c77cc6f75dfdc21650ac7aa
Michael Programs <michael.programs@gmail.com>
2024-12-12 19:25:17 -0500

    add lpc function docs to sefun/roll

--- lib/secure/sefun/roll.c
+++ lib/secure/sefun/roll.c
@@ -1,3 +1,10 @@
+/**
+ * Roll a dice a number of times with variable sides.
+ *
+ * @param times the number of dice to roll
+ * @param sides the number of sides that each dice has
+ * @returns an array containing total and all the dice rolls
+ */
 mixed *roll_die (int times, int sides) {
     int *rolls;
 
