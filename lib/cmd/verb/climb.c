87dc499be69b8d6b7a4c68899b95e247d975a674
Michael Programs <michael.programs@gmail.com>
2025-01-14 20:33:17 -0500

    fix lpcdoc warning in climb verb

--- lib/cmd/verb/climb.c
+++ lib/cmd/verb/climb.c
@@ -12,20 +12,22 @@ mixed can_climb () {
 }
 
 varargs mixed can_climb_str (string dir) {
-    object po = previous_object(), env;
+    object po = previous_object();
+    /** @type {STD_ROOM} env */
+    object env = environment(po);
 
-    if (!(env = environment(po))) return "You are nowhere.";
+    if (!env) return "You are nowhere.";
     if (!env->query_climb(dir)) return "You cannot climb that way.";
     if (po->query_posture() != "standing") return "You cannot climb in that position.";
-
     return 1;
 }
 varargs void do_climb_str (string dir) {
-    object po = previous_object(), env = environment(po);
+    object po = previous_object();
+    /** @type {STD_ROOM} env */
+    object env = environment(po);
 
     if (po->query_posture() != "standing") {
         po->handle_command("stand");
     }
-
     env->handle_climb(po, "climb", dir);
 }
