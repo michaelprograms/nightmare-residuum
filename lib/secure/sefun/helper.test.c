39a89cc5cdcb5faf1446b2fdcf91b25e76db6aac
Michael Programs <michael.programs@gmail.com>
2025-01-31 21:12:10 -0500

    start adding sefun/helper tests

--- lib/secure/sefun/helper.test.c
+++ lib/secure/sefun/helper.test.c
@@ -3,3 +3,23 @@ inherit M_TEST;
 /**
  * @var {"/secure/sefun/helper"} testOb
  */
+
+void test_living_helpers () {
+    object character = new(STD_CHARACTER);
+    object npc = new(STD_NPC);
+
+    expect("living helpers returns appropriately", (: ({
+        assert_equal(testOb->livingp($(character)), 1),
+        assert_equal(testOb->npcp($(character)), 0),
+        assert_equal(testOb->characterp($(character)), 1),
+        assert_equal(testOb->immortalp($(character)), 0),
+
+        assert_equal(testOb->livingp($(npc)), 1),
+        assert_equal(testOb->npcp($(npc)), 1),
+        assert_equal(testOb->characterp($(npc)), 0),
+        assert_equal(testOb->immortalp($(npc)), 0),
+    }) :));
+
+    if (character) destruct(character);
+    if (npc) destruct(npc);
+}
