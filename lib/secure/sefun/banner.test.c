e4e38932b446b2d66d5bf68271519e4a3bfe103c
Michael Programs <michael.programs@gmail.com>
2025-03-10 21:59:33 -0400

    add test coverage to sefun/banner

--- lib/secure/sefun/banner.test.c
+++ lib/secure/sefun/banner.test.c
@@ -1,1 +1,13 @@
 inherit M_TEST;
+
+/**
+ * @var {"/secure/sefun/banner"} testOb
+ */
+
+void test_banner () {
+    string banner = testOb->query_banner();
+    expect("handles banner", (: ({
+        assert_equal(sizeof($(banner)) > 0, 1),
+        assert_equal(sizeof(explode($(banner), "\n")), 9),
+    }) :));
+}
