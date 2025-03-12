f48b88f0b8d8a4216f9612687495679c2820476d
Michael Programs <michael.programs@gmail.com>
2025-03-11 20:17:55 -0400

    maximize sefun/banner test coverage

--- lib/secure/sefun/banner.test.c
+++ lib/secure/sefun/banner.test.c
@@ -4,9 +4,25 @@ inherit M_TEST;
  * @var {"/secure/sefun/banner"} testOb
  */
 
+string __TermColor;
+string query_terminal (string type) {
+    if (type == "color") {
+        return __TermColor;
+    }
+}
+
 void test_banner () {
-    string banner = testOb->query_banner();
-    expect("handles banner", (: ({
+    string banner;
+
+    banner = testOb->query_banner();
+    expect("handles banner 16 color mode", (: ({
+        assert_equal(sizeof($(banner)) > 0, 1),
+        assert_equal(sizeof(explode($(banner), "\n")), 9),
+    }) :));
+
+    __TermColor = "256";
+    banner = testOb->query_banner();
+    expect("handles banner 256 color mode", (: ({
         assert_equal(sizeof($(banner)) > 0, 1),
         assert_equal(sizeof(explode($(banner), "\n")), 9),
     }) :));
