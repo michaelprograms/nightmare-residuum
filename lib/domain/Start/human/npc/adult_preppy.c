6640234d88ebf5021d304c17a25048bcd14aef57
Michael Programs <michael.programs@gmail.com>
2025-04-19 21:20:27 -0400

    add a new adult npc

---
+++ lib/domain/Start/human/npc/adult_preppy.c
@@ -0,0 +1,18 @@
+#include <human.h>
+
+inherit STD_NPC;
+
+void create () {
+    ::create();
+    set_id(({ "adult", "preppy adult", "human" }));
+    set_name("preppy adult");
+    set_short("a preppy adult");
+    set_gender(element_of(({ "male", "female" })));
+    set_long("A adult who scoffs at your presence.");
+    set_level(5);
+    set_species("human");
+    set_ability_list(({ "bite" }));
+    set_ability_chance(25);
+
+    add_currency("copper", 10 + random(20));
+}
