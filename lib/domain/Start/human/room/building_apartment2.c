1eb7851e48a69caadfea187293c21fa78c4a8e54
Michael Programs <michael.programs@gmail.com>
2025-06-22 23:18:58 -0400

    add descriptions for new apartment building lobby

--- lib/domain/Start/human/room/building_apartment2.c
+++ lib/domain/Start/human/room/building_apartment2.c
@@ -5,12 +5,14 @@ inherit STD_ROOM;
 void create () {
     ::create();
     set_short("an apartment building");
-    set_long("An apartment building.");
+    set_long("A lobby floor of an apartment building. It is spacious enough to allow for foot traffic of numerous people. The floor is made of wooden planks that have been smoothed by traffic. Brick walls frame the hallway and entryways. The lobby continues west and south.");
     set_exits(([
         "west": HUMAN_ROOM + "building_apartment1.c",
         "south": HUMAN_ROOM + "building_apartment3.c",
     ]));
     set_looks(([
-
+        "floor": "The floor has been smoothed by the passage of many people.",
+        "building": "The building houses many people.",
+        "walls": "The exposed brick walls are reminiscent of many of the buildings in this village.",
     ]));
 }
--- lib/domain/Start/human/room/building_apartment3.c
+++ lib/domain/Start/human/room/building_apartment3.c
@@ -5,12 +5,14 @@ inherit STD_ROOM;
 void create () {
     ::create();
     set_short("an apartment building");
-    set_long("An apartment building.");
+    set_long("A lobby floor of an apartment building. It is spacious enough to allow for foot traffic of numerous people. The floor is made of wooden planks that have been smoothed by traffic. Brick walls frame the hallway and entryways. The lobby continues north.");
     set_exits(([
         "out west": HUMAN_ROOM + "chestnut_st_e6.c",
         "north": HUMAN_ROOM + "building_apartment2.c",
     ]));
     set_looks(([
-
+        "floor": "The floor has been smoothed by the passage of many people.",
+        "building": "The building houses many people.",
+        "walls": "The exposed brick walls are reminiscent of many of the buildings in this village.",
     ]));
 }
