ed896136a3ddfa96e298bc25b993460f1a4a777b
Michael Programs <michael.programs@gmail.com>
2026-07-20 20:15:07 -0400

    update build script

--- build.sh
+++ build.sh
@@ -2,19 +2,14 @@
 
 set -euo pipefail
 
-rm -rf build
-mkdir -p build
-
-cp -f local_options ./driver/src/
-
-cd build
+cp -f local_options ./driver/src/local_options
+rm -rf build && mkdir -p build && cd build
 cmake ../driver \
   -DPACKAGE_DB_SQLITE=2 \
   -DPACKAGE_UIDS=OFF \
   -DPACKAGE_MUDLIB_STATS=OFF \
-  -DPACKAGE_EXTERNAL=ON
-
-# -DCMAKE_BUILD_TYPE=Release \
+  -DPACKAGE_EXTERNAL=ON \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo
 
 make -j `nproc` install
 cd ..
